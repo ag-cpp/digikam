02bb98a70305702b236ae16e4d17368165eb56d1
Maik Qualmann <metzpinguin@gmail.com>
2019-10-30 07:52:28 +0100

    Revert "move these two static functions for a test in PreviewLoadingTask"

--- core/libs/threadimageio/preview/previewtask.h
+++ core/libs/threadimageio/preview/previewtask.h
@@ -58,9 +58,6 @@ private:
     bool loadImagePreview(int sizeLimit = -1);
     void convertQImageToDImg();
 
-    bool wasExifRotated();
-    bool exifRotate(const QString& filePath);
-
 private:
 
     QImage m_qimage;
