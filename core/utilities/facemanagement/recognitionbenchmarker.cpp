4da35bf77ec0395ac1128b3d1e8a90e7d2c075d7
Gilles Caulier <caulier.gilles@gmail.com>
2019-11-06 21:04:12 +0100

    files missing

---
+++ core/utilities/facemanagement/recognitionbenchmarker.cpp
@@ -0,0 +1,96 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * https://www.digikam.org
+ *
+ * Date        : 2010-09-03
+ * Description : Face recognition benchmarker
+ *
+ * Copyright (C) 2010-2011 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ * Copyright (C) 2012-2019 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "recognitionbenchmarker.h"
+
+// Local includes
+
+#include "digikam_debug.h"
+#include "tagscache.h"
+
+namespace Digikam
+{
+
+RecognitionBenchmarker::Statistics::Statistics()
+    : knownFaces(0),
+      correctlyRecognized(0)
+{
+}
+
+RecognitionBenchmarker::RecognitionBenchmarker(FacePipeline::Private* const d)
+    : d(d)
+{
+}
+
+// NOTE: Bench performance code. No need i18n here
+QString RecognitionBenchmarker::result() const
+{
+    int totalImages = 0;
+
+    foreach (const Statistics& stat, results)
+    {
+        totalImages += stat.knownFaces;
+    }
+
+    QString s = QString::fromUtf8("<p>"
+                        "<u>Collection Properties:</u><br/>"
+                        "%1 Images <br/>"
+                        "%2 Identities <br/>"
+                        "</p><p>").arg(totalImages).arg(results.size());
+
+    for (QMap<int, Statistics>::const_iterator it = results.begin() ;
+         it != results.end() ; ++it)
+    {
+        const Statistics& stat = it.value();
+        double correctRate     = double(stat.correctlyRecognized) / stat.knownFaces;
+        s                     += TagsCache::instance()->tagName(it.key());
+        s                     += QString::fromUtf8(": %1 faces, %2 (%3%) correctly recognized<br/>")
+                                 .arg(stat.knownFaces).arg(stat.correctlyRecognized).arg(correctRate * 100);
+    }
+
+    s += QLatin1String("</p>");
+
+    return s;
+}
+
+void RecognitionBenchmarker::process(FacePipelineExtendedPackage::Ptr package)
+{
+    FaceUtils utils;
+
+    for (int i = 0 ; i < package->databaseFaces.size() ; ++i)
+    {
+        Identity identity  = utils.identityForTag(package->databaseFaces[i].tagId(), database);
+        Statistics& result = results[package->databaseFaces[i].tagId()];
+        result.knownFaces++;
+
+        if (identity == package->recognitionResults[i])
+        {
+            result.correctlyRecognized++;
+        }
+    }
+
+    emit processed(package);
+}
+
+} // namespace Digikam
---
+++ core/utilities/facemanagement/recognitionbenchmarker.h
@@ -0,0 +1,76 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * https://www.digikam.org
+ *
+ * Date        : 2010-09-03
+ * Description : Face recognition benchmarker
+ *
+ * Copyright (C) 2010-2011 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
+ * Copyright (C) 2012-2019 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef DIGIKAM_RECOGNITION_BENCH_MARKER_H
+#define DIGIKAM_RECOGNITION_BENCH_MARKER_H
+
+// Local includes
+
+#include "facepipeline_p.h"
+
+namespace Digikam
+{
+
+class Q_DECL_HIDDEN RecognitionBenchmarker : public WorkerObject
+{
+    Q_OBJECT
+
+public:
+
+    explicit RecognitionBenchmarker(FacePipeline::Private* const d);
+    QString result() const;
+
+public Q_SLOTS:
+
+    void process(FacePipelineExtendedPackage::Ptr package);
+
+Q_SIGNALS:
+
+    void processed(FacePipelineExtendedPackage::Ptr package);
+
+protected:
+
+    class Q_DECL_HIDDEN Statistics
+    {
+    public:
+
+        Statistics();
+
+    public:
+
+        int knownFaces;
+        int correctlyRecognized;
+    };
+
+protected:
+
+    QMap<int, Statistics>        results;
+
+    FacePipeline::Private* const d;
+    RecognitionDatabase          database;
+};
+
+} // namespace Digikam
+
+#endif // DIGIKAM_RECOGNITION_BENCH_MARKER_H
