7992271157cec56f7b569fd874f454698e19d592
Gilles Caulier <caulier.gilles@gmail.com>
2019-11-12 05:29:31 +0100

    add incude to fix jenkins compilation

--- core/dplugins/generic/webservices/flickr/flickrtalker.cpp
+++ core/dplugins/generic/webservices/flickr/flickrtalker.cpp
@@ -8,7 +8,7 @@
  *
  * Copyright (C) 2005-2009 by Vardhman Jain <vardhman at gmail dot com>
  * Copyright (C) 2009-2019 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright (C) 2017      by Maik Qualmann <metzpinguin at gmail dot com>
+ * Copyright (C) 2017-2019 by Maik Qualmann <metzpinguin at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -40,6 +40,7 @@
 #include <QProgressDialog>
 #include <QDesktopServices>
 #include <QNetworkAccessManager>
+#include <QNetworkCookieJar>
 
 // KDE includes
 
