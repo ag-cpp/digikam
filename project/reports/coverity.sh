3b3293a4ba0e891c879eb881a66828aaceff448a
Gilles Caulier <caulier.gilles@gmail.com>
2019-11-18 08:35:18 +0100

    check if coverity toolkit is installed

--- project/reports/coverity.sh
+++ project/reports/coverity.sh
@@ -5,19 +5,24 @@
 # Run Coverity Scan static analyzer on whole digiKam source code.
 # https://scan.coverity.com/
 #
-# Before to run this script you must set these Shell variable:
-# $DKCoverityToken with token of digiKam project given by Coverity SCAN.
-# $DKCoverityEmail with email adress to send SCAN result.
-#
-# See this url to see how to prepare your computer with Coverity SCAN tool:
-# https://scan.coverity.com/projects/digikam/builds/new
+# Before to run this script you must set $DKCoverityToken Shell variable:
+# with token of digiKam project given by Coverity SCAN web interface.
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
 
+if ! which cov-build ; then
+    echo "Coverity SCAN Toolkit is not installed"
+    echo "See https://scan.coverity.com/projects/digikam/builds/new for details."
+    exit 1
+else
+    echo "Check Coverity SCAN Toolkit passed..."
+fi
+
+
 cd ../..
-if [ ] ; then
+
 # Manage build sub-dir
 if [ -d "build" ]; then
     rm -rfv ./build
@@ -31,7 +36,7 @@ else
     echo "Unsupported platform..."
     exit -1
 fi
-fi
+
 # Get active git branches to create report description string
 ./gits branch | sed -e "s/*/#/g" | sed -e "s/On:/#On:/g" | grep "#" | sed -e "s/#On:/On:/g" | sed -e "s/#/BRANCH:/g" > ./build/git_branches.txt
 desc=$(<build/git_branches.txt)
@@ -41,8 +46,8 @@ cd ./build
 CPU_CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 echo "CPU cores detected to compile : $CPU_CORES."
 
-#cov-build --dir cov-int --tmpdir ~/tmp make -j$CPU_CORES
-#tar czvf myproject.tgz cov-int
+cov-build --dir cov-int --tmpdir ~/tmp make -j$CPU_CORES
+tar czvf myproject.tgz cov-int
 
 echo "-- SCAN Import description --"
 echo $desc
@@ -64,7 +69,7 @@ curl https://scan.coverity.com/builds?project=digiKam \
      --progress-bar \
      --verbose \
      --form token=$DKCoverityToken \
-     --form email=$DKCoverityEmail \
+     --form email=$digikam-devel@kde.org \
      --form file=@myproject.tgz \
      --form version=git \
      --form description="$desc" \
@@ -72,4 +77,4 @@ curl https://scan.coverity.com/builds?project=digiKam \
 
 echo "Done. Coverity Scan tarball 'myproject.tgz' is uploaded."
 echo "That took approximately $SECONDS seconds to upload."
-echo "File will be post processed for analyse. A mail notification will be send to digikam-bugs-null@kde.org."
+echo "File will be post processed for analyse. A mail notification will be send to digikam-devel@kde.org."
