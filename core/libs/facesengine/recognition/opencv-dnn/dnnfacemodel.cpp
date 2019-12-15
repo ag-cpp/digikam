410f1bb6c21386a24d991b5a49fc0093efb29dec
Gilles Caulier <caulier.gilles@gmail.com>
2019-12-15 07:55:40 -0500

    fix header

--- core/libs/facesengine/recognition/opencv-dnn/dnnfacemodel.cpp
+++ core/libs/facesengine/recognition/opencv-dnn/dnnfacemodel.cpp
@@ -6,7 +6,7 @@
  * Description : Face recognition using deep learning
  *
  * Copyright (C) 2017      by Yingjie Liu <yingjiewudi at gmail dot com>
- * Copyright (C) 2017-2019 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2017-2020 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2019      by Thanh Trung Dinh <dinhthanhtrung1996 at gmail dot com>
  *
  * This program is free software; you can redistribute it
--- core/libs/facesengine/recognition/opencv-face/opencv_face.h
+++ core/libs/facesengine/recognition/opencv-face/opencv_face.h
@@ -11,7 +11,7 @@
  * Copyright (C) 2010      by Alex Jironkin <alexjironkin at gmail dot com>
  * Copyright (c) 2011-2012 by Philipp Wagner <bytefish at gmx dot de>
  * Copyright (C) 2010-2013 by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
- * Copyright (C) 2010-2019 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2010-2020 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
