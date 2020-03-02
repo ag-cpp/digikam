601f734790e09b23ba50a9a9d6459791ffe9efe9
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2020-03-02 20:56:03 +0100

    [PATCH 26/81] move SlideShowBuilder to /core/libs/albums/engine/AlbumParser
    Signed-off-by: Gilles Caulier <caulier.gilles@gmail.com>

---
+++ core/libs/album/engine/albumparser.cpp
@@ -0,0 +1,178 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * https://www.digikam.org
+ *
+ * Date        : 2012-01-24
+ * Description : album parser progress indicator
+ *
+ * Copyright (C) 2012-2020 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C)      2020 by Minh Nghia Duong <minhnghiaduong997 at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "albumparser.h"
+
+// Qt includes
+
+#include <QTimer>
+#include <QApplication>
+#include <QIcon>
+
+// KDE includes
+
+#include <klocalizedstring.h>
+
+// Local includes
+
+#include "album.h"
+#include "albummanager.h"
+#include "iteminfoalbumsjob.h"
+#include "applicationsettings.h"
+
+namespace Digikam
+{
+
+class Q_DECL_HIDDEN AlbumParser::Private
+{
+public:
+
+    explicit Private()
+      : cancel(false),
+        album(nullptr)
+    {
+    }
+
+    bool         cancel;
+    ItemInfoList infoList;
+    Album*       album;
+    //QUrl         startFrom;           // Overrides the startFromCurrent flag read from settings.
+};
+
+AlbumParser::AlbumParser(const ItemInfoList& infoList)
+    : ProgressItem(nullptr, QLatin1String("SlideShowBuilder"), QString(), QString(), true, true),
+      d(new Private)
+{
+    d->infoList = infoList;
+
+    ProgressManager::addProgressItem(this);
+}
+
+AlbumParser::AlbumParser(Album* const album)
+    : ProgressItem(nullptr, QLatin1String("SlideShowBuilder"), QString(), QString(), true, true),
+      d(new Private)
+{
+    d->album = album;
+
+    ProgressManager::addProgressItem(this);
+}
+
+AlbumParser::~AlbumParser()
+{
+    delete d;
+}
+
+/* TODO: init from slideshow plugin
+void AlbumParser::setOverrideStartFrom(const ItemInfo& info)
+{
+   d->startFrom = info.fileUrl();
+}
+
+void AlbumParser::setAutoPlayEnabled(bool enable)
+{
+    d->autoPlayEnabled = enable;
+}
+*/
+
+void AlbumParser::run()
+{
+    QTimer::singleShot(500, this, SLOT(slotRun()));
+}
+
+void AlbumParser::slotRun()
+{
+    connect(this, SIGNAL(progressItemCanceled(ProgressItem*)),
+            this, SLOT(slotCancel()));
+
+    setLabel(i18n("Scanning albums"));
+    setThumbnail(QIcon::fromTheme(QLatin1String("digikam")));
+
+    if (d->album)
+    {
+        AlbumList albumList;
+        albumList.append(d->album);
+        AlbumIterator it(d->album);
+
+        while (it.current())
+        {
+            albumList.append(*it);
+            ++it;
+        }
+
+        ItemInfoAlbumsJob* const job = new ItemInfoAlbumsJob;
+
+        connect(job, SIGNAL(signalCompleted(ItemInfoList)),
+                this, SLOT(slotParseItemInfoList(ItemInfoList)));
+
+        job->allItemsFromAlbums(albumList);
+    }
+    else
+    {
+        slotParseItemInfoList(d->infoList);
+    }
+}
+
+void AlbumParser::slotParseItemInfoList(const ItemInfoList& list)
+{
+    setTotalItems(list.count());
+
+    int               i = 0;
+
+    //TODO: parse to slideshow settings when init Slideshow plugin
+/*
+    SlideShowSettings settings;
+    settings.readFromConfig();
+    settings.autoPlayEnabled = d->autoPlayEnabled;
+    settings.previewSettings = ApplicationSettings::instance()->getPreviewSettings();
+
+    if (d->startFrom.isValid())
+    {
+        settings.imageUrl = d->startFrom;
+    }
+*/
+    QList<QUrl> imagesList;
+
+    for (ItemInfoList::const_iterator it = list.constBegin();
+         !d->cancel && (it != list.constEnd()) ;
+         ++it)
+    {
+        ItemInfo info = *it;
+        imagesList.append(info.fileUrl());
+
+        advance(i++);
+        qApp->processEvents();
+    }
+
+    if (!d->cancel)
+        emit signalComplete(imagesList);
+
+    setComplete();
+}
+
+void AlbumParser::slotCancel()
+{
+    d->cancel = true;
+}
+
+} // namespace Digikam
