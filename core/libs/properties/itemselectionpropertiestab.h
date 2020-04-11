5f1aeaeae127afbb8c61666af71c3496f34310cb
Kartik Ramesh <kartikx2000@gmail.com>
2020-04-11 16:28:13 +0530

    Added Copyright Header

--- core/libs/properties/itemselectionpropertiestab.h
+++ core/libs/properties/itemselectionpropertiestab.h
@@ -1,3 +1,26 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * https://www.digikam.org
+ *
+ * Date        : 2020-04-11
+ * Description : A tab to display information about the current selection.
+ *
+ * Copyright (C) 2020 by Kartik Ramesh <kartikx2000 at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
 #ifndef DIGIKAM_ITEM_SELECTION_PROPERTIES_TAB_H
 #define DIGIKAM_ITEM_SELECTION_PROPERTIES_TAB_H
 
