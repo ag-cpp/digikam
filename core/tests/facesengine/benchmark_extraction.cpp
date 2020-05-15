4801e0655037f9670f4bbec3239f7c474a620899
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2020-05-15 21:14:10 +0200

    integrate face detection in test with Gui

--- core/tests/facesengine/benchmark_extraction.cpp
+++ core/tests/facesengine/benchmark_extraction.cpp
@@ -33,7 +33,10 @@
 #include <QListWidgetItem>
 #include <QDir>
 #include <QImage>
+#include <QElapsedTimer>
 #include <QLabel>
+#include <QPen>
+#include <QPainter>
 #include <QDebug>
 
 // lib digikam includes
@@ -47,16 +50,53 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(const QDir& directory, QWidget *parent = nullptr);
     ~MainWindow();
+
+private Q_SLOTS:
+
+    void slotDetectFaces(const QListWidgetItem* imageItem);
+
+private:
+    FaceDetector m_detector;
+    QLabel*      m_fullImage;
+
+    QListWidget* m_imageListView;
+
+    QHBoxLayout* m_processingLayout;
 };
 
 MainWindow::MainWindow(const QDir &directory, QWidget *parent)
     : QMainWindow(parent)
 {
+    setWindowTitle(QLatin1String("Face detection Test"));
+
     QWidget* const mainWidget = new QWidget;
 
-    QLabel* const fullImage = new QLabel;
-    fullImage->setScaledContents(false);
+    // Image erea
+    QWidget* const imageArea = new QWidget;
 
+    m_fullImage = new QLabel;
+    m_fullImage->setScaledContents(true);
+    m_fullImage->setText(QLatin1String("place holder"));
+
+    QWidget* const metaDataArea = new QWidget;
+
+    QSizePolicy spLeft(QSizePolicy::Preferred, QSizePolicy::Preferred);
+    spLeft.setVerticalPolicy(QSizePolicy::Expanding);
+    m_fullImage->setSizePolicy(spLeft);
+
+    QSizePolicy spRight(QSizePolicy::Preferred, QSizePolicy::Preferred);
+    spRight.setVerticalPolicy(QSizePolicy::Preferred);
+    metaDataArea->setSizePolicy(spRight);
+
+
+    m_processingLayout = new QHBoxLayout(imageArea);
+    m_processingLayout->addWidget(m_fullImage);
+    //m_processingLayout->addWidget(metaDataArea);
+
+
+
+
+    // Itemlist erea
     QScrollArea* const scrollArea = new QScrollArea;
     scrollArea->setWidgetResizable(true);
     scrollArea->setAlignment(Qt::AlignBottom);
@@ -64,24 +104,33 @@ MainWindow::MainWindow(const QDir &directory, QWidget *parent)
     scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
 
 
+
+
+
+
+
     QSizePolicy spHigh(QSizePolicy::Preferred, QSizePolicy::Preferred);
     spHigh.setVerticalPolicy(QSizePolicy::Expanding);
-    fullImage->setSizePolicy(spHigh);
+    imageArea->setSizePolicy(spHigh);
 
     QSizePolicy spLow(QSizePolicy::Preferred, QSizePolicy::Preferred);
     spLow.setVerticalPolicy(QSizePolicy::Fixed);
     scrollArea->setSizePolicy(spLow);
 
-    QVBoxLayout* const layout = new QVBoxLayout(mainWidget);
-    layout->addWidget(fullImage);
-    layout->addWidget(scrollArea);
+    QVBoxLayout* const mainLayout = new QVBoxLayout(mainWidget);
+    mainLayout->addWidget(imageArea);
+    mainLayout->addWidget(scrollArea);
 
-    QListWidget* imageListView = new QListWidget(mainWidget);
-    imageListView->setViewMode(QListView::IconMode);
-    imageListView->setIconSize(QSize(200, 150));
-    imageListView->setResizeMode(QListWidget::Adjust);
-    imageListView->setFlow(QListView::LeftToRight);
-    imageListView->setWrapping(false);
+    m_imageListView = new QListWidget(mainWidget);
+    m_imageListView->setViewMode(QListView::IconMode);
+    m_imageListView->setIconSize(QSize(200, 150));
+    m_imageListView->setResizeMode(QListWidget::Adjust);
+    m_imageListView->setFlow(QListView::LeftToRight);
+    m_imageListView->setWrapping(false);
+    m_imageListView->setDragEnabled(false);
+
+    connect(m_imageListView, &QListWidget::currentItemChanged,
+            this,            &MainWindow::slotDetectFaces);
 
     QStringList subjects = directory.entryList(QDir::Files | QDir::NoDotAndDotDot);
 
@@ -92,16 +141,69 @@ MainWindow::MainWindow(const QDir &directory, QWidget *parent)
         QString filePath = directory.filePath(*iter);
         QListWidgetItem* item = new QListWidgetItem(QIcon(filePath), filePath);
 
-        imageListView->addItem(item);
+        m_imageListView->addItem(item);
     }
 
-    scrollArea->setWidget(imageListView);
+    scrollArea->setWidget(m_imageListView);
 
     setCentralWidget(mainWidget);
 }
 
 MainWindow::~MainWindow()
 {
+}
+
+void MainWindow::slotDetectFaces(const QListWidgetItem* imageItem)
+{
+    QString imagePath = imageItem->text();
+    qDebug() << "Loading " << imagePath;
+    QImage img(imagePath);
+    QImage imgScaled(img.scaled(416, 416, Qt::KeepAspectRatio));
+
+    QElapsedTimer timer;
+    unsigned int elapsedDetection = 0;
+
+    timer.start();
+    QList<QRectF> faces = m_detector.detectFaces(imagePath);
+
+    elapsedDetection = timer.elapsed();
+
+    qDebug() << "(Input CV) Found " << faces.size() << " faces, in " << elapsedDetection << "ms";
+
+    if (faces.isEmpty())
+    {
+        qDebug() << "No faces found";
+        return;
+    }
+
+    qDebug() << "Coordinates of detected faces : ";
+
+    foreach (const QRectF& r, faces)
+    {
+        qDebug() << r;
+    }
+
+    QPainter painter(&imgScaled);
+    QPen paintPen(Qt::green);
+    paintPen.setWidth(1);
+    painter.setPen(paintPen);
+
+    foreach (const QRectF& rr, faces)
+    {
+        QLabel* const label = new QLabel;
+        label->setScaledContents(false);
+        QRect rectDraw      = FaceDetector::toAbsoluteRect(rr, imgScaled.size());
+        QRect r             = FaceDetector::toAbsoluteRect(rr, img.size());
+        QImage part         = img.copy(r);
+        label->setPixmap(QPixmap::fromImage(part.scaled(qMin(img.size().width(), 50),
+                                                        qMin(img.size().width(), 50),
+                                                        Qt::KeepAspectRatio)));
+        m_processingLayout->addWidget(label);
+        painter.drawRect(rectDraw);
+    }
+
+    // Only setPixmap after finishing drawing bboxes around detected faces
+    m_fullImage->setPixmap(QPixmap::fromImage(imgScaled));
 }
 
 
