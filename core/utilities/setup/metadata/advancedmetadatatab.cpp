b05f6da6708196b826da28586756af3e587f5438
Maik Qualmann <metzpinguin@gmail.com>
2020-05-18 22:32:09 +0200

    polish comment #2

--- core/utilities/setup/metadata/advancedmetadatatab.cpp
+++ core/utilities/setup/metadata/advancedmetadatatab.cpp
@@ -423,7 +423,7 @@ int AdvancedMetadataTab::getModelIndex()
     {
         // for 4 metadata types:
         // read operation  = 4*0 + (0, 1, 2, 3)
-        // write operation = 4*1 + (0, 1, 2, 3) = (4, 5 ,6, 7)
+        // write operation = 4*1 + (0, 1, 2, 3) = (4, 5, 6, 7)
 
         return (
                 (d->metadataTypeSize * d->operationType->currentIndex()) +
