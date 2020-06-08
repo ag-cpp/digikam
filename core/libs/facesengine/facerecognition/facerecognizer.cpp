9ecf0f0e61f27765739b9ea6751785d1980ea2c0
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2020-06-08 16:31:44 +0200

    update licence

--- core/libs/facesengine/facerecognition/facerecognizer.cpp
+++ core/libs/facesengine/facerecognition/facerecognizer.cpp
@@ -1,8 +1,30 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam
+ *
+ * Date        : 2020-05-22
+ * Description : Wrapper of face recognition using OpenFace
+ *
+ * Copyright (C) 2019 by Thanh Trung Dinh <dinhthanhtrung1996 at gmail dot com>
+ * Copyright (C) 2020 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2020 by Nghia Duong <minhnghiaduong997 at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
 #include "facerecognizer.h"
 
-
 // Qt includes
-
 #include <QElapsedTimer>
 #include <QJsonArray>
 #include <QJsonDocument>
