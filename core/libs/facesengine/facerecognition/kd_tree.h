46d572ef099496ee553f7e108f23cade1f3cd524
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2020-06-08 16:25:48 +0200

    finish Kd-tree implementation

---
+++ core/libs/facesengine/facerecognition/kd_tree.h
@@ -0,0 +1,59 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam
+ *
+ * Date        : 2019-06-08
+ * Description : Implementation of KD-Tree for vector space partitioning
+ *
+ * Copyright (C) 2020 by Nghia Duong <minhnghiaduong997 at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef KD_TREE_H
+#define KD_TREE_H
+
+#include "kd_node.h"
+
+namespace ClearPath
+{
+
+class KDTree
+{
+public:
+
+    explicit KDTree(int dim);
+    ~KDTree();
+
+    /**
+     *
+     * @param position
+     * @param sqRange
+     * @param maxNbNeighbors
+     * @return Map of N-nearest neighbors, sorted by distance
+     */
+    QMap<double, QVector<KDNode*> > getClosestNeighbors(const std::vector<double>& position, double sqRange, int maxNbNeighbors);
+
+    void update();
+
+    bool add(std::vector<double> position);
+
+private:
+
+    class Private;
+    Private* d;
+};
+
+}
+
+#endif // KD_TREE_H
