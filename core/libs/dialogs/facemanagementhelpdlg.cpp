1e0085f819951e8167370cb6b3e3495054938477
Kartik Ramesh <kartikx2000@gmail.com>
2020-06-25 17:48:09 +0530

    Added Face Detection page to Help Box

--- core/libs/dialogs/facemanagementhelpdlg.cpp
+++ core/libs/dialogs/facemanagementhelpdlg.cpp
@@ -126,15 +126,68 @@ FaceManagementHelpDialog::FaceManagementHelpDialog(QWidget* const parent)
     QWidget* faceDetectionPage = new QWidget();
 
     QGridLayout* faceDetectionPageLayout = new QGridLayout();
+
+    QPixmap detectFaceOptionImage = QPixmap(i18n("/home/kartik/detectFaces.png"));
+    detectFaceOptionImage = detectFaceOptionImage.scaled(QSize(350,150));
+
+    QLabel* detectFaceOptionLabel = new QLabel(faceDetectionPage);
+    detectFaceOptionLabel->setPixmap(detectFaceOptionImage);
+    faceDetectionPageLayout->addWidget(detectFaceOptionLabel, 0, 0, 1, 2);
+
+    QLabel* detectFaceInfoLabel = new QLabel(faceDetectionPage);
+    detectFaceInfoLabel->setWordWrap(true);
+    detectFaceInfoLabel->setText(i18n("To run Face Detection, select the "
+                                      "<u>Detect Faces</u> option in the Face Scan panel "
+                                      "and then click on <u>Scan Collection for Faces</u> <hr>"
+                                      "<b>Caution: </b> Running Face Detection for the first time "
+                                      "can be fairly time consuming. Make sure you use the "
+                                      "<u>Search In</u> tab to restrict the target of the Scan."));
+    faceDetectionPageLayout->addWidget(detectFaceInfoLabel, 0, 2, 1, 1);
+
+    QLabel* mainBodyLabel = new QLabel(faceDetectionPage);
+    mainBodyLabel->setWordWrap(true);
+    mainBodyLabel->setText(i18n("DigiKam offers two kinds of Face Detection Scans: "));
+    faceDetectionPageLayout->addWidget(mainBodyLabel, 1, 0, 1, 3);
+
+    QLabel* skipScannedHeaderLabel = new QLabel(faceDetectionPage);
+    skipScannedHeaderLabel->setText(i18n("<b>Skip Images Already Scanned"));
+    faceDetectionPageLayout->addWidget(skipScannedHeaderLabel, 2, 0, 1, 1);
+
+    QLabel* skipScannedLabel = new QLabel(faceDetectionPage);
+    skipScannedLabel->setWordWrap(true);
+    skipScannedLabel->setText(i18n("Choose this option if you haven't scanned yet or "
+                                   "if the last scan yielded good results but you would "
+                                   "like to scan new images."));
+    faceDetectionPageLayout->addWidget(skipScannedLabel, 2, 1, 1, 2);
+
+    QLabel* scanAgainHeaderLabel = new QLabel(faceDetectionPage);
+    scanAgainHeaderLabel->setText(i18n("<b>Scan Again and Merge"));
+    faceDetectionPageLayout->addWidget(scanAgainHeaderLabel, 3, 0, 1, 1);
+
+    QLabel* scanAgainLabel = new QLabel(faceDetectionPage);
+    scanAgainLabel->setWordWrap(true);
+    scanAgainLabel->setText(i18n("Choose this option if the results "
+                                 "of the previous scan weren't accurate. "
+                                 "This would rescan all images again, and is time consuming."));
+    faceDetectionPageLayout->addWidget(scanAgainLabel, 3, 1, 1, 2);
+
+    QLabel* resultsDetectedLabel = new QLabel(faceDetectionPage);
+    resultsDetectedLabel->setWordWrap(true);
+    resultsDetectedLabel->setText(i18n("After the Face Scan is complete, you should see a new "
+                                       "tag <u>Unknown</u> where the results of the Face Scan "
+                                       "will appear. You may now manually identify a few of these "
+                                       "faces and then proceed for Face Recognition."));
+    faceDetectionPageLayout->addWidget(resultsDetectedLabel, 4, 0, 1, 3);
+
     faceDetectionPage->setLayout(faceDetectionPageLayout);
+
     tabWidget->addTab(faceDetectionPage, i18n("Face Detection"));
 
+    // --- Face Recognition Page --------------------------------------------------------
 
-    // QLabel* testImage = new QLabel(infoPage);
-    // QPixmap image = QPixmap(i18n("/home/kartik/test.png"));
-    // image = image.scaled(QSize(400,400));
-    // testImage->setPixmap(image);
-    // pageLayout->addWidget(testImage, 2, 0, 1, 2);
+
+    // ----------------------------------------------------------------------------------
+
     mainLayout->addWidget(tabWidget, 0, 0);
     setLayout(mainLayout);
     setVisible(true);
