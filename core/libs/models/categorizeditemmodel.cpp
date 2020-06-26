50a632a252ed6a4aa317746e72898418c7336815
Maik Qualmann <metzpinguin@gmail.com>
2020-06-26 23:30:08 +0200

    use QString::fromUtf8()

--- core/libs/models/categorizeditemmodel.cpp
+++ core/libs/models/categorizeditemmodel.cpp
@@ -77,7 +77,7 @@ static QString adjustedActionText(const QAction* const action)
 {
     QString text = action->text();
     text.remove(QLatin1Char('&'));
-    text.remove(QRegExp(QLatin1String("[ \u00A0]?(\\.\\.\\.|…)$")));
+    text.remove(QRegExp(QString::fromUtf8("[ \u00A0]?(\\.\\.\\.|…)$")));
     int slashPos = -1;
 
     while ((slashPos = text.indexOf(QLatin1Char('/'), slashPos + 1)) != -1)
