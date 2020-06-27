18995f917c196d2e21d3cc7d608edfda7cb555e5
Kartik Ramesh <kartikx2000@gmail.com>
2020-06-27 12:12:55 +0530

    Polish Formatting of Face Detection Page

--- core/libs/dialogs/facemanagementhelpdlg.cpp
+++ core/libs/dialogs/facemanagementhelpdlg.cpp
@@ -118,17 +118,16 @@ FaceManagementHelpDialog::FaceManagementHelpDialog(QWidget* const parent)
 
     // --- Face Detection Page ----------------------------------------------------------
 
-    QWidget* faceDetectionPage = new QWidget();
-
+    QWidget*     faceDetectionPage       = new QWidget();
     QGridLayout* faceDetectionPageLayout = new QGridLayout();
 
-    QPixmap detectFaceOptionImage = QPixmap(i18n("/home/kartik/detectFaces.png"));
-    detectFaceOptionImage = detectFaceOptionImage.scaled(QSize(350,150));
+    QPixmap detectFaceOptionImage  = QPixmap(i18n("/home/kartik/detectFaces2.png"));
+    detectFaceOptionImage = detectFaceOptionImage.scaled(QSize(350,170));
 
-    QLabel* detectFaceOptionLabel = new QLabel(faceDetectionPage);
+    QLabel* detectFaceOptionLabel  = new QLabel(faceDetectionPage);
     detectFaceOptionLabel->setPixmap(detectFaceOptionImage);
 
-    QLabel* detectFaceInfoLabel = new QLabel(faceDetectionPage);
+    QLabel* detectFaceInfoLabel    = new QLabel(faceDetectionPage);
     detectFaceInfoLabel->setWordWrap(true);
     detectFaceInfoLabel->setText(i18n("To run Face Detection, select the "
                                       "<u>Detect Faces</u> option in the Face Scan panel "
@@ -136,42 +135,50 @@ FaceManagementHelpDialog::FaceManagementHelpDialog(QWidget* const parent)
                                       "<b>Caution: </b> Running Face Detection for the first time "
                                       "can be fairly time consuming. Make sure you use the "
                                       "<u>Search In</u> tab to restrict the target of the Scan."));
-    faceDetectionPageLayout->addWidget(detectFaceInfoLabel, 0, 2, 1, 1);
 
-    QLabel* mainBodyLabel = new QLabel(faceDetectionPage);
-    mainBodyLabel->setWordWrap(true);
-    mainBodyLabel->setText(i18n("DigiKam offers two kinds of Face Detection Scans: "));
-    faceDetectionPageLayout->addWidget(mainBodyLabel, 1, 0, 1, 3);
+    QLabel* detectIntroLabel       = new QLabel(faceDetectionPage);
+    detectIntroLabel->setWordWrap(true);
+    detectIntroLabel->setText(i18n("DigiKam offers two kinds of Face Detection Scans: "));
 
     QLabel* skipScannedHeaderLabel = new QLabel(faceDetectionPage);
+    skipScannedHeaderLabel->setWordWrap(true);
+    skipScannedHeaderLabel->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
     skipScannedHeaderLabel->setText(i18n("<b>Skip Images Already Scanned"));
-    faceDetectionPageLayout->addWidget(skipScannedHeaderLabel, 2, 0, 1, 1);
 
-    QLabel* skipScannedLabel = new QLabel(faceDetectionPage);
+    QLabel* skipScannedLabel       = new QLabel(faceDetectionPage);
     skipScannedLabel->setWordWrap(true);
     skipScannedLabel->setText(i18n("Choose this option if you haven't scanned yet or "
                                    "if the last scan yielded good results but you would "
                                    "like to scan new images."));
-    faceDetectionPageLayout->addWidget(skipScannedLabel, 2, 1, 1, 2);
 
-    QLabel* scanAgainHeaderLabel = new QLabel(faceDetectionPage);
+    QLabel* scanAgainHeaderLabel   = new QLabel(faceDetectionPage);
+    scanAgainHeaderLabel->setWordWrap(true);
+    scanAgainHeaderLabel->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
     scanAgainHeaderLabel->setText(i18n("<b>Scan Again and Merge"));
-    faceDetectionPageLayout->addWidget(scanAgainHeaderLabel, 3, 0, 1, 1);
 
-    QLabel* scanAgainLabel = new QLabel(faceDetectionPage);
+    QLabel* scanAgainLabel         = new QLabel(faceDetectionPage);
     scanAgainLabel->setWordWrap(true);
     scanAgainLabel->setText(i18n("Choose this option if the results "
                                  "of the previous scan weren't accurate. "
                                  "This would rescan all images again, and is time consuming."));
-    faceDetectionPageLayout->addWidget(scanAgainLabel, 3, 1, 1, 2);
 
-    QLabel* resultsDetectedLabel = new QLabel(faceDetectionPage);
+    QLabel* resultsDetectedLabel   = new QLabel(faceDetectionPage);
     resultsDetectedLabel->setWordWrap(true);
     resultsDetectedLabel->setText(i18n("After the Face Scan is complete, you should see a new "
                                        "tag <u>Unknown</u> where the results of the Face Scan "
                                        "will appear. You may now manually identify a few of these "
                                        "faces and then proceed for Face Recognition."));
-    faceDetectionPageLayout->addWidget(resultsDetectedLabel, 4, 0, 1, 3);
+
+    faceDetectionPageLayout->addWidget(detectFaceOptionLabel,       0, 0, 1, 2);
+    faceDetectionPageLayout->addWidget(detectFaceInfoLabel,         0, 2, 1, 2);
+    faceDetectionPageLayout->addWidget(detectIntroLabel,            1, 0, 1, 4);
+    faceDetectionPageLayout->addWidget(skipScannedHeaderLabel,      2, 0, 1, 2);
+    faceDetectionPageLayout->addWidget(skipScannedLabel,            2, 2, 1, 2);
+    faceDetectionPageLayout->addWidget(scanAgainHeaderLabel,        3, 0, 1, 2);
+    faceDetectionPageLayout->addWidget(scanAgainLabel,              3, 2, 1, 2);
+    faceDetectionPageLayout->addWidget(resultsDetectedLabel,        4, 0, 1, 4);
+    faceDetectionPageLayout->setRowStretch(2, 1);
+    faceDetectionPageLayout->setRowStretch(3, 1);
 
     faceDetectionPage->setLayout(faceDetectionPageLayout);
 
