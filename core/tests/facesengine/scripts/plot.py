84dd76df9305d3c4d595ce1d1c79182f59ec7937
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2020-07-05 21:19:31 +0200

    test classifier with umap

--- core/tests/facesengine/scripts/plot.py
+++ core/tests/facesengine/scripts/plot.py
@@ -1,6 +1,8 @@
 import numpy as np
-from sklearn.datasets import load_iris, load_digits
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
@@ -9,7 +11,7 @@ import json
 
 sns.set(style='white', context='notebook', rc={'figure.figsize':(14,10)})
 
-with open('yalefaces.json') as f:
+with open('extendedB.json') as f:
     jsonData = json.load(f)
 
 faceEmbeddings = []
@@ -17,21 +19,43 @@ labels = []
 for json in jsonData:
     faceEmbeddings.append(json['faceembedding'])
     labels.append(json['id'])
+'''
+for i in range(1, 50):
+    reducer = umap.UMAP(n_neighbors=16, metric='euclidean', min_dist=0.1, n_components=i*2, transform_seed=42)
 
-reducer = umap.UMAP(a=None, angular_rp_forest=False, b=None, force_approximation_algorithm=False, init='spectral', learning_rate=1.0,
-                    local_connectivity=1.0, low_memory=False, metric='euclidean', metric_kwds=None, min_dist=0.01, n_components=2, n_epochs=None,
-                    n_neighbors=50, negative_sample_rate=5, output_metric='euclidean', output_metric_kwds=None, random_state=42, repulsion_strength=1.0,
-                    set_op_mix_ratio=1.0, spread=1.0, target_metric='categorical', target_metric_kwds=None, target_n_neighbors=-1, target_weight=0.5,
-                    transform_queue_size=4.0, transform_seed=42, unique=False, verbose=False)
+    reducer.fit(faceEmbeddings, y=labels)
 
-reducer.fit(faceEmbeddings)
+    reducedEmbedding = reducer.transform(faceEmbeddings)
 
+    X_train, X_test, y_train, y_test = train_test_split(reducedEmbedding,
+                                                        labels,
+                                                        stratify=labels,
+                                                        random_state=42)
+
+    svc = SVC().fit(X_train, y_train)
+    knn = KNeighborsClassifier().fit(X_train, y_train)
+
+    print(i*2)
+    print(svc.score(X_test, y_test), knn.score(X_test, y_test))
+'''
+
+reducer = umap.UMAP(n_neighbors=15, metric='euclidean', min_dist=0.1, n_components=2, transform_seed=42)
+reducer.fit(faceEmbeddings, y=labels)
 reducedEmbedding = reducer.transform(faceEmbeddings)
-print(reducedEmbedding)
+
+X_train, X_test, y_train, y_test = train_test_split(reducedEmbedding,
+                                                    labels,
+                                                    stratify=labels,
+                                                    random_state=42)
+svc = SVC().fit(X_train, y_train)
+knn = KNeighborsClassifier().fit(X_train, y_train)
+
+print(svc.score(X_test, y_test), knn.score(X_test, y_test))
+
 
 plt.scatter(reducedEmbedding[:, 0], reducedEmbedding[:, 1], c=labels, cmap='Spectral', s=5)
 plt.gca().set_aspect('equal', 'datalim')
 plt.colorbar(boundaries=np.arange(11)-0.5).set_ticks(np.arange(10))
-plt.title('UMAP projection of the Digits dataset', fontsize=24);
+plt.title('UMAP projection of the faceembedding', fontsize=24);
 plt.show()
 
