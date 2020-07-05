68b5d35fcb29cc655a0d41e19aa5d6adb217d68d
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2020-07-05 21:59:29 +0200

    test classifier with supervised umap

--- core/tests/facesengine/scripts/plot.py
+++ core/tests/facesengine/scripts/plot.py
@@ -19,43 +19,48 @@ labels = []
 for json in jsonData:
     faceEmbeddings.append(json['faceembedding'])
     labels.append(json['id'])
-'''
+
 for i in range(1, 50):
-    reducer = umap.UMAP(n_neighbors=16, metric='euclidean', min_dist=0.1, n_components=i*2, transform_seed=42)
-
-    reducer.fit(faceEmbeddings, y=labels)
-
-    reducedEmbedding = reducer.transform(faceEmbeddings)
-
-    X_train, X_test, y_train, y_test = train_test_split(reducedEmbedding,
+    X_train, X_test, y_train, y_test = train_test_split(faceEmbeddings,
                                                         labels,
                                                         stratify=labels,
                                                         random_state=42)
 
-    svc = SVC().fit(X_train, y_train)
-    knn = KNeighborsClassifier().fit(X_train, y_train)
+    reducer = umap.UMAP(n_neighbors=15, metric='euclidean', min_dist=0.1, n_components=i*2, transform_seed=42)
+    reducer.fit(X_train, y=y_train)
+
+    reduced_X_train = reducer.transform(X_train)
+    reduced_X_test  = reducer.transform(X_test)
+
+    svc = SVC().fit(reduced_X_train, y_train)
+    knn = KNeighborsClassifier().fit(reduced_X_train, y_train)
 
     print(i*2)
-    print(svc.score(X_test, y_test), knn.score(X_test, y_test))
+    print(svc.score(reduced_X_test, y_test), knn.score(reduced_X_test, y_test))
+
 '''
-
-reducer = umap.UMAP(n_neighbors=15, metric='euclidean', min_dist=0.1, n_components=2, transform_seed=42)
-reducer.fit(faceEmbeddings, y=labels)
-reducedEmbedding = reducer.transform(faceEmbeddings)
-
-X_train, X_test, y_train, y_test = train_test_split(reducedEmbedding,
+# split face embedding to train and test sets
+X_train, X_test, y_train, y_test = train_test_split(faceEmbeddings,
                                                     labels,
                                                     stratify=labels,
                                                     random_state=42)
-svc = SVC().fit(X_train, y_train)
-knn = KNeighborsClassifier().fit(X_train, y_train)
 
-print(svc.score(X_test, y_test), knn.score(X_test, y_test))
+reducer = umap.UMAP(n_neighbors=15, metric='euclidean', min_dist=0.1, n_components=28, transform_seed=42)
+reducer.fit(X_train, y=y_train)
 
+reduced_X_train = reducer.transform(X_train)
+reduced_X_test  = reducer.transform(X_test)
+
+svc = SVC().fit(reduced_X_train, y_train)
+knn = KNeighborsClassifier().fit(reduced_X_train, y_train)
+
+print(svc.score(reduced_X_test, y_test), knn.score(reduced_X_test, y_test))
+
+reducedEmbedding = reducer.transform(faceEmbeddings)
 
 plt.scatter(reducedEmbedding[:, 0], reducedEmbedding[:, 1], c=labels, cmap='Spectral', s=5)
 plt.gca().set_aspect('equal', 'datalim')
 plt.colorbar(boundaries=np.arange(11)-0.5).set_ticks(np.arange(10))
-plt.title('UMAP projection of the faceembedding', fontsize=24);
+plt.title('UMAP projection of the face embedding', fontsize=24);
 plt.show()
-
+'''
