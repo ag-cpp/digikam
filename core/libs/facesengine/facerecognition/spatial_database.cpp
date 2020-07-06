e0162aa6dbed3172e5e179fa1a788715d06c3c2c
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2020-07-06 16:06:18 +0200

    add spatial database for KD-Tree storage

---
+++ core/libs/facesengine/facerecognition/spatial_database.cpp
@@ -0,0 +1,33 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam
+ *
+ * Date        : 2019-07-06
+ * Description : Implementation of spatial database for KD-Tree storage
+ *
+ * Copyright (C) 2020 by Nghia Duong <minhnghiaduong997 at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "spatial_database.h"
+
+namespace RecognitionTest
+{
+
+SpatialDatabase::SpatialDatabase()
+{
+
+}
+
+}
---
+++ core/libs/facesengine/facerecognition/spatial_database.h
@@ -0,0 +1,37 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam
+ *
+ * Date        : 2019-07-06
+ * Description : Implementation of spatial database for KD-Tree storage
+ *
+ * Copyright (C) 2020 by Nghia Duong <minhnghiaduong997 at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef SPATIAL_DATABASE_H
+#define SPATIAL_DATABASE_H
+
+namespace RecognitionTest
+{
+
+
+class SpatialDatabase
+{
+public:
+    SpatialDatabase();
+};
+}
+
+#endif // SPATIAL_DATABASE_H
