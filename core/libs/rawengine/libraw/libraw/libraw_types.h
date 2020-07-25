382f974740485ce6018239ff85bdfb0ea720fee1
Gilles Caulier <caulier.gilles@gmail.com>
2020-07-25 05:32:51 +0200

    Update internal libraw with final 0.20 stable release

--- core/libs/rawengine/libraw/libraw/libraw_types.h
+++ core/libs/rawengine/libraw/libraw/libraw_types.h
@@ -99,7 +99,7 @@ typedef unsigned long long UINT64;
   typedef unsigned char uchar;
   typedef unsigned short ushort;
 
-/*#ifdef LIBRAW_WIN32_DLLDEFS
+#ifdef LIBRAW_WIN32_DLLDEFS
 #ifdef LIBRAW_NODLL
 #define DllDef
 #else
@@ -109,9 +109,9 @@ typedef unsigned long long UINT64;
 #define DllDef __declspec(dllimport)
 #endif
 #endif
-#else*/
+#else
 #define DllDef
-//#endif
+#endif
 
   typedef struct
   {
