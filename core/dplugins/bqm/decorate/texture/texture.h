2c146189089ee2c511956ab3fadd4a19fd5d7730
Gilles Caulier <caulier.gilles@gmail.com>
2020-08-13 18:48:24 +0200

    Batch Queue Manager: add new tool to apply texture over images

---
+++ core/dplugins/bqm/decorate/texture/texture.h
@@ -0,0 +1,68 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * https://www.digikam.org
+ *
+ * Date        : 2020-08-13
+ * Description : batch tool to add texture.
+ *
+ * Copyright (C) 2020 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef DIGIKAM_BQM_TEXTURE_H
+#define DIGIKAM_BQM_TEXTURE_H
+
+// Local includes
+
+#include "batchtool.h"
+#include "texturesettings.h"
+
+using namespace Digikam;
+
+namespace DigikamBqmTexturePlugin
+{
+
+class Texture : public BatchTool
+{
+    Q_OBJECT
+
+public:
+
+    explicit Texture(QObject* const parent = nullptr);
+    ~Texture();
+
+    BatchToolSettings defaultSettings();
+
+    BatchTool* clone(QObject* const parent=nullptr) const { return new Texture(parent); };
+
+    void registerSettingsWidget();
+
+private Q_SLOTS:
+
+    void slotSettingsChanged();
+    void slotAssignSettings2Widget();
+
+private:
+
+    bool toolOperations();
+
+private:
+
+    TextureSettings* m_settingsView;
+};
+
+} // namespace DigikamBqmTexturePlugin
+
+#endif // DIGIKAM_BQM_TEXTURE_H
