1dfa3addc17a5d3ba99088cbb98a3444c416248b
Kartik Ramesh <kartikx2000@gmail.com>
2020-08-20 18:41:08 +0530

    Ignored should not be shown on the Images.

--- core/app/items/delegate/itemdelegate.cpp
+++ core/app/items/delegate/itemdelegate.cpp
@@ -344,6 +344,7 @@ void ItemDelegate::paint(QPainter* p, const QStyleOptionViewItem& option, const 
     {
         QStringList tagsList = AlbumManager::instance()->tagNames(info.tagIds());
         tagsList.sort();
+        tagsList.removeAll(QStringLiteral("Ignored"));
         QString tags         = tagsList.join(QLatin1String(", "));
         drawTags(p, d->tagRect, tags, isSelected);
     }
