6b3a0c2f059a2ec911d33bf44fb6b8198f6c935e
Gilles Caulier <caulier.gilles@gmail.com>
2020-09-02 06:10:53 -0400

    i18n fixes: EXIF => Exif

--- core/dplugins/generic/metadata/metadataedit/exif/exifdevice.cpp
+++ core/dplugins/generic/metadata/metadataedit/exif/exifdevice.cpp
@@ -322,7 +322,7 @@ EXIFDevice::EXIFDevice(QWidget* const parent)
     // --------------------------------------------------------
 
     QLabel* const note = new QLabel(i18n("<b>Note: "
-                 "<b><a href='https://en.wikipedia.org/wiki/EXIF'>EXIF</a></b> "
+                 "<b><a href='https://en.wikipedia.org/wiki/EXIF'>Exif</a></b> "
                  "text tags marked by (*) only support printable "
                  "<b><a href='https://en.wikipedia.org/wiki/Ascii'>ASCII</a></b> "
                  "characters.</b>"), this);
