80f6d6a4baede7d6d216678ed871c4f407b6afdc
Gilles Caulier <caulier.gilles@gmail.com>
2020-09-13 18:51:16 +0200

    cppcheck++

--- core/utilities/imageeditor/widgets/imageregionitem.h
+++ core/utilities/imageeditor/widgets/imageregionitem.h
@@ -9,7 +9,6 @@
  * Copyright (C) 2013-2014 by Yiou Wang <geow812 at gmail dot com>
  * Copyright (C) 2013-2020 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
- *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
@@ -62,7 +61,7 @@ public:
 private:
 
     class Private;
-    Private* const d_ptr;
+    Private* const dd;
 };
 
 } // namespace Digikam
