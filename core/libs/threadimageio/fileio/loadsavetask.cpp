a73fae62da76f2db287a6bf5a59b287896ccf65a
Maik Qualmann <metzpinguin@gmail.com>
2020-10-01 12:26:54 +0200

    Revert "remove only one listener"

--- core/libs/threadimageio/fileio/loadsavetask.cpp
+++ core/libs/threadimageio/fileio/loadsavetask.cpp
@@ -455,7 +455,7 @@ void SharedLoadingTask::addListener(LoadingProcessListener* const listener)
 
 void SharedLoadingTask::removeListener(LoadingProcessListener* const listener)
 {
-    m_listeners.removeOne(listener);
+    m_listeners.removeAll(listener);
 }
 
 void SharedLoadingTask::notifyNewLoadingProcess(LoadingProcess* const process,
