b201a18ae8722e5ea4524c643c85e5d5eac317ac
Gilles Caulier <caulier.gilles@gmail.com>
2020-10-27 05:01:21 +0100

    cppcheck++

--- core/libs/properties/history/itempropertieshistorytab.h
+++ core/libs/properties/history/itempropertieshistorytab.h
@@ -47,22 +47,14 @@ public:
 
     explicit RemoveFilterAction(const QString& label,
                                 const QModelIndex& index,
-                                QObject* const parent = 0);
+                                QObject* const parent = nullptr);
     ~RemoveFilterAction()
-    {
-    }
 
-    void setIndex(QModelIndex& index)
-    {
-        m_index = index;
-    }
+    void setIndex(const QModelIndex& index)
 
 public Q_SLOTS:
 
     void triggerSlot()
-    {
-        emit actionTriggered(m_index);
-    }
 
 Q_SIGNALS:
 
