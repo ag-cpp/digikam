e2cef397e4426343a91dafeff9b9cbad279d7a36
Gilles Caulier <caulier.gilles@gmail.com>
2021-01-04 08:44:15 -0500

    First step to get online release version from digikam.org web site CCBUGS: 426997

---
+++ core/libs/widgets/mainview/onlineversionchecker.cpp
@@ -0,0 +1,121 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * https://www.digikam.org
+ *
+ * Date        : 2021-01-04
+ * Description : Online version checker
+ *
+ * Copyright (C) 2021 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "onlineversionchecker.h"
+
+// Qt includes
+
+#include <QFile>
+#include <QTextStream>
+#include <QDesktopServices>
+
+// Local includes
+
+#include "digikam_version.h"
+#include "digikam_debug.h"
+
+namespace Digikam
+{
+
+const QUrl OnlineVersionChecker::versionCheckUrl("https://invent.kde.org/websites/digikam-org/-/raw/master/data/release.yml");
+
+OnlineVersionChecker::OnlineVersionChecker(QObject* const parent)
+    : QObject   (parent),
+      curRequest(nullptr)
+{
+    connect(&manager, SIGNAL(finished(QNetworkReply*)),
+            this, SLOT(downloadFinished(QNetworkReply*)));
+}
+
+OnlineVersionChecker::~OnlineVersionChecker()
+{
+}
+
+void OnlineVersionChecker::checkForNewVersion()
+{
+    QNetworkRequest request(versionCheckUrl);
+    curRequest = manager.get(request);
+
+    if (curRequest->error())
+    {
+        emit signalNewVersionCheckError(curRequest->error());
+    }
+}
+
+void OnlineVersionChecker::cancelCheck()
+{
+    if (curRequest)
+    {
+        curRequest->abort();
+    }
+}
+
+void OnlineVersionChecker::downloadFinished(QNetworkReply* reply)
+{
+    // mark for deletion
+
+    reply->deleteLater();
+    curRequest = nullptr;
+
+    QNetworkReply::NetworkError error = reply->error();
+
+    if (error)
+    {
+        emit signalNewVersionCheckError(error);
+        return;
+    }
+
+    QString data = reply->readAll();
+
+    if (data.isEmpty()))
+    {
+        emit signalNewVersionCheckError(QNetworkReply::ProtocolFailure);
+        return;
+    }
+
+    qDebug() << data;
+
+    QString tag         = QLatin1String("version: ");
+    int start           = data.indexOf(tag) + tag.size();
+    QString rightVer    = data.mid(start);
+    int end             = rightVer.indexOf(QLatin1Char('\n'));
+    QString readVersion = data.mid(0, end);
+
+    qDebug() << readVersion;
+
+    if (readVersion > QLatin1String(digikam_version_short);)
+    {
+        emit newVersionAvailable(readVersion);
+    }
+    else
+    {
+        emit newVersionCheckError(QNetworkReply::NoError);
+    }
+}
+
+void OnlineVersionChecker::error(QNetworkReply::NetworkError code)
+{
+    emit newVersionCheckError(code);
+}
+
+} // namespace Digikam
---
+++ core/libs/widgets/mainview/onlineversionchecker.h
@@ -0,0 +1,70 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * https://www.digikam.org
+ *
+ * Date        : 2021-01-04
+ * Description : Online version checker
+ *
+ * Copyright (C) 2021 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef DIGIKAM_ONLINE_VERSION_CHECKER_H
+#define DIGIKAM_ONLINE_VERSION_CHECKER_H
+
+// Qt includes
+
+#include <QObject>
+#include <QUrl>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QDateTime>
+
+namespace Digikam
+{
+
+class OnlineVersionChecker : public QObject
+{
+    Q_OBJECT
+
+public:
+
+    explicit OnlineVersionChecker(QObject* const parent);
+    ~OnlineVersionChecker();
+
+    void checkForNewVersion();
+    void cancelCheck();
+
+private:
+
+    static const QUrl       versionCheckUrl;
+
+    QNetworkAccessManager   manager;
+    QNetworkReply*          curRequest;
+
+private Q_SLOTS:
+
+    void downloadFinished(QNetworkReply* reply);
+    void error(QNetworkReply::NetworkError code);
+
+Q_SIGNALS:
+
+    void signalNewVersionAvailable(const QString& newTag);
+    void signalNewVersionCheckError(QNetworkReply::NetworkError error);
+};
+
+} // namespace Digikam
+
+#endif // DIGIKAM_ONLINE_VERSION_CHECKER_H
