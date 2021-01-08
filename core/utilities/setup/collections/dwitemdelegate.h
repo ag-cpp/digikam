5d158f1dddbdb08df12fa437ed57b3fbd2a40f7f
Gilles Caulier <caulier.gilles@gmail.com>
2021-01-08 11:37:48 +0100

    fix headers

--- core/utilities/setup/collections/dwitemdelegate.h
+++ core/utilities/setup/collections/dwitemdelegate.h
@@ -6,7 +6,7 @@
  * Date        : 2007-11-15
  * Description : widget item delegate for setup collection view
  *
- * Copyright (C) 2015-2020 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2015-2021 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2007-2008 by Rafael Fernández López <ereslibre at kde dot org>
  * Copyright (C) 2008      by Kevin Ottens <ervin at kde dot org>
  *
