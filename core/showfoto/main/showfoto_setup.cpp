547382cc7845c2e499eb62b9a712eb1f592ba55e
Gilles Caulier <caulier.gilles@gmail.com>
2021-01-08 11:37:17 +0100

    fix headers

--- core/showfoto/main/showfoto_setup.cpp
+++ core/showfoto/main/showfoto_setup.cpp
@@ -6,7 +6,7 @@
  * Date        : 2004-11-22
  * Description : stand alone digiKam image editor - Internal setup
  *
- * Copyright (C) 2004-2020 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2004-2021 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
