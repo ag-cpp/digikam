83e9360100394041a40a0577c307e48558952623
Gilles Caulier <caulier.gilles@gmail.com>
2021-01-08 11:36:18 +0100

    fix headers

--- core/cmake/modules/MacroOpenCV.cmake
+++ core/cmake/modules/MacroOpenCV.cmake
@@ -6,7 +6,7 @@
 # which try to find OpenCV version 3.3.0
 # with internal components "core", "highgui", "objdetect", and "contrib".
 #
-# Copyright (c) 2010-2020 by Gilles Caulier, <caulier dot gilles at gmail dot com>
+# Copyright (c) 2010-2021 by Gilles Caulier, <caulier dot gilles at gmail dot com>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
--- core/cmake/modules/MacroOpenMP.cmake
+++ core/cmake/modules/MacroOpenMP.cmake
@@ -7,7 +7,7 @@
 # OPENMP_FOUND     - openMP detection flag
 # OpenMP_CXX_FLAGS - flags to add to the CXX compiler for OpenMP support
 #
-# Copyright (c) 2010-2020 by Gilles Caulier, <caulier dot gilles at gmail dot com>
+# Copyright (c) 2010-2021 by Gilles Caulier, <caulier dot gilles at gmail dot com>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
