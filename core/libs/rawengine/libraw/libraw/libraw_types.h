67933bf61f0ec43465429d00a66cb6fb30eb2656
Gilles Caulier <caulier.gilles@gmail.com>
2021-01-30 10:29:26 +0100

    apply usual patch to link properly under Windows without export symbol rules

--- core/libs/rawengine/libraw/libraw/libraw_types.h
+++ core/libs/rawengine/libraw/libraw/libraw_types.h
@@ -104,7 +104,7 @@ typedef unsigned long long UINT64;
   typedef unsigned short ushort;
 
 #ifdef LIBRAW_WIN32_DLLDEFS
-#ifdef LIBRAW_NODLL
+/*#ifdef LIBRAW_NODLL
 #define DllDef
 #else
 #ifdef LIBRAW_BUILDLIB
@@ -113,9 +113,9 @@ typedef unsigned long long UINT64;
 #define DllDef __declspec(dllimport)
 #endif
 #endif
-#else
+#else*/
 #define DllDef
-#endif
+//#endif
 
   typedef struct
   {
