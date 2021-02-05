61b9548db0ee73640efc96efee1a9abf00da768d
Maik Qualmann <metzpinguin@gmail.com>
2021-02-05 08:11:13 +0100

    modify the user agent CCBUGS: 432480

--- core/libs/dialogs/webbrowserdlg.cpp
+++ core/libs/dialogs/webbrowserdlg.cpp
@@ -102,7 +102,7 @@ WebBrowserDlg::WebBrowserDlg(const QUrl& url, QWidget* const parent, bool hideDe
     d->browser = new QWebEngineView(this);
     d->browser->page()->profile()->cookieStore()->deleteAllCookies();
     QString userAgent = d->browser->page()->profile()->httpUserAgent();
-    userAgent.replace(QLatin1String("Chrome/"), QLatin1String("Chromium/"));
+    userAgent.replace(QLatin1String("Chrome/"), QLatin1String("QWebEngine/"));
     d->browser->page()->profile()->setHttpUserAgent(userAgent);
 
 #else
