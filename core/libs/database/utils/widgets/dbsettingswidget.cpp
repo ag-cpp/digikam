28a8524ae4c61d068800470cffd5172fc4925f08
Maik Qualmann <metzpinguin@gmail.com>
2021-02-13 20:53:34 +0100

    MariaDB 10.5 version is up to date

--- core/libs/database/utils/widgets/dbsettingswidget.cpp
+++ core/libs/database/utils/widgets/dbsettingswidget.cpp
@@ -157,8 +157,8 @@ void DatabaseSettingsWidget::setupMainArea()
 
 #ifdef Q_OS_WIN
 
-    d->dbBinariesWidget->addDirectory(QLatin1String("C:/Program Files/MariaDB 10.3/bin"));
-    d->dbBinariesWidget->addDirectory(QLatin1String("C:/Program Files (x86/MariaDB 10.3/bin"));
+    d->dbBinariesWidget->addDirectory(QLatin1String("C:/Program Files/MariaDB 10.5/bin"));
+    d->dbBinariesWidget->addDirectory(QLatin1String("C:/Program Files (x86/MariaDB 10.5/bin"));
 
 #endif
 
