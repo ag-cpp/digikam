274add9df55b2eb362bda7751d96eb8eae74455e
Gilles Caulier <caulier.gilles@gmail.com>
2021-02-18 05:01:02 +0100

    polish

--- core/libs/onlineversion/onlineversiondlg.cpp
+++ core/libs/onlineversion/onlineversiondlg.cpp
@@ -3,10 +3,10 @@
  * This file is a part of digiKam project
  * https://www.digikam.org
  *
- * Date        : 2021-01-05
+ * Date        : 2020-12-31
  * Description : Online version dialog.
  *
- * Copyright (C) 2021 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2020-2021 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
--- core/libs/onlineversion/onlineversiondwnl.cpp
+++ core/libs/onlineversion/onlineversiondwnl.cpp
@@ -3,11 +3,11 @@
  * This file is a part of digiKam project
  * https://www.digikam.org
  *
- * Date        : 2021-01-05
+ * Date        : 2020-12-31
  * Description : Online version downloader.
  *
- * Copyright (C) 2020 by Maik Qualmann <metzpinguin at gmail dot com>
- * Copyright (C) 2021 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2020-2021 by Maik Qualmann <metzpinguin at gmail dot com>
+ * Copyright (C) 2010-2021 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -155,7 +155,7 @@ void OnlineVersionDwnl::startDownload(const QString& version)
             return;
         }
 
-        QString os    = 
+        QString os    =
 
 #ifdef Q_OS_MACOS
 
@@ -263,7 +263,7 @@ void OnlineVersionDwnl::slotDownloaded(QNetworkReply* reply)
         return;
     }
 
-    // Whole file to download arrive
+    // Whole file to download is arrive
 
     QByteArray data = reply->readAll();
 
