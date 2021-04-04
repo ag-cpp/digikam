1f2105cd68e404c7b845e84d2f29e079d1d40d92
Gilles Caulier <caulier.gilles@gmail.com>
2021-04-04 23:44:27 +0200

    identify tool star to work but not yet completed

--- core/tests/dimg/magickidentify.cpp
+++ core/tests/dimg/magickidentify.cpp
@@ -37,7 +37,6 @@ int main(int argc, char** argv)
 {
     MagickCoreGenesis((char*)nullptr, MagickFalse);
 
-    MagickCore::Image* image          = nullptr;
     MagickCore::ImageInfo* image_info = nullptr;
     ExceptionInfo ex                  = *AcquireExceptionInfo();
 
@@ -45,13 +44,34 @@ int main(int argc, char** argv)
 
     try
     {
+        char** metadata                   = (char**)malloc(256 * sizeof(char*));
+
+        for (int i = 0 ; i < 256; ++i)
+        {
+            metadata[i]    = (char*)malloc(256 * sizeof(char));
+            metadata[i][0] = '\0';
+        }
+
         image_info          = CloneImageInfo((ImageInfo*)nullptr);
         strcpy(image_info->filename, argv[1]);
         image_info->ping    = MagickTrue;
-        image_info->verbose = MagickFalse;
-        image               = PingImages(image_info, argv[1], &ex);
+        image_info->verbose = MagickTrue;
+        image_info->debug   = MagickTrue;
+        IdentifyImageCommand(image_info, argc, argv, metadata, &ex);
+
+        for (int i = 0 ; i < 256; ++i)
+        {
+            printf("%i: %s\n", i, metadata[i]);
+        }
+
         DestroyImageInfo(image_info);
-        DestroyImage(image);
+
+        for (int i = 0 ; i < 256; ++i)
+        {
+            free(metadata[i]);
+        }
+
+        free (metadata);
     }
     catch (Exception& error_)
     {
