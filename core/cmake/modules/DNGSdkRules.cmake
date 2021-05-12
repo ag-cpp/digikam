03fe0a1cf506923eb7d8860f650cbaaa90419c0b
Gilles Caulier <caulier.gilles@gmail.com>
2021-05-12 05:24:43 +0200

    No need to pass full pass to common definitions for DNGWriter and RawEngine

--- core/cmake/modules/DNGSdkRules.cmake
+++ core/cmake/modules/DNGSdkRules.cmake
@@ -6,9 +6,10 @@
 
 #---------------------------------
 
-# Common DNG SDK definitions for DNGWriter.
+# Common DNG SDK and XMP SDK definitions shared for DNGWriter and RawEngine.
 #
 # Set platteforms flags.
+#
 # NOTE: see bug #195735: do not enable Mac flags provided by Adobe.
 #       Sounds like all compile fine like under Linux.
 
@@ -16,6 +17,7 @@ add_definitions(-DEnablePluginManager=0)
 add_definitions(-DXMP_StaticBuild=1)
 
 if(MSVC)
+
     add_definitions(
                     # XMP SDK
                     -DWIN_ENV=1
@@ -24,8 +26,10 @@ if(MSVC)
                     -DqWinOS=1
                     -DqMacOS=0
                     -DqDNGThreadSafe=0
-                    )
+    )
+
 else()
+
     add_definitions(
                      # XMP SDK
                     -DUNIX_ENV=1
@@ -35,21 +39,27 @@ else()
                     -DqMacOS=0
                     -DqLinux=1
                     -DqDNGUseStdInt=1
-                   )
+    )
+
 endif()
 
 # Check processor endianness
+
 include(TestBigEndian)
 TEST_BIG_ENDIAN(IS_BIG_ENDIAN)
 
 if(NOT IS_BIG_ENDIAN)
+
     add_definitions(-DqDNGLittleEndian)
+
 endif()
 
 # Thread safe support under Mac and Linux using pthread library
 
 if(NOT MSVC)
+
     add_definitions(-DqDNGThreadSafe)
+
 endif()
 
 # Mode definition for CLI tool.
