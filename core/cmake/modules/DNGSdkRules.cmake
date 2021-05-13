e9401db6aed40024a18e02910a558417824c62e3
Gilles Caulier <caulier.gilles@gmail.com>
2021-05-13 04:58:29 +0200

    More compile flags for XMP SDK backported from Exiv2, especially the important security one

--- core/cmake/modules/DNGSdkRules.cmake
+++ core/cmake/modules/DNGSdkRules.cmake
@@ -8,46 +8,55 @@
 
 # Common DNG SDK and XMP SDK definitions shared for DNGWriter and RawEngine.
 #
-# Set platteforms flags.
-#
-# NOTE: see bug #195735: do not enable Mac flags provided by Adobe.
+# NOTE: see bug #195735: for platteforms flags, do not enable Mac flags provided by Adobe.
 #       Sounds like all compile fine like under Linux.
 
-add_definitions(-DEnablePluginManager=0)    # XMP SDK
-add_definitions(-DXMP_StaticBuild=1)        # XMP SDK
-add_definitions(-DqDNGXMPFiles=0)           # DNG SDK: minimum XMP-set
-add_definitions(-DqDNGXMPDocOps=0)          # DNG SDK: minimum XMP-set
-add_definitions(-DqDNGDebug=0)              # DNG SDK: no debug code
-add_definitions(-DqDNGUseLibJPEG=1)         # DNG SDK: use libjpeg
-add_definitions(-DqDNGUseStdInt=1)          # DNG SDK: use standard definition of integer
-add_definitions(-DqDNGThreadSafe=1)         # DNG SDK: Use thread safety
-add_definitions(-DqDNGValidateTarget=1)     # DNG SDK: compilation of DNG validation CLI tool
+# --- For XMP SDK ---------------------------------------------------------------------
 
-# Plateform definitions
+add_definitions(-DEnablePluginManager=0)    # Do not use plugin extensions
+add_definitions(-DXMP_StaticBuild=1)        # Build statically
+
+# Prevent a denial-service-attack related to XML entity expansion
+# ("billion laughs attack").
+# See https://bugzilla.redhat.com/show_bug.cgi?id=888769
+
+add_definitions(-DBanAllEntityUsage=1)
+
+# Platteform definitions
 
 if(MSVC)
 
-    add_definitions(
-                    # XMP SDK
-                    -DWIN_ENV=1
-
-                    # DNG SDK
-                    -DqWinOS=1
-                    -DqMacOS=0
-                    -DqLinux=0
-    )
+    add_definitions(-DWIN_ENV=1)
 
 else()
 
-    add_definitions(
-                     # XMP SDK
-                    -DUNIX_ENV=1
+    add_definitions(-DUNIX_ENV=1)
 
-                    # DNG SDK
-                    -DqWinOS=0
+endif()
+
+# --- For DNG SDK ----------------------------------------------------------------------
+
+add_definitions(-DqDNGXMPFiles=0)           # Minimum XMP-set
+add_definitions(-DqDNGXMPDocOps=0)          # Minimum XMP-set
+add_definitions(-DqDNGDebug=0)              # No debug code
+add_definitions(-DqDNGUseLibJPEG=1)         # Use libjpeg
+add_definitions(-DqDNGUseStdInt=1)          # Use standard definition of integer
+add_definitions(-DqDNGThreadSafe=1)         # Use thread safety
+add_definitions(-DqDNGValidateTarget=1)     # Compilation of DNG validation CLI tool
+
+# Platteform definitions
+
+if(MSVC)
+
+    add_definitions(-DqWinOS=1
                     -DqMacOS=0
-                    -DqLinux=1
-    )
+                    -DqLinux=0)
+
+else()
+
+    add_definitions(-DqWinOS=0
+                    -DqMacOS=0
+                    -DqLinux=1)
 
 endif()
 
