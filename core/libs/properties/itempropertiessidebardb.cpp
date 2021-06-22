8d334d9e5f3e6a59d34e9ba562eb6f9d366a0eee
Anjani Kumar <anjanik012@gmail.com>
2021-06-22 16:27:11 +0530

    Use std::accumulate in itempropertiessidebardb.cpp

--- core/libs/properties/itempropertiessidebardb.cpp
+++ core/libs/properties/itempropertiessidebardb.cpp
@@ -686,12 +686,11 @@ void ItemPropertiesSideBarDB::setImageSelectionPropertiesInformation()
 
     m_selectionPropertiesTab->setSelectionCount(QLocale().toString(d->currentInfos.count()));
 
-    qint64 selectionFileSize = 0;
-
-    foreach (const ItemInfo& info, d->currentInfos)
-    {
-        selectionFileSize += info.fileSize();
-    }
+    auto opAddFileSize = [] (qlonglong a, const ItemInfo &b) {
+        return a + b.fileSize();
+    };
+    const auto currentInfos = d->currentInfos;
+    qint64 selectionFileSize = std::accumulate(currentInfos.begin(), currentInfos.end(), 0L, opAddFileSize);
 
     m_selectionPropertiesTab->setSelectionSize(ItemPropertiesTab::humanReadableBytesCount(selectionFileSize));
 
@@ -699,16 +698,10 @@ void ItemPropertiesSideBarDB::setImageSelectionPropertiesInformation()
 
     m_selectionPropertiesTab->setTotalCount(QLocale().toString(d->allInfos.count()));
 
-    qint64 totalFileSize = 0;
-
-    foreach (const ItemInfo& info, d->allInfos)
-    {
-        totalFileSize += info.fileSize();
-    }
+    const auto allInfos = d->allInfos;
+    qint64 totalFileSize = std::accumulate(allInfos.begin(), allInfos.end(), 0L, opAddFileSize);
 
     m_selectionPropertiesTab->setTotalSize(ItemPropertiesTab::humanReadableBytesCount(totalFileSize));
-
-    return;
 }
 
 ItemPropertiesVersionsTab* ItemPropertiesSideBarDB::getFiltersHistoryTab() const
