e43c85bb9eab2c1010dec3e7dbde5ec2e8a631d5
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2021-06-25 23:44:52 +0200

    save projecting data

--- core/tests/facesengine/scripts/umap.py
+++ core/tests/facesengine/scripts/umap.py
@@ -8,12 +8,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn import svm
 
-import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
 import umap.umap_ as umap
-
-sns.set(style='white', context='notebook', rc={'figure.figsize':(14,10)})
 
 faceData = pd.read_csv('../data/face_data.txt',header=None)
 
@@ -21,25 +17,8 @@ embeddingIndex = set(faceData.columns)
 embeddingIndex.remove(0)
 faceEmbeddings = faceData[embeddingIndex].values
 labels = faceData[0].values
-'''
-X_train, X_test, y_train, y_test = train_test_split(faceEmbeddings,
-                                                    labels,
-                                                    stratify=labels,
-                                                    random_state=42)
 
-reducer = umap.UMAP(n_neighbors=15, metric='euclidean', min_dist=0.1, n_components=2, transform_seed=42)
-reducer.fit(X_train, y=y_train)
-
-reducedEmbedding = reducer.transform(X_test)
-
-plt.scatter(reducedEmbedding[:, 0], reducedEmbedding[:, 1], c=y_test, cmap='Spectral', s=5)
-plt.gca().set_aspect('equal', 'datalim')
-plt.colorbar(boundaries=np.arange(11)-0.5).set_ticks(np.arange(10))
-plt.title('UMAP projection of the face embedding from Extended Yale B dataset', fontsize=24);
-plt.show()
-'''
-
-for i in range(1,10):
+for i in range(10,60,5):
     reducer = umap.UMAP(n_neighbors=15, metric='euclidean', min_dist=0.1, n_components=i*2, transform_seed=42)
     reducer.fit(faceEmbeddings)
 
@@ -53,15 +32,16 @@ for i in range(1,10):
     CC = [0.001,0.01,0.1,1,10,100,1000]
     scores = np.array([])
     for C in CC:
-        svc = svm.SVC(C=C, kernel='rbf',).fit(reduced_X_train, y_train)
+        svc = svm.SVC(C=C, kernel='rbf', gamma='auto').fit(reduced_X_train, y_train)
         scores = np.append(scores, cross_val_score(svc, reduced_X_train, y_train, cv=5).mean())
 
     print(CC)
     print(scores)
 
     C = CC[np.argmax((scores))]
-    svc = svm.SVC(C=C, kernel='rbf',).fit(reduced_X_train, y_train)
+    svc = svm.SVC(C=C, kernel='rbf', gamma='auto').fit(reduced_X_train, y_train)
     print(svc.score(reduced_X_test, y_test))
+
 '''
 print("NuSVC")
 Nu = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 0.7, 0.8]
---
+++ core/tests/facesengine/scripts/umap_export.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import umap.umap_ as umap
+
+sns.set(style='white', context='notebook', rc={'figure.figsize':(14,10)})
+
+faceData = pd.read_csv('../data/face_data.txt',header=None)
+
+embeddingIndex = set(faceData.columns)
+embeddingIndex.remove(0)
+faceEmbeddings = faceData[embeddingIndex].values
+labels = faceData[0].values
+
+reducer = umap.UMAP(n_neighbors=15, metric='euclidean', min_dist=0.1, n_components=2, transform_seed=42)
+reducer.fit(faceEmbeddings)
+reducedEmbedding = reducer.transform(faceEmbeddings)
+
+reducedData = pd.DataFrame({'label':labels, 'X1': reducedEmbedding[:, 0], 'X2': reducedEmbedding[:, 1]})
+reducedData.to_csv('../data/projected_data.txt', index=False, header=False)
+
+plt.scatter(reducedEmbedding[:, 0], reducedEmbedding[:, 1], c=y_test, cmap='Spectral', s=5)
+plt.gca().set_aspect('equal', 'datalim')
+plt.colorbar(boundaries=np.arange(11)-0.5).set_ticks(np.arange(10))
+plt.title('UMAP projection of the face embedding from Extended Yale B dataset', fontsize=24);
+plt.show()
