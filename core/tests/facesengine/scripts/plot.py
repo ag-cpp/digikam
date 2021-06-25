71b9509cf0f0277a033d8f3f98c2e96940772c8c
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2021-06-25 04:48:54 +0200

    update umap script

--- core/tests/facesengine/scripts/plot.py
+++ core/tests/facesengine/scripts/plot.py
@@ -6,56 +6,56 @@
 
 import numpy as np
 from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
+from sklearn import svm
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import umap.umap_ as umap
-import json
 
 sns.set(style='white', context='notebook', rc={'figure.figsize':(14,10)})
 
-with open('Error_images.json') as f:
-    jsonData = json.load(f)
+faceData = pd.read_csv('../data/face_data.txt',header=None)
 
-faceEmbeddings = []
-labels = []
-for json in jsonData:
-    faceEmbeddings.append(json['faceembedding'])
-    labels.append(json['id'])
+embeddingIndex = set(faceData.columns)
+embeddingIndex.remove(0)
+faceEmbeddings = faceData[embeddingIndex].values
+labels = faceData[0].values
+
+X_train, X_test, y_train, y_test = train_test_split(faceEmbeddings,
+                                                    labels,
+                                                    stratify=labels,
+                                                    random_state=42)
 
 reducer = umap.UMAP(n_neighbors=15, metric='euclidean', min_dist=0.1, n_components=2, transform_seed=42)
-reducer.fit(faceEmbeddings)
+reducer.fit(X_train, y=y_train)
 
-reducedEmbedding = reducer.transform(faceEmbeddings)
+reducedEmbedding = reducer.transform(X_test)
 
-plt.scatter(reducedEmbedding[:, 0], reducedEmbedding[:, 1], c=labels, cmap='Spectral', s=5)
+plt.scatter(reducedEmbedding[:, 0], reducedEmbedding[:, 1], c=y_test, cmap='Spectral', s=5)
 plt.gca().set_aspect('equal', 'datalim')
 plt.colorbar(boundaries=np.arange(11)-0.5).set_ticks(np.arange(10))
 plt.title('UMAP projection of the face embedding from Extended Yale B dataset', fontsize=24);
 plt.show()
-
 '''
-for i in range(1, 50):
-    X_train, X_test, y_train, y_test = train_test_split(faceEmbeddings,
+X_train, X_test, y_train, y_test = train_test_split(faceEmbeddings,
                                                         labels,
                                                         stratify=labels,
                                                         random_state=42)
 
-    reducer = umap.UMAP(n_neighbors=15, metric='euclidean', min_dist=0.1, n_components=i*2, transform_seed=42)
-    reducer.fit(X_train, y=y_train)
+reducer = umap.UMAP(n_neighbors=15, metric='euclidean', min_dist=0.1, n_components=2, transform_seed=42)
+reducer.fit(X_train, y=y_train)
 
-    reduced_X_train = reducer.transform(X_train)
-    reduced_X_test  = reducer.transform(X_test)
+reduced_X_train = reducer.transform(X_train)
+reduced_X_test  = reducer.transform(X_test)
 
-    svc = SVC().fit(reduced_X_train, y_train)
-    knn = KNeighborsClassifier().fit(reduced_X_train, y_train)
+svc = svm.SVC(C=10.0, kernel='rbf',).fit(reduced_X_train, y_train)
+nusvc = svm.NuSVC(nu=0.1, kernel='rbf',).fit(reduced_X_train, y_train)
 
-    print(i*2)
-    print(svc.score(reduced_X_test, y_test), knn.score(reduced_X_test, y_test))
-
+print(svc.score(reduced_X_test, y_test), nusvc.score(reduced_X_test, y_test))
+#for i in range(1, 50):
+'''
+'''
 # split face embedding to train and test sets
 X_train, X_test, y_train, y_test = train_test_split(faceEmbeddings,
                                                     labels,
