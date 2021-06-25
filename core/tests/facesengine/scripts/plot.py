d8e9b6da57b714894994dee28dd93152cbeee2a9
Minh Nghia Duong <minhnghiaduong997@gmail.com>
2021-06-25 06:05:35 +0200

    cross validation umap

--- core/tests/facesengine/scripts/plot.py
+++ core/tests/facesengine/scripts/plot.py
@@ -21,7 +21,7 @@ embeddingIndex = set(faceData.columns)
 embeddingIndex.remove(0)
 faceEmbeddings = faceData[embeddingIndex].values
 labels = faceData[0].values
-
+'''
 X_train, X_test, y_train, y_test = train_test_split(faceEmbeddings,
                                                     labels,
                                                     stratify=labels,
@@ -38,22 +38,44 @@ plt.colorbar(boundaries=np.arange(11)-0.5).set_ticks(np.arange(10))
 plt.title('UMAP projection of the face embedding from Extended Yale B dataset', fontsize=24);
 plt.show()
 '''
-X_train, X_test, y_train, y_test = train_test_split(faceEmbeddings,
+
+for i in range(1,10):
+    reducer = umap.UMAP(n_neighbors=15, metric='euclidean', min_dist=0.1, n_components=i*2, transform_seed=42)
+    reducer.fit(faceEmbeddings)
+
+    reduced_Embedding = reducer.transform(faceEmbeddings)
+    reduced_X_train, reduced_X_test, y_train, y_test = train_test_split(reduced_Embedding,
                                                         labels,
                                                         stratify=labels,
                                                         random_state=42)
 
-reducer = umap.UMAP(n_neighbors=15, metric='euclidean', min_dist=0.1, n_components=2, transform_seed=42)
-reducer.fit(X_train, y=y_train)
+    print(i*2)
+    CC = [0.001,0.01,0.1,1,10,100,1000]
+    scores = np.array([])
+    for C in CC:
+        svc = svm.SVC(C=C, kernel='rbf',).fit(reduced_X_train, y_train)
+        scores = np.append(scores, cross_val_score(svc, reduced_X_train, y_train, cv=5).mean())
 
-reduced_X_train = reducer.transform(X_train)
-reduced_X_test  = reducer.transform(X_test)
+    print(CC)
+    print(scores)
 
-svc = svm.SVC(C=10.0, kernel='rbf',).fit(reduced_X_train, y_train)
-nusvc = svm.NuSVC(nu=0.1, kernel='rbf',).fit(reduced_X_train, y_train)
+    C = CC[np.argmax((scores))]
+    svc = svm.SVC(C=C, kernel='rbf',).fit(reduced_X_train, y_train)
+    print(svc.score(reduced_X_test, y_test))
+'''
+print("NuSVC")
+Nu = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 0.7, 0.8]
+scores = np.array([])
+for n in Nu:
+    nusvc = svm.NuSVC(nu=n, kernel='rbf',).fit(reduced_X_train, y_train)
+    scores = np.append(scores, cross_val_score(nusvc, reduced_X_train, y_train, cv=5).mean())
 
-print(svc.score(reduced_X_test, y_test), nusvc.score(reduced_X_test, y_test))
-#for i in range(1, 50):
+print(Nu)
+print(scores)
+nu = Nu[np.argmax((scores))]
+
+nusvc = svm.NuSVC(nu=nu, kernel='rbf',).fit(reduced_X_train, y_train)
+print(nusvc.score(reduced_X_test, y_test))
 '''
 '''
 # split face embedding to train and test sets
