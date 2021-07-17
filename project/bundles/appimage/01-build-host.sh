4070cca7aeb06334b1aff9dabab4c91e43bbcab6
Gilles Caulier <caulier.gilles@gmail.com>
2021-07-17 16:51:32 -0400

    Build openssl 1.1.1 before qt for Mageia7 and AppImage bundle

--- project/bundles/appimage/01-build-host.sh
+++ project/bundles/appimage/01-build-host.sh
@@ -227,7 +227,7 @@ rm -rf $BUILDING_DIR/* || true
 # NOTE: The order to compile each component here is very important.
 
 #/opt/cmake/bin/cmake --build . --config RelWithDebInfo --target ext_libicu        -- -j$CPU_CORES
-#/opt/cmake/bin/cmake --build . --config RelWithDebInfo --target ext_openssl       -- -j$CPU_CORES
+/opt/cmake/bin/cmake --build . --config RelWithDebInfo --target ext_openssl       -- -j$CPU_CORES
 
 /opt/cmake/bin/cmake --build . --config RelWithDebInfo --target ext_qt            -- -j$CPU_CORES    # depend of tiff, png, jpeg
 
--- project/bundles/appimage/config.sh
+++ project/bundles/appimage/config.sh
@@ -43,7 +43,7 @@ DK_VERSION=master
 DK_DEBUG=0
 
 # Qt version to use in bundle. Possible values: 5.14, 5.15
-DK_QTVERSION="5.15-LTS"
+DK_QTVERSION="5.15"
 
 # Option to use QtWebEngine instead QtWebkit
 DK_QTWEBENGINE=1
