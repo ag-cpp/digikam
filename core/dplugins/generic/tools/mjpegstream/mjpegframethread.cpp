f55ec6e985aeb767c48dcf478e5a49f9258d5960
Gilles Caulier <caulier.gilles@gmail.com>
2021-07-25 09:43:15 +0200

    New digiKam/Showfoto generic plugin to stream items through the local network using a Motion JPEG server. A all standard Web Browsers supports MJPEG de facto without to use extension. Video player as VLC can be used to render stream. Note: it's a stream, as a read only slideshow without control to back or forward in items list. TODO: adds settings to tune IP port, dealy between items, random list, play list as loop, etc.

---
+++ core/dplugins/generic/tools/mjpegstream/mjpegframethread.cpp
@@ -0,0 +1,279 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * https://www.digikam.org
+ *
+ * Date        : 2021-07-24
+ * Description : a MJPEG frame generator.
+ *
+ * Copyright (C) 2021 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "mjpegframethread.h"
+
+// Qt includes
+
+#include <QString>
+#include <QBuffer>
+#include <QApplication>
+#include <QFontMetrics>
+#include <QPainter>
+#include <QDateTime>
+
+// Local includes
+
+#include "digikam_debug.h"
+#include "previewloadthread.h"
+
+namespace DigikamGenericMjpegStreamPlugin
+{
+
+MjpegFrameThread::MjpegFrameThread(QObject* const parent)
+    : ActionThreadBase(parent)
+{
+}
+
+MjpegFrameThread::~MjpegFrameThread()
+{
+    // cancel the thread
+    cancel();
+    // wait for the thread to finish
+    wait();
+}
+
+void MjpegFrameThread::createFrameJob(const MjpegServerMap& map)
+{
+    ActionJobCollection collection;
+
+    MjpegFrame* const t = new MjpegFrame();
+    t->setCollectionMap(map);
+    t->setQuality(75);
+
+    connect(t, SIGNAL(signalFrameChanged(QByteArray)),
+            this, SIGNAL(signalFrameChanged(QByteArray)));
+
+    collection.insert(t, 0);
+
+    appendJobs(collection);
+}
+
+// -----------------------------------------------------------
+
+MjpegFrame::MjpegFrame()
+    : ActionJob(nullptr),
+      m_quality(75)
+{
+}
+
+MjpegFrame::~MjpegFrame()
+{
+}
+
+bool MjpegFrame::setQuality(int qa)
+{
+    if ((qa < 1) || (qa > 100))
+    {
+        qCWarning(DIGIKAM_GENERAL_LOG) << "Error: quality value is out of range: " << qa;
+        return false;
+    }
+
+    m_quality = qa;
+    qCDebug(DIGIKAM_GENERAL_LOG) << "JPEG quality:" << m_quality;
+
+    return true;
+}
+
+int MjpegFrame::quality() const
+{
+    return m_quality;
+}
+
+void MjpegFrame::setCollectionMap(const MjpegServerMap& map)
+{
+    m_urlsList.clear();
+
+    for (MjpegServerMap::const_iterator it = map.constBegin() ; it != map.constEnd() ; ++it)
+    {
+        m_urlsList.append(it.value());
+    }
+}
+
+void MjpegFrame::insertOSDToFrame(QImage& frm, const OSDProperties& osd)
+{
+    // Title section
+
+    QString mess = osd.m_title;
+
+    if (osd.m_titleShowDate)
+    {
+        // date must be provided by caller
+
+        mess.append(osd.m_titleDate.toString(QLatin1String("\ndd-MM-yyyy hh:mm:ss.zzz\n")));
+    }
+
+    if (osd.m_titleShowRelDate)
+    {
+        // date must be provided by caller
+        // Julian TODO
+
+        // jour = 24 * 60 * 60 * 1000 = 86 400 000
+
+        int nbj   = osd.m_titleRelDate  / 86400000;
+        int reste =  osd.m_titleRelDate  % 86400000;
+
+        // h = 60 * 60 * 1000 = 3600000
+
+        int nbh   = reste  / 3600000;
+        reste     = reste % 3600000;
+
+        // m =  60 * 1000 = 60000
+
+        int nbm   = reste  / 60000;
+        reste     = reste % 60000;
+
+        // s =  1000
+
+        int nbs   = reste  / 1000;
+        int nbms  = reste % 1000;
+        QString extraDateFormat = QString::number(nbj) +
+                                  QLatin1String(":")   +
+                                  QString::number(nbh) +
+                                  QLatin1String(":")   +
+                                  QString::number(nbm) +
+                                  QLatin1String(":")   +
+                                  QString::number(nbs) +
+                                  QLatin1String(":")   +
+                                  QString::number(nbms)+
+                                  QLatin1String(" ms");
+        mess.append(extraDateFormat);
+
+        //mess.append(QLatin1String(" ms"));
+    }
+
+    QPainter p(&frm);
+
+    QFontMetrics titleMt(osd.m_titleFnt);
+    p.setFont(osd.m_titleFnt);
+
+    QRect titleRect = titleMt.boundingRect(0, 0, frm.width(), frm.height(), 0, mess);
+    QRect bgTitleRect(osd.m_titlePos.x(),
+                      osd.m_titlePos.y() - titleRect.height() + 1,
+                      titleRect.width(),
+                      titleRect.height() + 3);
+
+    p.fillRect(bgTitleRect, osd.m_titleBg);
+
+    p.setPen(QPen(Qt::white));
+    p.drawText(bgTitleRect, osd.m_titleAlign, mess);
+
+    if (!osd.m_titleLogo.isNull())
+    {
+        p.drawImage(osd.m_titlePos.x(), osd.m_titlePos.y() + 5, osd.m_titleLogo);
+    }
+
+    // Description section
+
+    if (!osd.m_desc.isEmpty())
+    {
+        QFontMetrics descMt(osd.m_descFnt);
+        p.setFont(osd.m_descFnt);
+
+        QRect descRect = descMt.boundingRect(0, 0, frm.width(), frm.height(),
+                                             0, osd.m_desc);
+        QRect bgDescRect(osd.m_descPos.x(),
+                         osd.m_descPos.y() - descRect.height() + 1,
+                         descRect.width(),
+                         descRect.height() + 3);
+
+        p.fillRect(bgDescRect, osd.m_descBg);
+
+        p.setPen(QPen(Qt::white));
+        p.drawText(bgDescRect, osd.m_descAlign, osd.m_desc);
+    }
+
+    // Comment section
+
+    if (!osd.m_comment.isEmpty())
+    {
+        QFontMetrics commentMt(osd.m_commentFnt);
+        p.setFont(osd.m_commentFnt);
+
+        QRect commentRect = commentMt.boundingRect(0, 0, frm.width(), frm.height(),
+                                                   0, osd.m_comment);
+        QRect bgCommentRect(osd.m_commentPos.x(),
+                            osd.m_commentPos.y() - commentRect.height() + 1,
+                            commentRect.width(),
+                            commentRect.height() + 3);
+
+        p.fillRect(bgCommentRect, osd.m_commentBg);
+
+        p.setPen(QPen(Qt::white));
+        p.drawText(bgCommentRect, osd.m_commentAlign, osd.m_comment);
+    }
+}
+
+QByteArray MjpegFrame::imageToJPEGArray(const QImage& frame, int quality)
+{
+    QByteArray outbuf;
+    QBuffer buffer(&outbuf);
+    buffer.open(QIODevice::WriteOnly);
+    frame.save(&buffer, "JPEG", quality);
+
+    return outbuf;
+}
+
+void MjpegFrame::run()
+{
+    OSDProperties osd;
+    QImage img;
+    DImg dimg;
+
+    do
+    {
+        foreach (const QUrl& url, m_urlsList)
+        {
+            qCDebug(DIGIKAM_GENERAL_LOG) << "MjpegFrame: Generate frame for" << url.toLocalFile();
+
+            osd.m_title          = url.fileName();
+            osd.m_titlePos       = QPoint(280, 474);
+            osd.m_titleFnt.setPixelSize(15);
+            osd.m_titleBg        = Qt::black;
+            osd.m_desc           = QLatin1String("digiKam");
+            osd.m_descPos        = QPoint(330, 90);
+            osd.m_descFnt.setPixelSize(26);
+            osd.m_descAlign      = Qt::AlignCenter;
+            osd.m_descBg         = Qt::black;
+
+            dimg = PreviewLoadThread::loadHighQualitySynchronously(url.toLocalFile());
+
+            if (dimg.isNull())
+            {
+            }
+            else
+            {
+                img = dimg.copyQImage();
+                insertOSDToFrame(img, osd);
+
+                emit signalFrameChanged(imageToJPEGArray(img, m_quality));
+
+                QThread::sleep(5);
+            }
+        }
+    }
+    while (!m_cancel);
+
+    emit signalDone();
+}
+
+} // namespace DigikamGenericMjpegStreamPlugin
---
+++ core/dplugins/generic/tools/mjpegstream/mjpegstreamdlg.cpp
@@ -0,0 +1,370 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * https://www.digikam.org
+ *
+ * Date        : 2021-07-24
+ * Description : MJPEG SStream mngr configuration dialog to share a single list of files
+ *
+ * Copyright (C) 2021 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "mjpegstreamdlg.h"
+
+// Qt includes
+
+#include <QVBoxLayout>
+#include <QDialogButtonBox>
+#include <QApplication>
+#include <QMessageBox>
+#include <QPushButton>
+#include <QGridLayout>
+#include <QLabel>
+#include <QStyle>
+#include <QIcon>
+#include <QCheckBox>
+
+// KDE includes
+
+#include <klocalizedstring.h>
+#include <ksharedconfig.h>
+#include <kconfiggroup.h>
+
+// Local includes
+
+#include "dinfointerface.h"
+#include "ditemslist.h"
+#include "dxmlguiwindow.h"
+#include "workingwidget.h"
+#include "mjpegservermngr.h"
+
+namespace DigikamGenericMjpegStreamPlugin
+{
+
+class Q_DECL_HIDDEN MjpegStreamDlg::Private
+{
+public:
+
+    explicit Private()
+      : dirty(false),
+        mngr            (MjpegServerMngr::instance()),
+        srvButton       (nullptr),
+        srvStatus       (nullptr),
+        progress        (nullptr),
+        aStats          (nullptr),
+        separator       (nullptr),
+        iStats          (nullptr),
+        startOnStartup  (nullptr),
+        albumSupport    (false),
+        albumSelector   (nullptr),
+        listView        (nullptr),
+        iface           (nullptr),
+        page            (nullptr),
+        buttons         (nullptr)
+    {
+    }
+
+    bool                dirty;
+    MjpegServerMngr*    mngr;
+    QPushButton*        srvButton;
+    QLabel*             srvStatus;
+    WorkingWidget*      progress;
+    QLabel*             aStats;
+    QLabel*             separator;
+    QLabel*             iStats;
+    QCheckBox*          startOnStartup;
+    bool                albumSupport;
+    QWidget*            albumSelector;
+    DItemsList*         listView;
+    DInfoInterface*     iface;
+    QWidget*            page;
+    QDialogButtonBox*   buttons;
+};
+
+MjpegStreamDlg::MjpegStreamDlg(QObject* const /*parent*/,
+                               DInfoInterface* const iface)
+    : DPluginDialog(nullptr, MjpegServerMngr::instance()->configGroupName()),
+      d            (new Private)
+{
+    setWindowTitle(i18nc("@title:window", "Share Files with MJPEG Stream Server"));
+
+    d->iface                 = iface;
+
+    // NOTE: We overwrite the default albums chooser object name for load save check items state between sessions.
+    // The goal is not mix these settings with other export tools.
+
+    d->iface->setObjectName(QLatin1String("SetupMjpegStreamIface"));
+
+    m_buttons->addButton(QDialogButtonBox::Cancel);
+    m_buttons->addButton(QDialogButtonBox::Ok);
+    m_buttons->button(QDialogButtonBox::Ok)->setDefault(true);
+    d->page                  = new QWidget(this);
+    QVBoxLayout* const vbx   = new QVBoxLayout(this);
+    vbx->addWidget(d->page);
+    vbx->addWidget(m_buttons);
+    setLayout(vbx);
+    setModal(false);
+
+    // -------------------
+
+    QGridLayout* const grid = new QGridLayout(d->page);
+    d->albumSupport         = (d->iface && d->iface->supportAlbums());
+
+    if (d->albumSupport)
+    {
+        d->albumSelector = d->iface->albumChooser(this);
+        grid->addWidget(d->albumSelector, 0, 0, 1, 6);
+
+        connect(d->iface, SIGNAL(signalAlbumChooserSelectionChanged()),
+                this, SLOT(slotSelectionChanged()));
+    }
+    else
+    {
+        d->listView = new DItemsList(d->page);
+        d->listView->setObjectName(QLatin1String("MjpegStream ImagesList"));
+        d->listView->setControlButtonsPlacement(DItemsList::ControlButtonsRight);
+        d->listView->setIface(d->iface);
+
+        // Add all items currently loaded in application.
+
+        d->listView->loadImagesFromCurrentSelection();
+
+        // Replug the previous shared items list.
+
+        d->listView->slotAddImages(d->mngr->itemsList());
+        grid->addWidget(d->listView, 0, 0, 1, 6);
+
+        connect(d->listView, SIGNAL(signalImageListChanged()),
+                this, SLOT(slotSelectionChanged()));
+    }
+
+    // -------------------
+
+    const int spacing         = QApplication::style()->pixelMetric(QStyle::PM_DefaultLayoutSpacing);
+
+    d->startOnStartup         = new QCheckBox(i18nc("@option", "Start Server at Startup"));
+    d->startOnStartup->setWhatsThis(i18nc("@info", "Set this option to turn-on the MJPEG server at application start-up automatically"));
+    d->startOnStartup->setChecked(true);
+
+    d->srvButton              = new QPushButton(this);
+    d->srvStatus              = new QLabel(this);
+    d->progress               = new WorkingWidget(this);
+    d->aStats                 = new QLabel(this);
+    d->separator              = new QLabel(QLatin1String(" / "), this);
+    d->iStats                 = new QLabel(this);
+
+    QLabel* const explanation = new QLabel(this);
+    explanation->setOpenExternalLinks(true);
+    explanation->setWordWrap(true);
+    explanation->setFrameStyle(QFrame::Box | QFrame::Plain);
+    QString txt;
+
+    explanation->setText(i18nc("@info",
+                               "The %1 server allows to share items through the local network using a web browser."
+                               QLatin1String("<a href='https://en.wikipedia.org/wiki/Motion_JPEG'>Motion JPEG</a>")));
+
+    grid->addWidget(d->startOnStartup, 1, 0, 1, 6);
+    grid->addWidget(d->srvButton,      2, 0, 1, 1);
+    grid->addWidget(d->srvStatus,      2, 1, 1, 1);
+    grid->addWidget(d->aStats,         2, 2, 1, 1);
+    grid->addWidget(d->separator,      2, 3, 1, 1);
+    grid->addWidget(d->iStats,         2, 4, 1, 1);
+    grid->addWidget(d->progress,       2, 5, 1, 1);
+    grid->addWidget(explanation,       3, 0, 1, 6);
+    grid->setColumnStretch(1, 10);
+    grid->setRowStretch(0, 10);
+    grid->setSpacing(spacing);
+
+    // --------------------------------------------------------
+
+    connect(d->srvButton, SIGNAL(clicked()),
+            this, SLOT(slotToggleMjpegServer()));
+
+    connect(m_buttons->button(QDialogButtonBox::Cancel), &QPushButton::clicked,
+            this, &MjpegStreamDlg::reject);
+
+    connect(m_buttons->button(QDialogButtonBox::Ok), &QPushButton::clicked,
+            this, &MjpegStreamDlg::accept);
+
+    // -------------------
+
+    readSettings();
+}
+
+MjpegStreamDlg::~MjpegStreamDlg()
+{
+    delete d;
+}
+
+void MjpegStreamDlg::accept()
+{
+    if (d->dirty)
+    {
+        bool empty = false;
+
+        if (d->albumSupport)
+        {
+            empty = d->iface->albumChooserItems().isEmpty();
+        }
+        else
+        {
+            empty = d->listView->imageUrls().isEmpty();
+        }
+
+        if (!empty)
+        {
+            int rc = QMessageBox::question(this, i18nc("@title", "Media Server Contents"),
+                                           i18nc("@info", "The items list to share has changed. "
+                                                "Do you want to start now the MJPEG server with this contents?"));
+            if (rc == QMessageBox::Yes)
+            {
+                startMjpegServer();
+            }
+        }
+    }
+
+    saveSettings();
+    QDialog::accept();
+}
+
+void MjpegStreamDlg::readSettings()
+{
+    KSharedConfig::Ptr config = KSharedConfig::openConfig();
+    KConfigGroup group        = config->group(d->mngr->configGroupName());
+
+    d->startOnStartup->setChecked(group.readEntry(d->mngr->configStartServerOnStartupEntry(), false));
+
+    updateServerStatus();
+}
+
+void MjpegStreamDlg::saveSettings()
+{
+    setMjpegServerContents();
+
+    KSharedConfig::Ptr config = KSharedConfig::openConfig();
+    KConfigGroup group        = config->group(d->mngr->configGroupName());
+    group.writeEntry(d->mngr->configStartServerOnStartupEntry(), d->startOnStartup->isChecked());
+    config->sync();
+}
+
+void MjpegStreamDlg::updateServerStatus()
+{
+    if (d->mngr->isRunning())
+    {
+        d->srvStatus->setText(i18nc("@label", "Server is running"));
+        d->aStats->setText(i18ncp("@info", "1 album shared", "%1 albums shared", d->mngr->albumsShared()));
+        d->separator->setVisible(true);
+        d->iStats->setText(i18ncp("@info", "1 item shared",  "%1 items shared",  d->mngr->itemsShared()));
+        d->srvButton->setText(i18nc("@action: button", "Stop"));
+        d->srvButton->setIcon(QIcon::fromTheme(QLatin1String("media-playback-stop")));
+        d->progress->toggleTimer(true);
+        d->progress->setVisible(true);
+    }
+    else
+    {
+        d->srvStatus->setText(i18nc("@label", "Server is not running"));
+        d->aStats->clear();
+        d->separator->setVisible(false);
+        d->iStats->clear();
+        d->srvButton->setText(i18nc("@action: button", "Start"));
+        d->srvButton->setIcon(QIcon::fromTheme(QLatin1String("media-playback-start")));
+        d->progress->toggleTimer(false);
+        d->progress->setVisible(false);
+    }
+}
+
+bool MjpegStreamDlg::setMjpegServerContents()
+{
+    if (d->albumSupport)
+    {
+        DInfoInterface::DAlbumIDs albums = d->iface->albumChooserItems();
+        MjpegServerMap map;
+
+        foreach (int id, albums)
+        {
+            DAlbumInfo anf(d->iface->albumInfo(id));
+            map.insert(anf.title(), d->iface->albumItems(id));
+        }
+
+        if (map.isEmpty())
+        {
+            QMessageBox::information(this, i18nc("@title", "Media Server Contents"),
+                                     i18nc("@info", "There is no collection to share with the current selection..."));
+            return false;
+        }
+
+        d->mngr->setCollectionMap(map);
+    }
+    else
+    {
+        QList<QUrl> urls = d->listView->imageUrls();
+
+        if (urls.isEmpty())
+        {
+            QMessageBox::information(this, i18nc("@title", "Media Server Contents"),
+                                     i18nc("@info", "There is no item to share with the current selection..."));
+
+            return false;
+        }
+
+        d->mngr->setItemsList(i18nc("@info", "Shared Items"), urls);
+    }
+
+    return true;
+}
+
+void MjpegStreamDlg::startMjpegServer()
+{
+    if (d->dirty)
+    {
+        d->dirty = false;
+    }
+
+    if (!setMjpegServerContents())
+    {
+        return;
+    }
+
+    if (!d->mngr->startMjpegServer())
+    {
+        QMessageBox::warning(this, i18nc("@title", "Starting Media Server"),
+                             i18nc("@info", "An error occurs while to start Media Server..."));
+    }
+    else
+    {
+        d->mngr->mjpegServerNotification(true);
+    }
+
+    updateServerStatus();
+}
+
+void MjpegStreamDlg::slotSelectionChanged()
+{
+    d->dirty = true;
+}
+
+void MjpegStreamDlg::slotToggleMjpegServer()
+{
+    if (!d->mngr->isRunning())
+    {
+        startMjpegServer();
+    }
+    else
+    {
+        d->mngr->cleanUp();
+        updateServerStatus();
+    }
+}
+
+} // namespace DigikamGenericMjpegStreamPlugin
