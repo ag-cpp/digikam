d9fe7a52f63189b6d3705eaee61603ee1e5ea9e0
Maik Qualmann <metzpinguin@gmail.com>
2021-07-28 23:02:39 +0200

    QLocale++

--- core/utilities/geolocation/geoiface/items/gpsitemcontainer.cpp
+++ core/utilities/geolocation/geoiface/items/gpsitemcontainer.cpp
@@ -450,7 +450,7 @@ QVariant GPSItemContainer::data(const int column, const int role) const
             return QString();
         }
 
-        return QString::fromLatin1("%1").arg(m_gpsData.getCoordinates().alt(), 7);
+        return QString::fromLatin1("%1").arg(QLocale().toString(m_gpsData.getCoordinates().alt()), 7);
     }
     else if (column == ColumnAccuracy)
     {
