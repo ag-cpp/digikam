0e3dfa37c51e90214a823b6f66a113888a9a7967
Anjani Kumar <anjanik012@gmail.com>
2021-08-16 00:03:53 +0530

    Restore default Qt version(5.15)

--- project/bundles/appimage/config.sh
+++ project/bundles/appimage/config.sh
@@ -42,8 +42,8 @@ DK_VERSION=master
 # Installer will include or not digiKam debug symbols
 DK_DEBUG=0
 
-# Qt version to use in bundle. Possible values: 5.14, 5.15
-DK_QTVERSION="6.2"
+# Qt version to use in bundle. Possible values: 5.14, 5.15, 6.2
+DK_QTVERSION="5.15"
 
 # Option to use QtWebEngine instead QtWebkit
 DK_QTWEBENGINE=1
