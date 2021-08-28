03223d032ccb14858556b028cadf3d0e04bc44f7
Gilles Caulier <caulier.gilles@gmail.com>
2021-08-28 15:46:11 +0200

    fix broken compilation under FreeBSD

--- core/libs/dimg/filters/imgqsort/detector/noise_detector.cpp
+++ core/libs/dimg/filters/imgqsort/detector/noise_detector.cpp
@@ -3,14 +3,14 @@
  * This file is a part of digiKam project
  * https://www.digikam.org
  *
- * Date        : 
+ * Date        : 28/08/2021
  * Description : Image Quality Parser - Noise detection
  *
- * Copyright (C) 2013-2021 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2021 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2021 by Phuoc Khanh Le <phuockhanhnk94 at gmail dot com>
  *
  * References  : https://cse.buffalo.edu/~siweilyu/papers/ijcv14.pdf
- * 
- * 
+ *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
@@ -37,40 +37,34 @@
 namespace Digikam
 {
 
-using namespace Digikam;
-
 const int SIZE_FILTER = 4;
 
 NoiseDetector::Mat3D initFiltersHaar()
 {
     NoiseDetector::Mat3D res;
-    
+
     res.reserve(SIZE_FILTER * SIZE_FILTER);
 
-    int size = SIZE_FILTER;
+    float mat_base[SIZE_FILTER][SIZE_FILTER] =
+        {
+            {   0.5,            0.5,            0.5,            0.5            },
+            {   0.5,            0.5,           -0.5,           -0.5            },
+            {   sqrt(2.0)/2.0, -sqrt(2.0)/2.0,  0.,             0.0            },
+            {   0.0,            0.0,            sqrt(2.0)/2.0, -sqrt(2.0)/2.0  }
+        };
 
-    float mat_base[size][size] = {{0.5, 0.5, 0.5, 0.5},
-                                    {0.5, 0.5, -0.5, -0.5},
-                                    {sqrt(2.0)/2.0,-sqrt(2.0)/2.0, 0., 0.},
-                                    {0., 0., sqrt(2.0)/2.0, -sqrt(2.0)/2.0}}; 
-    
     cv::Mat mat_base_opencv = cv::Mat(SIZE_FILTER, SIZE_FILTER, CV_32FC1, &mat_base);
 
-    for (int i = 0; i < SIZE_FILTER; i++)
+    for (int i = 0 ; i < SIZE_FILTER ; i++)
     {
-        for (int j = 0; j < SIZE_FILTER; j++)
+        for (int j = 0 ; j < SIZE_FILTER ; j++)
         {
             res.push_back(mat_base_opencv.row(i).t() * mat_base_opencv.row(j));
-        }   
+        }
     }
 
     return res;
 }
-
-}
-
-namespace Digikam
-{
 
 const NoiseDetector::Mat3D NoiseDetector::filtersHaar = initFiltersHaar();
 
@@ -93,7 +87,7 @@ public:
 // Main noise detection
 NoiseDetector::NoiseDetector()
     :  DetectorDistortion(),
-       d(new Private)
+       d                 (new Private)
 {
 }
 
@@ -103,15 +97,15 @@ NoiseDetector::~NoiseDetector()
 }
 
 float NoiseDetector::detect(const cv::Mat& image) const
-{    
+{
     cv::Mat image_float = image;
 
     image_float.convertTo(image_float,CV_32F);
-    
+
     // Decompose to channels
-    
+
     Mat3D channels = decompose_by_filter(image_float, filtersHaar);
-    
+
     // Calculate variance and kurtosis
 
     cv::Mat variance, kurtosis ;
@@ -128,7 +122,7 @@ float NoiseDetector::detect(const cv::Mat& image) const
 NoiseDetector::Mat3D NoiseDetector::decompose_by_filter(const cv::Mat& image, const Mat3D& filters) const
 {
     Mat3D filtersUsed = filters.mid(1); // do not use first filter
-    
+
     Mat3D channels;
 
     channels.reserve(filtersUsed.size());
@@ -136,9 +130,9 @@ NoiseDetector::Mat3D NoiseDetector::decompose_by_filter(const cv::Mat& image, co
     for (const auto& filter: filtersUsed)
     {
         cv::Mat tmp = cv::Mat(image.size().width, image.size().height, CV_32FC1 );
-   
+
         cv::filter2D(image, tmp, -1, filter, cv::Point(-1,-1), 0.0, cv::BORDER_REPLICATE);
-        
+
         channels.push_back(tmp);
     }
 
@@ -148,14 +142,14 @@ NoiseDetector::Mat3D NoiseDetector::decompose_by_filter(const cv::Mat& image, co
 void NoiseDetector::calculate_variance_kurtosis(const Mat3D& channels, cv::Mat& variance, cv::Mat& kurtosis) const
 {
     // Get raw moments
-    
+
     cv::Mat mu1 = raw_moment(channels,1) ;
     cv::Mat mu2 = raw_moment(channels,2) ;
     cv::Mat mu3 = raw_moment(channels,3) ;
     cv::Mat mu4 = raw_moment(channels,4) ;
-    
+
     // Calculate variance and kurtosis projection
-    
+
     variance = mu2 - pow_mat(mu1,2);
 
     kurtosis = (mu4 - 4.0*mu1.mul(mu3) + 6.0*pow_mat(mu1,2).mul(mu2) - 3.0*pow_mat(mu1,4)) / pow_mat(variance,2)-3.0;
@@ -166,7 +160,7 @@ void NoiseDetector::calculate_variance_kurtosis(const Mat3D& channels, cv::Mat& 
 float NoiseDetector::noise_variance(const cv::Mat& variance, const cv::Mat& kurtosis) const
 {
     cv::Mat sqrt_kurtosis;
-    
+
     cv::sqrt(kurtosis, sqrt_kurtosis);
 
     float a = mean_mat(sqrt_kurtosis);
@@ -185,22 +179,22 @@ float NoiseDetector::noise_variance(const cv::Mat& variance, const cv::Mat& kurt
 cv::Mat NoiseDetector::raw_moment(const NoiseDetector::Mat3D& mat, int order) const
 {
     float taille_image = mat[0].size().width * mat[0].size().height;
-    
+
     std::vector<float> vec;
     vec.reserve(mat.size());
-    
+
     for (const auto& mat2d : mat)
     {
         vec.push_back(cv::sum(pow_mat(mat2d,order))[0] / taille_image);
     }
-    
+
     return cv::Mat(vec, true);
 }
 
 cv::Mat NoiseDetector::pow_mat(const cv::Mat& mat, float order) const
 {
     cv::Mat res = cv::Mat(mat.size().width, mat.size().height, CV_32FC1 );
-    
+
     cv::pow(mat, order, res);
 
     return res;
@@ -209,7 +203,7 @@ cv::Mat NoiseDetector::pow_mat(const cv::Mat& mat, float order) const
 float NoiseDetector::mean_mat(const cv::Mat& mat) const
 {
     cv::Scalar mean,std;
-    
+
     cv::meanStdDev(mat,mean,std);
 
     return mean[0];
@@ -221,4 +215,4 @@ float NoiseDetector::normalize(const float number) const
     return 1.0 / (1.0 + qExp(-(number - d->alpha)/d->beta ));
 }
 
-}
+} // namespace Digikam
--- core/libs/dimg/filters/imgqsort/detector/noise_detector.h
+++ core/libs/dimg/filters/imgqsort/detector/noise_detector.h
@@ -3,12 +3,11 @@
  * This file is a part of digiKam project
  * https://www.digikam.org
  *
- * Date        : 
+ * Date        : 28/08/2021
  * Description : Image Quality Parser - noise detection
  *
- * Copyright (C) 2013-2021 by Gilles Caulier <caulier dot gilles at gmail dot com>
- *
- * References  : 
+ * Copyright (C) 2021 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2021 by Phuoc Khanh Le <phuockhanhnk94 at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -35,38 +34,42 @@
 namespace Digikam
 {
 
-class NoiseDetector : public DetectorDistortion  
+class NoiseDetector : public DetectorDistortion
 {
     Q_OBJECT
+
 public:
+
     typedef QList<cv::Mat> Mat3D;
+
 public:
 
     explicit NoiseDetector();
     ~NoiseDetector();
 
-    float detect(const cv::Mat& image)                                  const override;
+    float detect(const cv::Mat& image)                                          const override;
 
 public:
-    
+
     static const Mat3D filtersHaar;
 
 private:
-    
-    Mat3D   decompose_by_filter(const cv::Mat& image, const Mat3D& filters)     const;                          
-    void    calculate_variance_kurtosis(const Mat3D& channels, cv::Mat& variance, cv::Mat& kurtosis) const;
+
+    Mat3D   decompose_by_filter(const cv::Mat& image, const Mat3D& filters)     const;
+    void    calculate_variance_kurtosis(const Mat3D& channels,
+                                        cv::Mat& variance,
+                                        cv::Mat& kurtosis)                      const;
     float   noise_variance(const cv::Mat& variance, const cv::Mat& kurtosis)    const;
     float   normalize(const float number)                                       const;
 
     cv::Mat raw_moment(const NoiseDetector::Mat3D& mat, int order)              const;
     cv::Mat pow_mat(const cv::Mat& mat, float ordre)                            const;
     float   mean_mat(const cv::Mat& mat)                                        const;
-    
+
 private:
 
     class Private;
     Private* const d;
-
 };
 
 } // namespace Digikam
