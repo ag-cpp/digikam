8f27e3256e158138acc76544b3131fd477d868e2
Gilles Caulier <caulier.gilles@gmail.com>
2021-08-31 10:31:13 +0200

    krazy++

--- core/tests/facesengine/scripts/face_svm.R
+++ core/tests/facesengine/scripts/face_svm.R
@@ -1,3 +1,26 @@
+# ============================================================
+#
+# This file is a part of digiKam project
+# https://www.digikam.org
+#
+# Date        : 2019-08-11
+# Description : faces engine test scripts
+#
+# Copyright (C) 2021 by Nghia Duong <minhnghiaduong997 at gmail dot com>
+#
+# This program is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation;
+# either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# ============================================================ */
+
 # Method 1: SVM
 # 1.1: SVM linear kernel
 library("foreach")
