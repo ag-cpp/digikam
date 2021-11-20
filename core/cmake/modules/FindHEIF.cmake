ded92877d3e8af16711f4808f152c94d504e8b7a
Gilles Caulier <caulier.gilles@gmail.com>
2021-11-20 06:23:16 +0100

    add HEIF definitions support

--- core/cmake/modules/FindHEIF.cmake
+++ core/cmake/modules/FindHEIF.cmake
@@ -4,6 +4,7 @@
 # Heif_FOUND            True if HEIF library was found.
 # HEIF_INCLUDES_DIR     Where to find HEIF library headers
 # HEIF_LIBRARIES        List of libraries to link against when using HEIF library
+# HEIF_DEFINITIONS      Compiler switches required for using HEIF library
 # HEIF_VERSION          Version of HEIF library (e.g., 3.6.2)
 #
 #  Copyright (c) 2019-2021 Caulier Gilles <caulier dot gilles at gmail dot com>
@@ -12,25 +13,37 @@
 #  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
 
-if(HEIF_LIBRARIES AND HEIF_INCLUDE_DIRS AND HEIF_VERSION)
+if(HEIF_LIBRARIES AND HEIF_INCLUDE_DIRS AND HEIF_VERSION AND HEIF_DEFINITIONS)
 
     # in cache already
     Set(HEIF_FOUND TRUE)
 
 else()
 
+    # use pkg-config to get the directories and then use these values
+    # in the FIND_PATH() and FIND_LIBRARY() calls
+    if(NOT WIN32)
+
+        find_package(PkgConfig)
+
+        if(PKG_CONFIG_FOUND)
+
+           PKG_CHECK_MODULES(PC_HEIF libheif)
+
+        endif()
+
+    endif()
+
+    set(HEIF_DEFINITIONS ${PC_HEIF_CFLAGS_OTHER})
+
     include(FindPackageHandleStandardArgs)
 
     find_path(HEIF_INCLUDES_DIR libheif/heif_version.h
-              HINTS             ${HEIF_INCLUDE_PATH}
-              ENV               HEIF_INCLUDE_PATH
-              DOC               "The directory where libheif headers reside"
+              HINTS             ${PC_HEIF_INCLUDE_DIRS}
     )
 
     find_library(HEIF_LIBRARIES heif
-                 HINTS          ${HEIF_LIBRARY_PATH}
-                 ENV            HEIF_LIBRARY_PATH
-                 DOC            "The directory where libheif libraries reside"
+                 HINTS          ${PC_HEIF_LIBRARY_DIRS}
     )
 
     if(HEIF_INCLUDES_DIR)
@@ -64,11 +77,12 @@ else()
 
     endif()
 
-    mark_as_advanced(HEIF_INCLUDES_DIR HEIF_LIBRARIES HEIF_VERSION)
+    mark_as_advanced(HEIF_INCLUDES_DIR HEIF_LIBRARIES HEIF_VERSION HEIF_DEFINITIONS)
 
     message(STATUS "HEIF_FOUND        = ${HEIF_FOUND}")
     message(STATUS "HEIF_INCLUDES_DIR = ${HEIF_INCLUDES_DIR}")
     message(STATUS "HEIF_LIBRARIES    = ${HEIF_LIBRARIES}")
+    message(STATUS "HEIF_DEFINITIONS  = ${HEIF_DEFINITIONS}")
     message(STATUS "HEIF_VERSION      = ${HEIF_VERSION}")
 
 endif()
