376950ac954dbc078142a40910105f3b9844ad78
Gilles Caulier <caulier.gilles@gmail.com>
2021-12-30 14:43:18 +0100

    upload MacOS bundle log files

--- project/bundles/macports/update.sh
+++ project/bundles/macports/update.sh
@@ -1,12 +1,45 @@
 #!/bin/bash
 
-# Script to update digiKam installer.
+# Script to update digiKam MacOS package.
 #
 # Copyright (c) 2013-2022 by Gilles Caulier  <caulier dot gilles at gmail dot com>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
+
+########################################################################
+# Function to upload bundle log files
+BundleUploadLogFiles()
+{
+
+if [[ $DK_UPLOAD = 1 ]] ; then
+
+    echo -e "---------- Cleanup older bundle logs from files.kde.org repository \n"
+
+    sftp -q $DK_UPLOADURL:$DK_UPLOADDIR/build.logs/macos <<< "rm build-digikam.full.log.gz"
+    sftp -q $DK_UPLOADURL:$DK_UPLOADDIR/build.logs/macos <<< "rm build-installer.full.log.gz"
+
+    echo -e "---------- Compress bundle log files \n"
+
+    gzip -k $ORIG_WD/logs/build-digikam.full.log $ORIG_WD/logs/build-digikam.full.log.gz     || true
+    gzip -k $ORIG_WD/logs/build-installer.full.log $ORIG_WD/logs/build-installer.full.log.gz || true
+
+    echo -e "---------- Upload new bundle logs to files.kde.org repository \n"
+
+    rsync -r -v --progress -e ssh $ORIG_WD/logs/build-digikam.full.log.gz $DK_UPLOADURL:$DK_UPLOADDIR/build.logs/macos   || true
+    rsync -r -v --progress -e ssh $ORIG_WD/logs/build-installer.full.log.gz $DK_UPLOADURL:$DK_UPLOADDIR/build.logs/macos || true
+
+    echo -e "---------- Cleanup local bundle log file archives \n"
+
+    rm -f $ORIG_WD/logs/build-digikam.full.log.gz   || true
+    rm -f $ORIG_WD/logs/build-installer.full.log.gz || true
+
+fi
+
+}
+
+########################################################################
 
 # Ask to run as root
 (( EUID != 0 )) && exec sudo -- "$0" "$@"
@@ -15,6 +48,7 @@
 set -eE
 trap 'PREVIOUS_COMMAND=$THIS_COMMAND; THIS_COMMAND=$BASH_COMMAND' DEBUG
 trap 'echo "FAILED COMMAND: $PREVIOUS_COMMAND"' ERR
+trap BundleUploadLogFiles ERR exit
 
 . ./config.sh
 . ./common.sh
@@ -22,6 +56,8 @@ StartScript
 ChecksRunAsRoot
 ChecksXCodeCLI
 ChecksCPUCores
+
+OORIG_WD="`pwd`"
 
 echo "++++++++++++++++++++ Update MacOS Installer +++++++++++++++++++++++++++++++++++"
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
