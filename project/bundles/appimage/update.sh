d7c628d8b8e24eb755bdfabfa4575e60a5678846
Gilles Caulier <caulier.gilles@gmail.com>
2021-12-30 14:39:07 +0100

    Fix current path to process log files

--- project/bundles/appimage/update.sh
+++ project/bundles/appimage/update.sh
@@ -18,22 +18,22 @@ if [[ $DK_UPLOAD = 1 ]] ; then
     echo -e "---------- Cleanup older bundle logs from files.kde.org repository \n"
 
     sftp -q $DK_UPLOADURL:$DK_UPLOADDIR/build.logs/lin64 <<< "rm build-digikam.full.log.gz"
-    sftp -q $DK_UPLOADURL:$DK_UPLOADDIR/build.logs/lin64 <<< "rm build-installer.full.log.gz"
+    sftp -q $DK_UPLOADURL:$DK_UPLOADDIR/build.logs/lin64 <<< "rm build-appimage.full.log.gz"
 
     echo -e "---------- Compress bundle log files \n"
 
     gzip -k $ORIG_WD/logs/build-digikam.full.log $ORIG_WD/logs/build-digikam.full.log.gz     || true
-    gzip -k $ORIG_WD/logs/build-installer.full.log $ORIG_WD/logs/build-installer.full.log.gz || true
+    gzip -k $ORIG_WD/logs/build-appimage.full.log $ORIG_WD/logs/build-appimage.full.log.gz || true
 
     echo -e "---------- Upload new bundle logs to files.kde.org repository \n"
 
     rsync -r -v --progress -e ssh $ORIG_WD/logs/build-digikam.full.log.gz $DK_UPLOADURL:$DK_UPLOADDIR/build.logs/lin64   || true
-    rsync -r -v --progress -e ssh $ORIG_WD/logs/build-installer.full.log.gz $DK_UPLOADURL:$DK_UPLOADDIR/build.logs/lin64 || true
+    rsync -r -v --progress -e ssh $ORIG_WD/logs/build-appimage.full.log.gz $DK_UPLOADURL:$DK_UPLOADDIR/build.logs/lin64 || true
 
     echo -e "---------- Cleanup local bundle log file archives \n"
 
     rm -f $ORIG_WD/logs/build-digikam.full.log.gz   || true
-    rm -f $ORIG_WD/logs/build-installer.full.log.gz || true
+    rm -f $ORIG_WD/logs/build-appimage.full.log.gz || true
 
 fi
 
@@ -54,6 +54,8 @@ StartScript
 ChecksRunAsRoot
 ChecksCPUCores
 HostAdjustments
+
+ORIG_WD="`pwd`"
 
 echo "+++++++++++++++++++++++ Update Linux AppImage bundle ++++++++++++++++++++++++++++++"
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
