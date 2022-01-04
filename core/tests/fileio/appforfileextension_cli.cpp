6264d320c28dfa16b4cdfdbe49d872a3666190b5
Gilles Caulier <caulier.gilles@gmail.com>
2022-01-04 16:07:00 +0100

    new cli tool to open file with default MAcOS applucation bundle CCBUGS: 432336

--- core/tests/fileio/appforfileextension_cli.cpp
+++ core/tests/fileio/appforfileextension_cli.cpp
@@ -3,10 +3,11 @@
  * This file is a part of digiKam project
  * https://www.digikam.org
  *
- * Date        : 2009-02-04
- * Description : a command line tool to load PGF data and convert to QImage
+ * Date        : 2021-02-04
+ * Description : a command line tool to list MacOS apllication bundle
+ *               suitable to open a type mime..
  *
- * Copyright (C) 2011-2022 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2021-2022 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -40,32 +41,32 @@ int main(int /*argc*/, char** /*argv*/)
     suffix = QLatin1String("jpg");
     list   = MacApplicationForFileExtension(suffix);
 
-    qCDebug(DIGIKAM_GENERAL_LOG) << "MacOS Bundle urls for" << suffix;
-    qCDebug(DIGIKAM_GENERAL_LOG) << list;
+    qCDebug(DIGIKAM_TESTS_LOG) << "MacOS Bundle urls for" << suffix;
+    qCDebug(DIGIKAM_TESTS_LOG) << list;
 
     suffix = QLatin1String("jpeg");
     list   = MacApplicationForFileExtension(suffix);
 
-    qCDebug(DIGIKAM_GENERAL_LOG) << "MacOS Bundle urls for" << suffix;
-    qCDebug(DIGIKAM_GENERAL_LOG) << list;
+    qCDebug(DIGIKAM_TESTS_LOG) << "MacOS Bundle urls for" << suffix;
+    qCDebug(DIGIKAM_TESTS_LOG) << list;
 
     suffix = QLatin1String("png");
     list   = MacApplicationForFileExtension(suffix);
 
-    qCDebug(DIGIKAM_GENERAL_LOG) << "MacOS Bundle urls for" << suffix;
-    qCDebug(DIGIKAM_GENERAL_LOG) << list;
+    qCDebug(DIGIKAM_TESTS_LOG) << "MacOS Bundle urls for" << suffix;
+    qCDebug(DIGIKAM_TESTS_LOG) << list;
 
     suffix = QLatin1String("tiff");
     list   = MacApplicationForFileExtension(suffix);
 
-    qCDebug(DIGIKAM_GENERAL_LOG) << "MacOS Bundle urls for" << suffix;
-    qCDebug(DIGIKAM_GENERAL_LOG) << list;
+    qCDebug(DIGIKAM_TESTS_LOG) << "MacOS Bundle urls for" << suffix;
+    qCDebug(DIGIKAM_TESTS_LOG) << list;
 
     suffix = QLatin1String("tif");
     list   = MacApplicationForFileExtension(suffix);
 
-    qCDebug(DIGIKAM_GENERAL_LOG) << "MacOS Bundle urls for" << suffix;
-    qCDebug(DIGIKAM_GENERAL_LOG) << list;
+    qCDebug(DIGIKAM_TESTS_LOG) << "MacOS Bundle urls for" << suffix;
+    qCDebug(DIGIKAM_TESTS_LOG) << list;
 
     return 0;
 }
---
+++ core/tests/fileio/openfilewithapplication_cli.cpp
@@ -0,0 +1,71 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * https://www.digikam.org
+ *
+ * Date        : 2021-02-04
+ * Description : a command line tool to open file with the default
+ *               MacOS apllication bundle.
+ *
+ * Copyright (C) 2021-2022 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+// Qt includes
+
+#include <QString>
+#include <QUrl>
+#include <QList>
+#include <QFileInfo>
+
+// Local includes
+
+#include "digikam_debug.h"
+
+QList<QUrl> MacApplicationForFileExtension(const QString& suffix);
+bool        MacOpenFileWithApplication(const QUrl& fileUrl, const QUrl& appUrl);
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        qCDebug(DIGIKAM_TESTS_LOG) << "openfilewithapplication - Open file with default MacOS bundle Application";
+        qCDebug(DIGIKAM_TESTS_LOG) << "Usage: <file path>";
+        return -1;
+    }
+
+    QString fname    = QString::fromUtf8(argv[1]);
+    QString suffix   = QFileInfo(fname).suffix();
+    QList<QUrl> list = MacApplicationForFileExtension(suffix);
+
+    if (list.isEmpty())
+    {
+        qCDebug(DIGIKAM_TESTS_LOG) << "No application found to open" << fname;
+        return -1;
+    }
+
+    QUrl appUrl  = list.first();
+    QUrl fileUrl = QUrl::fromLocalFile(fname);
+
+    bool ret = MacOpenFileWithApplication(fileUrl, appUrl);
+
+    if (!ret)
+    {
+        qCDebug(DIGIKAM_TESTS_LOG) << "Cannot start application" << appUrl << "to open" << fname;
+        return -1;
+    }
+
+    qCDebug(DIGIKAM_TESTS_LOG) << "Application" << appUrl << "started to open" << fname;
+
+    return 0;
+}
