06e1bcccc40a77347662ee231f67304d2ea67263
Gilles Caulier <caulier.gilles@gmail.com>
2022-01-27 11:37:08 +0100

    polish

--- project/bundles/3rdparty/ext_qt/6.3/bootstrap-qt6.sh
+++ project/bundles/3rdparty/ext_qt/6.3/bootstrap-qt6.sh
@@ -21,68 +21,69 @@ rm -f CMakeCache.txt
 EXT_PREFIX=$1
 DROP_QTWEBENGINE_DEPS=$2
 
-OPENSSL_LIBS='-L$EXT_PREFIX/local/include/openssl/ -l$EXT_PREFIX/local/lib/libssl.a -l$EXT_PREFIX/local/lib/libcrypto.a -lpthread -ldl' \
+OPENSSL_LIBS='-L$EXT_PREFIX/local/include/openssl/ -l$EXT_PREFIX/local/lib/libssl.a -l$EXT_PREFIX/local/lib/libcrypto.a -lpthread -ldl'
 
-./configure       -cmake-generator Ninja\
-                  -prefix $EXT_PREFIX \
-                  -release \
-                  -opensource \
-                  -confirm-license \
-                  -sql-sqlite \
-                  -sql-mysql \
-                  -fontconfig \
-                  -system-freetype \
-                  -openssl-linked \
-                  -nomake tests \
-                  -nomake examples \
-                  -no-qml-debug \
-                  -no-mtdev \
-                  -no-journald \
-                  -no-syslog \
-                  -no-tslib \
-                  -no-directfb \
-                  -no-linuxfb \
-                  -no-libproxy \
-                  -no-pch \
-                  -qt-zlib \
-                  -qt-pcre \
-                  -qt-harfbuzz \
-                  -xcb \
-                  -skip qt3d \
-                  -skip qtactiveqt                   \
-                  -skip qtcanvas3d                   \
-                  -skip qtcharts                     \
-                  -skip qtcoap                       \
-                  -skip qtconnectivity               \
-                  -skip qtdatavis3d                  \
-                  -skip qtdoc                        \
-                  -skip qtfeedback                   \
-                  -skip qtgamepad                    \
-                  -skip qtgraphicaleffects           \
-                  -skip qtlanguageserver             \
-                  -skip qtlocation                   \
-                  -skip qtlottie                     \
-                  -skip qtmqtt                       \
-                  -skip qtmultimedia                 \
-                  -skip qtnetworkauth                \
-                  -skip qtopcua                      \
-                  -skip qtpim                        \
-                  -skip qtpositioning                \
-                  -skip qtqa                         \
-                  -skip qtpurchasing                 \
-                  -skip qtquick3d                    \
-                  -skip qtquicktimeline              \
-                  -skip qtrepotools                  \
-                  -skip qtremoteobjects              \
-                  -skip qtscxml                      \
-                  -skip qtsensors                    \
-                  -skip qtserialbus                  \
-                  -skip qtserialport                 \
-                  -skip qtspeech                     \
-                  -skip qtsystems                    \
-                  -skip qtvirtualkeyboard            \
-                  -skip qtwebglplugin                \
-                  -skip qtwebsockets                 \
-                  -skip qtwebview                    \
-                  $DROP_QTWEBENGINE_DEPS
+./configure       -cmake-generator Ninja                `#  Qt6 use Ninja build system by default.` \
+                  -prefix $EXT_PREFIX                   `#  Install path for the install stage.` \
+                  -release                              `#  Release compiled version.` \
+                  -opensource                           `#  Open Source compiled version.` \
+                  -confirm-license                      `#  Valid directly the license agreement.` \
+                  -sql-sqlite                           `#  Compile Sqlite SQL plugin.` \
+                  -sql-mysql                            `#  Compile Mysql SQL plugin.` \
+                  -fontconfig                           `#  .` \
+                  -system-freetype                      `#  Use system font rendering lib https://doc.qt.io/qt-5/qtgui-attribution-freetype.html.` \
+                  -openssl-linked                       `#  hard link to static version of ssl libraries.` \
+                  -nomake tests                         `#  Do not build test codes.` \
+                  -nomake examples                      `#  Do not build basis example codes.` \
+                  -no-qml-debug                         `#  .` \
+                  -no-mtdev                             `#  .` \
+                  -no-journald                          `#  .` \
+                  -no-syslog                            `#  .` \
+                  -no-tslib                             `#  .` \
+                  -no-directfb                          `#  .` \
+                  -no-linuxfb                           `#  .` \
+                  -no-libproxy                          `#  .` \
+                  -no-iconv                             `#  No International string conversion.` \
+                  -no-pch                               `#  .` \
+                  -qt-zlib                              `#  .` \
+                  -qt-pcre                              `#  .` \
+                  -qt-harfbuzz                          `#  .` \
+                  -xcb                                  `#  .` \
+                  -skip qt3d                            `#  3D core.` \
+                  -skip qtactiveqt                      `#  No need ActiveX support.` \
+                  -skip qtcanvas3d                      `#  New to Qt6.` \
+                  -skip qtcharts                        `#  No need data models charts support.` \
+                  -skip qtcoap                          `#  New to Qt6.` \
+                  -skip qtconnectivity                  `#  For embedded devices only.` \
+                  -skip qtdatavis3d                     `#  no need 3D data visualizations support.` \
+                  -skip qtdoc                           `#  No need documentation.` \
+                  -skip qtfeedback                      `#  New to Qt6.` \
+                  -skip qtgamepad                       `#  No need gamepad hardware support..` \
+                  -skip qtgraphicaleffects              `#  No need Advanced graphical effects in GUI.` \
+                  -skip qtlanguageserver                `#  New to Qt6.` \
+                  -skip qtlocation                      `#  No need geolocation.` \
+                  -skip qtlottie                        `#  No need Adobe QtQuick After Effect animations integration.` \
+                  -skip qtmqtt                          `#  No need Mqtt support.` \
+                  -skip qtmultimedia                    `#  No need multimedia support (replaced by QtAV+ffmpeg).` \
+                  -skip qtnetworkauth                   `#  No need network authentication support.` \
+                  -skip qtopcua                         `#  New to Qt6.` \
+                  -skip qtpim                           `#  New to Qt6.` \
+                  -skip qtpositioning                   `#  New to Qt6.` \
+                  -skip qtqa                            `#  New to Qt6.` \
+                  -skip qtpurchasing                    `#  No need in-app purchase of products support.` \
+                  -skip qtquick3d                       `#  New to Qt6.` \
+                  -skip qtquicktimeline                 `#  New to Qt6.` \
+                  -skip qtremoteobjects                 `#  No need sharing QObject properties between processes support.` \
+                  -skip qtrepotools                     `#  New to Qt6.` \
+                  -skip qtscxml                         `#  No need SCXML state machines support.` \
+                  -skip qtsensors                       `#  For embedded devices only.` \
+                  -skip qtserialbus                     `#  No need serial bus support.` \
+                  -skip qtserialport                    `#  No need serial port support.` \
+                  -skip qtspeech                        `#  No need speech synthesis support.` \
+                  -skip qtsystems                       `#  New to Qt6.` \
+                  -skip qtvirtualkeyboard               `#  No need virtual keyboard support.` \
+                  -skip qtwebglplugin                   `#  No need browser OpenGL extension support.` \
+                  -skip qtwebsockets                    `#  No need websocket support.` \
+                  -skip qtwebview                       `#  QML extension for QWebEngine.` \
+                  $DROP_QTWEBENGINE_DEPS                `#  Extra QtWebEngine dependencies to drop if this module is disabled.` \
 
