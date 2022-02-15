fee5aff1b719d388b20ed747debe6911fd5abf29
Gilles Caulier <caulier.gilles@gmail.com>
2022-02-15 17:48:28 +0100

    qt6 port ++

--- core/libs/dplugins/widgets/dpreviewimage.cpp
+++ core/libs/dplugins/widgets/dpreviewimage.cpp
@@ -1219,9 +1219,9 @@ void DPreviewImage::mouseMoveEvent(QMouseEvent* e)
 }
 
 #if (QT_VERSION > QT_VERSION_CHECK(5, 99, 0))
-void DPreviewImage::enterEvent(QEnterEvent* event)
+void DPreviewImage::enterEvent(QEnterEvent* /*event*/)
 #else
-void DPreviewImage::enterEvent(QEvent* event)
+void DPreviewImage::enterEvent(QEvent* /*event*/)
 #endif
 {
     d->toolBar->show();
