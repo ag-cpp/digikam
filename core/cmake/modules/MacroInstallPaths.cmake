6edbb938ca40aad2a23e7b5eb74309938df3e0ca
Gilles Caulier <caulier.gilles@gmail.com>
2022-02-15 11:14:11 +0100

    export install paths detection in a dedicated cmake macro add service5 rules. Use non-versionned variables name everywhere.

---
+++ core/cmake/modules/MacroInstallPaths.cmake
@@ -0,0 +1,43 @@
+# Macro to rule install paths for Qt5 and Qt6 target depending of ECM provided variables
+#
+# Copyright (c) 2010-2022 by Gilles Caulier, <caulier dot gilles at gmail dot com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#
+
+macro(CHECK_INSTALL_PATHS)
+
+    if (NOT ${KDE_INSTALL_FULL_KNOTIFY5RCDIR} STREQUAL "")
+
+        set(KDE_INSTALL_FULL_KNOTIFYRCDIR ${KDE_INSTALL_FULL_KNOTIFY5RCDIR})
+
+    endif()
+
+    if (NOT ${KDE_INSTALL_FULL_KXMLGUI5DIR} STREQUAL "")
+
+        set(KDE_INSTALL_FULL_KXMLGUIDIR ${KDE_INSTALL_FULL_KXMLGUI5DIR})
+
+    endif()
+
+    if (NOT ${KDE_INSTALL_FULL_KSERVICES5DIR} STREQUAL "")
+
+        set(KDE_INSTALL_FULL_KSERVICESDIR ${KDE_INSTALL_FULL_KSERVICES5DIR})
+
+    endif()
+
+    message(STATUS "--- Install Paths:")
+    message(STATUS "INSTALL DATA DIR      : ${KDE_INSTALL_FULL_DATADIR}")
+    message(STATUS "INSTALL ICON DIR      : ${KDE_INSTALL_FULL_ICONDIR}")
+    message(STATUS "INSTALL APP DIR       : ${KDE_INSTALL_FULL_APPDIR}")
+    message(STATUS "INSTALL KNOTIFYRC DIR : ${KDE_INSTALL_FULL_KNOTIFYRCDIR}")
+    message(STATUS "INSTALL BIN DIR       : ${KDE_INSTALL_FULL_BINDIR}")
+    message(STATUS "INSTALL MAN DIR       : ${KDE_INSTALL_FULL_MANDIR}")
+    message(STATUS "INSTALL PLUGIN DIR    : ${KDE_INSTALL_FULL_PLUGINDIR}")
+    message(STATUS "INSTALL KXMLGUI DIR   : ${KDE_INSTALL_FULL_KXMLGUIDIR}")
+    message(STATUS "INSTALL SERVICES DIR  : ${KDE_INSTALL_FULL_KSERVICESDIR}")
+    message(STATUS "INSTALL LIBEXEC DIR   : ${KDE_INSTALL_FULL_LIBEXECDIR}")
+    message(STATUS "INSTALL METAINFO DIR  : ${KDE_INSTALL_FULL_METAINFODIR}")
+    message(STATUS "---")
+
+endmacro()
