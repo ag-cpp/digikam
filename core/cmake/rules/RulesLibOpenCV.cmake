e63c17e9ec7b5f012534a0b9e85843315e53e1e0
Gilles Caulier <caulier.gilles@gmail.com>
2022-02-26 10:24:53 +0100

    more separated rules in delegate cmake scripts to improve readability and maintenance

---
+++ core/cmake/rules/RulesLibOpenCV.cmake
@@ -0,0 +1,21 @@
+#
+# Copyright (c) 2010-2022 by Gilles Caulier, <caulier dot gilles at gmail dot com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+DETECT_OPENCV(${OPENCV_MIN_VERSION} core objdetect imgproc imgcodecs dnn flann)
+
+if(${OpenCV_FOUND})
+
+    if(${OpenCV_VERSION} VERSION_LESS ${OPENCV_MIN_VERSION})
+
+        message(STATUS "OpenCV < ${OPENCV_MIN_VERSION} have been found. Please use a more recent version.")
+        set(OpenCV_FOUND FALSE)
+
+    endif()
+
+endif()
+
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/app/utils/digikam_opencv.h.cmake.in
+               ${CMAKE_CURRENT_BINARY_DIR}/app/utils/digikam_opencv.h)
---
+++ core/cmake/rules/RulesX11.cmake
@@ -0,0 +1,23 @@
+#
+# Copyright (c) 2010-2022 by Gilles Caulier, <caulier dot gilles at gmail dot com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+# NOTE: X11 API are used to manage Monitor Profiles with LCMS
+
+find_package(X11)
+
+if(X11_FOUND)
+
+    if(NOT Qt6_FOUND)
+        find_package(Qt5 ${QT_MIN_VERSION} NO_MODULE COMPONENTS X11Extras)
+    endif()
+
+    set(HAVE_X11 TRUE)
+
+else()
+
+    set(HAVE_X11 FALSE)
+
+endif()
