07cdc1be3d479c52ebf9b08ed106c1632c6bb093
Gilles Caulier <caulier.gilles@gmail.com>
2022-03-03 16:13:56 +0100

    these targets do not exists anymore

--- core/app/DigikamCoreTarget.cmake
+++ core/app/DigikamCoreTarget.cmake
@@ -27,8 +27,6 @@ set(DIGIKAMCORE_OBJECTS
             $<TARGET_OBJECTS:core_rawengine_obj>
             $<TARGET_OBJECTS:core_dpluginsinterface_obj>
             $<TARGET_OBJECTS:core_libwso2_obj>
-            $<TARGET_OBJECTS:core_libheif_obj>
-            $<TARGET_OBJECTS:core_libde265_obj>
 
             $<TARGET_OBJECTS:core_libmd5_obj>
             $<TARGET_OBJECTS:core_libxmp_obj>
