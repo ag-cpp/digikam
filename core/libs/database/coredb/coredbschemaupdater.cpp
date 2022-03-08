82ef9588282ab4c0d9f9486cc7d7aa4580ef323a
Gilles Caulier <caulier.gilles@gmail.com>
2022-03-08 11:57:09 -0500

    add AVIF as supported type mime. CCBUGS: 109060

--- core/libs/database/coredb/coredbschemaupdater.cpp
+++ core/libs/database/coredb/coredbschemaupdater.cpp
@@ -507,6 +507,7 @@ void CoreDbSchemaUpdater::defaultFilterSettings(QStringList& defaultItemFilter, 
                       << QLatin1String("pnm") << QLatin1String("pgf")  << QLatin1String("bmp")
                       << QLatin1String("pcx") << QLatin1String("heic") << QLatin1String("heif")
                       << QLatin1String("jxl")
+                      << QLatin1String("avif")
                       << QLatin1String("webp");
 
     // Raster graphics editor containers: https://en.wikipedia.org/wiki/Raster_graphics_editor
