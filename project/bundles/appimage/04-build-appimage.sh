300b151b17b54a3ebdf74d3b5ceb8baafb9fa27f
Gilles Caulier <caulier.gilles@gmail.com>
2022-03-10 12:52:31 +0100

    QMimeDatabase use these XML files under Linux

--- project/bundles/appimage/04-build-appimage.sh
+++ project/bundles/appimage/04-build-appimage.sh
@@ -112,6 +112,7 @@ cp -r /usr/share/kxmlgui5                                 ./usr/share
 cp -r /usr/share/kf5                                      ./usr/share
 cp -r /usr/share/solid                                    ./usr/share
 cp -r /usr/share/icu                                      ./usr/share
+cp -r /usr/share/mime                                     ./usr/share
 
 # depending of OpenCV version installed, data directory is not the same.
 cp -r /usr/share/OpenCV                                   ./usr/share  || true
