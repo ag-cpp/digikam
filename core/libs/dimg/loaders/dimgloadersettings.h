cc60fec4344ea7d36213acad5046f2a47e2d0d2b
Gilles Caulier <caulier.gilles@gmail.com>
2022-03-30 22:54:25 +0200

    New abstract class to define DImg loader settings widget implementations. The goal is to move all loader settings widgets to DImg plugins and create new ones easily.

---
+++ core/libs/dimg/loaders/dimgloadersettings.h
@@ -0,0 +1,61 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * https://www.digikam.org
+ *
+ * Date        : 2022-03-30
+ * Description : abstract class to host DImg loader settings.
+ *
+ * Copyright (C) 2022 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef DIGIKAM_DIMG_LOADER_SETTINGS_H
+#define DIGIKAM_DIMG_LOADER_SETTINGS_H
+
+// Qt includes
+
+#include <QWidget>
+#include <QMap>
+#include <QVariant>
+
+// Local includes
+
+#include "digikam_export.h"
+
+namespace Digikam
+{
+
+typedef QMap<QString, QVariant> DImgLoaderPrms;
+
+class DIGIKAM_EXPORT DImgLoaderSettings : public QWidget
+{
+    Q_OBJECT
+
+public:
+
+    explicit DImgLoaderSettings(QWidget* const parent = nullptr);
+    ~DImgLoaderSettings() override;
+
+    virtual void setSettings(const DImgLoaderPrms& set) = 0;
+    virtual DImgLoaderPrms settings() const             = 0;
+
+Q_SIGNALS:
+
+    void signalSettingsChanged();
+};
+
+} // namespace Digikam
+
+#endif // DIGIKAM_DIMG_LOADER_SETTINGS_H
