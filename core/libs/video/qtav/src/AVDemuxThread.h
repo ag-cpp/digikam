31752a9a97dff81622cbf933a0fd3f040edb52fb
Gilles Caulier <caulier.gilles@gmail.com>
2022-04-15 04:03:06 +0200

    Fix init members order (done) Add missing init members in 2nd constructor

--- core/libs/video/qtav/src/AVDemuxThread.h
+++ core/libs/video/qtav/src/AVDemuxThread.h
@@ -29,32 +29,38 @@
 #include "PacketBuffer.h"
 #include <QTimer>
 
-namespace QtAV {
+namespace QtAV
+{
 
 class AVDemuxer;
 class AVThread;
+
 class AVDemuxThread : public QThread
 {
     Q_OBJECT
+
 public:
+
     explicit AVDemuxThread(QObject *parent = 0);
     explicit AVDemuxThread(AVDemuxer *dmx, QObject *parent = 0);
     void setDemuxer(AVDemuxer *dmx);
-    void setAudioDemuxer(AVDemuxer *demuxer); //not thread safe
+    void setAudioDemuxer(AVDemuxer *demuxer);   ///< not thread safe
     void setAudioThread(AVThread *thread);
     AVThread* audioThread();
     void setVideoThread(AVThread *thread);
     AVThread* videoThread();
-    void stepForward(); // show next video frame and pause
+    void stepForward();                         ///< show next video frame and pause
     void stepBackward();
     void seek(qint64 external_pos, qint64 pos, SeekType type); //ms
+
     //AVDemuxer* demuxer
+
     bool isPaused() const;
     bool isEnd() const;
     bool atEndOfMedia() const;
     PacketBuffer* buffer();
     void updateBufferState();
-    void stop(); //TODO: remove it?
+    void stop();                                ///< TODO: remove it?
     void pause(bool p, bool wait = false);
 
     MediaEndAction mediaEndAction() const;
@@ -62,7 +68,9 @@ public:
     bool waitForStarted(int msec = -1);
     qint64 lastSeekPos();
     bool hasSeekTasks();
+
 Q_SIGNALS:
+
     void requestClockPause(bool value);
     void mediaEndActionPauseTriggered();
     void mediaStatusChanged(QtAV::MediaStatus);
@@ -70,7 +78,9 @@ Q_SIGNALS:
     void seekFinished(qint64 timestamp);
     void stepFinished();
     void internalSubtitlePacketRead(int index, const QtAV::Packet& packet);
+
 private slots:
+
     void finishedStepBackward();
     void seekOnPauseFinished();
     void frameDeliveredOnStepForward();
@@ -79,44 +89,54 @@ private slots:
     void onAVThreadQuit();
 
 protected:
+
     virtual void run();
-    /*
+
+    /**
      * If the pause state is true setted by pause(true), then block the thread and wait for pause state changed, i.e. pause(false)
      * and return true. Otherwise, return false immediatly.
      */
     bool tryPause(unsigned long timeout = 100);
 
 private:
+
     void setAVThread(AVThread *&pOld, AVThread* pNew);
     void newSeekRequest(QRunnable *r);
     void processNextSeekTask();
     void seekInternal(qint64 pos, SeekType type, qint64 external_pos = std::numeric_limits < qint64 >::min()); //must call in AVDemuxThread
     void pauseInternal(bool value);
 
-    bool paused;
-    bool user_paused;
-    volatile bool end;
-    MediaEndAction end_action;
-    bool m_buffering;
-    PacketBuffer *m_buffer;
-    AVDemuxer *demuxer;
-    AVDemuxer *ademuxer;
-    AVThread *audio_thread, *video_thread;
-    int audio_stream, video_stream;
-    QMutex buffer_mutex;
-    QWaitCondition cond;
-    BlockingQueue<QRunnable*> seek_tasks;
-    qint64 last_seek_pos;
-    QRunnable *current_seek_task;
-    bool stepping;
-    qint64 stepping_timeout_time;
-        
-    QSemaphore sem;
-    QMutex next_frame_mutex;
-    int clock_type; // change happens in different threads(direct connection)
+private:
+
+    bool                        paused;
+    bool                        user_paused;
+    volatile bool               end;
+    MediaEndAction              end_action;
+    bool                        m_buffering;
+    PacketBuffer*               m_buffer;
+    AVDemuxer*                  demuxer;
+    AVDemuxer*                  ademuxer;
+    AVThread*                   audio_thread, *video_thread;
+    int                         clock_type;         ///< change happens in different threads(direct connection)
+    qint64                      last_seek_pos;
+    QRunnable*                  current_seek_task;
+    bool                        stepping;
+    qint64                      stepping_timeout_time;
+
+    int                         audio_stream, video_stream;
+    QMutex                      buffer_mutex;
+    QWaitCondition              cond;
+    BlockingQueue<QRunnable*>   seek_tasks;
+
+    QSemaphore                  sem;
+    QMutex                      next_frame_mutex;
+
+private:
+
     friend class SeekTask;
     friend class stepBackwardTask;
 };
 
-} //namespace QtAV
+} // namespace QtAV
+
 #endif // QAV_DEMUXTHREAD_H
