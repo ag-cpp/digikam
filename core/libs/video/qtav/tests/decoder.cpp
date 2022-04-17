db84560e6e79a89a3668e25ae4d033f93a089305
Gilles Caulier <caulier.gilles@gmail.com>
2022-04-17 21:39:00 +0200

    Fix header

--- core/libs/video/qtav/tests/decoder.cpp
+++ core/libs/video/qtav/tests/decoder.cpp
@@ -1,9 +1,35 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * https://www.digikam.org
+ *
+ * Date        : 2012-10-31
+ * Description : QtAV: Multimedia framework based on Qt and FFmpeg
+ *
+ * Copyright (C) 2012-2022 Wang Bin <wbsecg1 at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+// Qt includes
 
 #include <QCoreApplication>
 #include <QtDebug>
 #include <QDateTime>
 #include <QQueue>
 #include <QStringList>
+
+// Local includes
+
 #include "AVDemuxer.h"
 #include "VideoDecoder.h"
 #include "Packet.h"
--- core/libs/video/qtav/tests/extract.cpp
+++ core/libs/video/qtav/tests/extract.cpp
@@ -1,28 +1,34 @@
-/******************************************************************************
-    VideoGroup:  this file is part of QtAV examples
-    Copyright (C) 2014-2015 Wang Bin <wbsecg1@gmail.com>
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * https://www.digikam.org
+ *
+ * Date        : 2012-10-31
+ * Description : QtAV: Multimedia framework based on Qt and FFmpeg
+ *
+ * Copyright (C) 2012-2022 Wang Bin <wbsecg1 at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
 
-*   This file is part of QtAV
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-******************************************************************************/
+// Qt includes
 
 #include <QApplication>
 #include <QWidget>
 #include <QtDebug>
 #include <QElapsedTimer>
 #include <QTimer>
+
+// Local includes
 
 #include "VideoFrameExtractor.h"
 #include "QtAVWidgets.h"
