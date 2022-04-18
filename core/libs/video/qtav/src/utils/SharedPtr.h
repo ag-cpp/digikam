1d349efa1537689fcd8e560b4c47ab6678eda2f0
Gilles Caulier <caulier.gilles@gmail.com>
2022-04-18 19:32:46 +0200

    fix coding style

--- core/libs/video/qtav/src/utils/SharedPtr.h
+++ core/libs/video/qtav/src/utils/SharedPtr.h
@@ -20,87 +20,196 @@
  *
  * ============================================================ */
 
-#ifndef SHARED_PTR
-#define SHARED_PTR
+#ifndef QTAV_SHARED_PTR
+#define QTAV_SHARED_PTR
+
+// C++ includes
 
 #include <algorithm>
 #include <QAtomicInt>
+
 /*
  * a simple thread safe shared ptr. QSharedPointer does not provide a way to get how many the ref count is.
  */
 
-namespace impl {
+namespace impl
+{
+
 template <typename T>
-class SharedPtrImpl {
+
+class SharedPtrImpl
+{
+
 public:
-    explicit SharedPtrImpl(T *ptr = 0) : m_ptr(ptr), m_counter(1) { }
-    ~SharedPtrImpl() { delete m_ptr;}
-    T * operator->() const { return m_ptr;}
-    T & operator*() const { return *m_ptr;}
-    T * get() const { return m_ptr;}
-    void ref() { m_counter.ref(); }
-    // return false if becomes 0
-    bool deref() { return m_counter.deref();}
-    int count() const {
-#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0) || QT_VERSION >= QT_VERSION_CHECK(5, 3, 0)
-        return m_counter;
-#else
-        return m_counter.load();
-#endif
+
+    explicit SharedPtrImpl(T* ptr = 0)
+        : m_ptr(ptr),
+          m_counter(1)
+    {
     }
-    bool isNull() const { return !m_ptr;}
+
+    ~SharedPtrImpl()
+    {
+        delete m_ptr;
+    }
+
+    T* operator->() const
+    {
+        return m_ptr;
+    }
+
+    T& operator*() const
+    {
+        return* m_ptr;
+    }
+
+    T* get() const
+    {
+        return m_ptr;
+    }
+
+    void ref()
+    {
+        m_counter.ref();
+    }
+
+    // return false if becomes 0
+
+    bool deref()
+    {
+        return m_counter.deref();
+    }
+
+    int count() const
+    {
+
+#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0) || QT_VERSION >= QT_VERSION_CHECK(5, 3, 0)
+
+        return m_counter;
+
+#else
+
+        return m_counter.load();
+
+#endif
+
+    }
+
+    bool isNull() const
+    {
+        return !m_ptr;
+    }
+
 private:
-    T *m_ptr;
+
+    T*         m_ptr;
     QAtomicInt m_counter;
 };
+
 } // namespace impl
 
 template <typename T>
-class SharedPtr {
+class SharedPtr
+{
 public:
-    explicit SharedPtr(T *ptr = 0) : m_impl(new impl::SharedPtrImpl<T>(ptr)) { }
+
+    explicit SharedPtr(T* ptr = 0)
+        : m_impl(new impl::SharedPtrImpl<T>(ptr))
+    {
+    }
+
     template <typename U>
-    explicit SharedPtr(U *ptr = 0) : m_impl(new impl::SharedPtrImpl<T>(ptr)) { }
-    ~SharedPtr() {
+    explicit SharedPtr(U* ptr = 0)
+        : m_impl(new impl::SharedPtrImpl<T>(ptr))
+    {
+    }
+
+    ~SharedPtr()
+    {
         if (!m_impl->deref())
             delete m_impl;
     }
-    SharedPtr(const SharedPtr &other) {
+
+    SharedPtr(const SharedPtr& other)
+    {
         m_impl = other.m_impl;
         m_impl->ref();
     }
+
     template <typename U>
-    SharedPtr(const SharedPtr<U> &other) {
+    SharedPtr(const SharedPtr<U>& other)
+    {
         m_impl = other.m_impl;
         m_impl->ref();
     }
-    SharedPtr & operator=(const SharedPtr &other) {
+
+    SharedPtr& operator=(const SharedPtr& other)
+    {
         SharedPtr tmp(other);
         swap(tmp);
+
         return *this;
     }
+
     template <typename U>
-    SharedPtr<T> & operator=(const SharedPtr<U> &other) {
+    SharedPtr<T>& operator=(const SharedPtr<U>& other)
+    {
         SharedPtr<T> tmp(other);
         swap(tmp);
+
         return *this;
     }
-    T * operator->() const { return m_impl->operator->();}
-    T & operator*() const { return **m_impl;}
-    bool operator !() const { return isNull();}
+
+    T* operator->() const
+    {
+        return m_impl->operator->();
+    }
+
+    T& operator*() const
+    {
+        return **m_impl;
+    }
+
+    bool operator !() const
+    {
+        return isNull();
+    }
+
     template <typename U>
-    bool operator<(const SharedPtr<U> &other) {  return m_impl->get() < other.get();}
-    bool isNull() const { return m_impl->isNull();}
-    T * get() const {  return m_impl->get();}
-    int count() const { return m_impl->count();}
-    void swap(SharedPtr<T> &other) { std::swap(m_impl, other.m_impl);}
+    bool operator<(const SharedPtr<U> &other)
+    {
+        return m_impl->get() < other.get();
+    }
+
+    bool isNull() const
+    {
+        return m_impl->isNull();
+    }
+
+    T* get() const
+    {
+        return m_impl->get();
+    }
+
+    int count() const
+    {
+        return m_impl->count();
+    }
+
+    void swap(SharedPtr<T> &other)
+    {
+        std::swap(m_impl, other.m_impl);
+    }
+
 private:
-    impl::SharedPtrImpl<T> *m_impl;
+
+    impl::SharedPtrImpl<T>* m_impl;
 };
 
 template <typename T>
-void swap(SharedPtr<T> &left, SharedPtr<T> &right) {
+void swap(SharedPtr<T>& left, SharedPtr<T>& right)
+{
     left.swap(right);
 }
 
-#endif //SHARED_PTR
+#endif // QTAV_SHARED_PTR
