ae31d8e0c80e789b175b4964884e0c03fcd44ae8
Gilles Caulier <caulier.gilles@gmail.com>
2022-04-19 16:57:28 +0200

    move capi.h to utils sur dir

--- core/libs/video/qtav/src/QtAV/private/mkid.h
+++ core/libs/video/qtav/src/QtAV/private/mkid.h
@@ -9,6 +9,7 @@
  *               Template based. A replacement of FourCC.
  *
  * Copyright (C) 2012-2022 Wang Bin <wbsecg1 at gmail dot com>
+ * Copyright (C)      2022 by Gilles Caulier, <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
--- core/libs/video/qtav/src/output/audio/xaudio2_compat.h
+++ core/libs/video/qtav/src/output/audio/xaudio2_compat.h
@@ -8,6 +8,7 @@
  *               XAudio2 compat layer with both DXSDK and WINSDK support
  *
  * Copyright (C) 2012-2022 Wang Bin <wbsecg1 at gmail dot com>
+ * Copyright (C)      2022 by Gilles Caulier, <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
