d16af40106b42a7a46e17af40308cb05e6b1f094
Gilles Caulier <caulier.gilles@gmail.com>
2022-05-05 13:51:33 +0200

    fix header

--- core/libs/video/qtav/examples/framereader/main.cpp
+++ core/libs/video/qtav/examples/framereader/main.cpp
@@ -1,22 +1,27 @@
-/******************************************************************************
-    framereader:  this file is part of QtAV examples
-    Copyright (C) 2012-2016 Wang Bin <wbsecg1@gmail.com>
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * https://www.digikam.org
+ *
+ * Date        : 2012-10-31
+ * Description : QtAV: Multimedia framework based on Qt and FFmpeg
+ *
+ * Copyright (C) 2012-2022 Wang Bin <wbsecg1 at gmail dot com>
+ * Copyright (C)      2022 by Gilles Caulier, <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
 
-*   This file is part of QtAV (from 2016)
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-******************************************************************************/
 #include <cstdio>
 #include <cstdlib>
 #include <QCoreApplication>
