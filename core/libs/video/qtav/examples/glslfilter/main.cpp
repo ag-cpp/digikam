3069c9e829fb75174aab1bf4b07049255fbf62af
Gilles Caulier <caulier.gilles@gmail.com>
2022-05-08 10:02:26 +0200

    QtAV ex++

--- core/libs/video/qtav/examples/glslfilter/main.cpp
+++ core/libs/video/qtav/examples/glslfilter/main.cpp
@@ -21,54 +21,78 @@
  *
  * ============================================================ */
 
+// Qt includes
 
-#include <QtAV>
-#include <QtAV/VideoShaderObject.h>
-#include <QtAV/OpenGLVideo.h>
 #include <QApplication>
 #include <QScopedPointer>
 #include <qmath.h>
 #include <QWidget>
+
+// Local includes
+
+#include "QtAV.h"
+#include "VideoShaderObject.h"
+#include "OpenGLVideo.h"
 #include "digikam_debug.h"
 
 using namespace QtAV;
+
 #define GLSL(x) #x ""
 
-class WaveShader : public DynamicShaderObject {
+class WaveShader : public DynamicShaderObject
+{
     float t;
     qreal A;
 
     Q_OBJECT
     Q_PROPERTY(qreal u_A READ u_A WRITE setU_A NOTIFY u_AChanged) //you can use meta property
+
 public:
-    qreal u_A() const {return A;}
-    void setU_A(qreal v) { A=v; Q_EMIT u_AChanged();}
-    WaveShader(QObject *parent = 0) : DynamicShaderObject(parent)
-      , t(0)
-      , A(0.06)
+
+    qreal u_A() const    { return A;                   }
+    void setU_A(qreal v) { A = v; Q_EMIT u_AChanged(); }
+
+    WaveShader(QObject* const parent = nullptr)
+        : DynamicShaderObject(parent),
+          t(0),
+          A(0.06)
     {
         setProperty("u_t", 0); // you can use dynamic property
+
         //setProperty("u_A", 0.06);
+
         setProperty("u_omega", 5);
+
         setHeader(QLatin1String(GLSL(
-                      uniform float u_omega;
-                      uniform float u_A;
-                      uniform float u_t;
-                      )));
+                                     uniform float u_omega;
+                                     uniform float u_A;
+                                     uniform float u_t;
+                                    )
+        ));
+
         setSample(QLatin1String(GLSL(
-                                    vec4 sample2d(sampler2D tex, vec2 pos, int p)
-                                    {
-                                        vec2 pulse = sin(u_t - u_omega * pos);
-                                        vec2 coord = pos + u_A*vec2(pulse.x, -pulse.x);
-                                        return texture(tex, coord);
-                                    })));
+                                     vec4 sample2d(sampler2D tex, vec2 pos, int p)
+                                     {
+                                         vec2 pulse = sin(u_t - u_omega * pos);
+                                         vec2 coord = pos + u_A*vec2(pulse.x, -pulse.x);
+
+                                         return texture(tex, coord);
+                                     }
+                                    )
+        ));
+
         startTimer(40);
     }
+
 Q_SIGNALS:
+
     void u_AChanged();
+
 protected:
-    void timerEvent(QTimerEvent*) {
-        t+=2.0*M_PI/25.0;
+
+    void timerEvent(QTimerEvent*)
+    {
+        t += 2.0*M_PI / 25.0;
         setProperty("u_t", t);
     }
 };
@@ -76,28 +100,42 @@ protected:
 int main(int argc, char** argv)
 {
     QApplication a(argc, argv);
-    if (a.arguments().size() < 2) {
+
+    if (a.arguments().size() < 2)
+    {
         qCDebug(DIGIKAM_TESTS_LOG) << a.arguments().at(0) << " file";
+
         return 0;
     }
+
     VideoOutput vo;
-    if (!vo.opengl()) {
+
+    if (!vo.opengl())
+    {
         qFatal("opengl renderer is required!");
+
         return 1;
     }
+
     if (!vo.widget())
         return 1;
-    GLSLFilter *f = new GLSLFilter();
+
+    GLSLFilter* const f = new GLSLFilter();
+
     f->setOwnedByTarget();
     f->opengl()->setUserShader(new WaveShader(f));
     f->installTo(&vo);
+
     vo.widget()->show();
     vo.widget()->resize(800, 500);
     vo.widget()->setWindowTitle(QLatin1String("GLSLFilter example"));
+
     AVPlayer player;
     player.setRenderer(&vo);
 
     player.play(a.arguments().last());
+
     return a.exec();
 }
+
 #include "main.moc"
