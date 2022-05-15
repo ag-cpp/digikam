9c82a5bb38a46023ecaf1db79c392730bd5347a4
Maik Qualmann <metzpinguin@gmail.com>
2022-05-15 19:52:44 +0200

    try to fix compile with FFmpeg from Arch linux CCBUGS: 453840

--- core/libs/threadimageio/video/videothumbdecoder_p.h
+++ core/libs/threadimageio/video/videothumbdecoder_p.h
@@ -56,7 +56,7 @@ public:
     AVFormatContext*   pFormatContext;
     AVCodecContext*    pVideoCodecContext;
     AVCodecParameters* pVideoCodecParameters;
-    AVCodec*           pVideoCodec;
+    const AVCodec*     pVideoCodec;
     AVStream*          pVideoStream;
     AVFrame*           pFrame;
     quint8*            pFrameBuffer;
