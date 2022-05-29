5c5e9c42b5d595386356b0665c21941ec8dac881
Gilles Caulier <caulier.gilles@gmail.com>
2022-05-29 10:42:55 +0200

    update

--- core/libs/video/qtav/src/cuda/dynlink_cuda.h
+++ core/libs/video/qtav/src/cuda/dynlink_cuda.h
@@ -78,8 +78,8 @@ typedef enum CUctx_flags_enum
  */
 typedef enum CUstream_flags_enum
 {
-    CU_STREAM_DEFAULT      = 0x0, /**< Default stream flag                                            */
-    CU_STREAM_NON_BLOCKING = 0x1  /**< Stream does not synchronize with stream 0 (the nullptr stream) */
+    CU_STREAM_DEFAULT          = 0x00, /**< Default stream flag                                            */
+    CU_STREAM_NON_BLOCKING     = 0x01  /**< Stream does not synchronize with stream 0 (the nullptr stream) */
 } CUstream_flags;
 
 /**
@@ -87,55 +87,57 @@ typedef enum CUstream_flags_enum
  */
 typedef enum CUdevice_attribute_enum
 {
-    CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_BLOCK = 1,              /**< Maximum number of threads per block */
-    CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_X = 2,                    /**< Maximum block dimension X */
-    CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_Y = 3,                    /**< Maximum block dimension Y */
-    CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_Z = 4,                    /**< Maximum block dimension Z */
-    CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_X = 5,                     /**< Maximum grid dimension X */
-    CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_Y = 6,                     /**< Maximum grid dimension Y */
-    CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_Z = 7,                     /**< Maximum grid dimension Z */
-    CU_DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_BLOCK = 8,        /**< Maximum shared memory available per block in bytes */
-    CU_DEVICE_ATTRIBUTE_SHARED_MEMORY_PER_BLOCK = 8,            /**< Deprecated, use CU_DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_BLOCK */
-    CU_DEVICE_ATTRIBUTE_TOTAL_CONSTANT_MEMORY = 9,              /**< Memory available on device for __constant__ variables in a CUDA C kernel in bytes */
-    CU_DEVICE_ATTRIBUTE_WARP_SIZE = 10,                         /**< Warp size in threads */
-    CU_DEVICE_ATTRIBUTE_MAX_PITCH = 11,                         /**< Maximum pitch in bytes allowed by memory copies */
-    CU_DEVICE_ATTRIBUTE_MAX_REGISTERS_PER_BLOCK = 12,           /**< Maximum number of 32-bit registers available per block */
-    CU_DEVICE_ATTRIBUTE_REGISTERS_PER_BLOCK = 12,               /**< Deprecated, use CU_DEVICE_ATTRIBUTE_MAX_REGISTERS_PER_BLOCK */
-    CU_DEVICE_ATTRIBUTE_CLOCK_RATE = 13,                        /**< Peak clock frequency in kilohertz */
-    CU_DEVICE_ATTRIBUTE_TEXTURE_ALIGNMENT = 14,                 /**< Alignment requirement for textures */
-    CU_DEVICE_ATTRIBUTE_GPU_OVERLAP = 15,                       /**< Device can possibly copy memory and execute a kernel concurrently */
-    CU_DEVICE_ATTRIBUTE_MULTIPROCESSOR_COUNT = 16,              /**< Number of multiprocessors on device */
-    CU_DEVICE_ATTRIBUTE_KERNEL_EXEC_TIMEOUT = 17,               /**< Specifies whether there is a run time limit on kernels */
-    CU_DEVICE_ATTRIBUTE_INTEGRATED = 18,                        /**< Device is integrated with host memory */
-    CU_DEVICE_ATTRIBUTE_CAN_MAP_HOST_MEMORY = 19,               /**< Device can map host memory into CUDA address space */
-    CU_DEVICE_ATTRIBUTE_COMPUTE_MODE = 20,                      /**< Compute mode (See ::CUcomputemode for details) */
-    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE1D_WIDTH = 21,           /**< Maximum 1D texture width */
-    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_WIDTH = 22,           /**< Maximum 2D texture width */
-    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_HEIGHT = 23,          /**< Maximum 2D texture height */
-    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_WIDTH = 24,           /**< Maximum 3D texture width */
-    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_HEIGHT = 25,          /**< Maximum 3D texture height */
-    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_DEPTH = 26,           /**< Maximum 3D texture depth */
-    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_ARRAY_WIDTH = 27,     /**< Maximum texture array width */
-    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_ARRAY_HEIGHT = 28,    /**< Maximum texture array height */
-    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_ARRAY_NUMSLICES = 29, /**< Maximum slices in a texture array */
-    CU_DEVICE_ATTRIBUTE_SURFACE_ALIGNMENT = 30,                 /**< Alignment requirement for surfaces */
-    CU_DEVICE_ATTRIBUTE_CONCURRENT_KERNELS = 31,                /**< Device can possibly execute multiple kernels concurrently */
-    CU_DEVICE_ATTRIBUTE_ECC_ENABLED = 32,                       /**< Device has ECC support enabled */
-    CU_DEVICE_ATTRIBUTE_PCI_BUS_ID = 33,                        /**< PCI bus ID of the device */
-    CU_DEVICE_ATTRIBUTE_PCI_DEVICE_ID = 34,                     /**< PCI device ID of the device */
-    CU_DEVICE_ATTRIBUTE_TCC_DRIVER = 35                         /**< Device is using TCC driver model */
+    CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_BLOCK             = 1,  /**< Maximum number of threads per block                                                */
+    CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_X                   = 2,  /**< Maximum block dimension X                                                          */
+    CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_Y                   = 3,  /**< Maximum block dimension Y                                                          */
+    CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_Z                   = 4,  /**< Maximum block dimension Z                                                          */
+    CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_X                    = 5,  /**< Maximum grid dimension X                                                           */
+    CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_Y                    = 6,  /**< Maximum grid dimension Y                                                           */
+    CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_Z                    = 7,  /**< Maximum grid dimension Z                                                           */
+    CU_DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_BLOCK       = 8,  /**< Maximum shared memory available per block in bytes                                 */
+    CU_DEVICE_ATTRIBUTE_SHARED_MEMORY_PER_BLOCK           = 8,  /**< Deprecated, use CU_DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_BLOCK                    */
+    CU_DEVICE_ATTRIBUTE_TOTAL_CONSTANT_MEMORY             = 9,  /**< Memory available on device for __constant__ variables in a CUDA C kernel in bytes  */
+    CU_DEVICE_ATTRIBUTE_WARP_SIZE                         = 10, /**< Warp size in threads                                                               */
+    CU_DEVICE_ATTRIBUTE_MAX_PITCH                         = 11, /**< Maximum pitch in bytes allowed by memory copies                                    */
+    CU_DEVICE_ATTRIBUTE_MAX_REGISTERS_PER_BLOCK           = 12, /**< Maximum number of 32-bit registers available per block                             */
+    CU_DEVICE_ATTRIBUTE_REGISTERS_PER_BLOCK               = 12, /**< Deprecated, use CU_DEVICE_ATTRIBUTE_MAX_REGISTERS_PER_BLOCK                        */
+    CU_DEVICE_ATTRIBUTE_CLOCK_RATE                        = 13, /**< Peak clock frequency in kilohertz                                                  */
+    CU_DEVICE_ATTRIBUTE_TEXTURE_ALIGNMENT                 = 14, /**< Alignment requirement for textures                                                 */
+    CU_DEVICE_ATTRIBUTE_GPU_OVERLAP                       = 15, /**< Device can possibly copy memory and execute a kernel concurrently                  */
+    CU_DEVICE_ATTRIBUTE_MULTIPROCESSOR_COUNT              = 16, /**< Number of multiprocessors on device                                                */
+    CU_DEVICE_ATTRIBUTE_KERNEL_EXEC_TIMEOUT               = 17, /**< Specifies whether there is a run time limit on kernels                             */
+    CU_DEVICE_ATTRIBUTE_INTEGRATED                        = 18, /**< Device is integrated with host memory                                              */
+    CU_DEVICE_ATTRIBUTE_CAN_MAP_HOST_MEMORY               = 19, /**< Device can map host memory into CUDA address space                                 */
+    CU_DEVICE_ATTRIBUTE_COMPUTE_MODE                      = 20, /**< Compute mode (See ::CUcomputemode for details)                                     */
+    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE1D_WIDTH           = 21, /**< Maximum 1D texture width                                                           */
+    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_WIDTH           = 22, /**< Maximum 2D texture width                                                           */
+    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_HEIGHT          = 23, /**< Maximum 2D texture height                                                          */
+    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_WIDTH           = 24, /**< Maximum 3D texture width                                                           */
+    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_HEIGHT          = 25, /**< Maximum 3D texture height                                                          */
+    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_DEPTH           = 26, /**< Maximum 3D texture depth                                                           */
+    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_ARRAY_WIDTH     = 27, /**< Maximum texture array width                                                        */
+    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_ARRAY_HEIGHT    = 28, /**< Maximum texture array height                                                       */
+    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_ARRAY_NUMSLICES = 29, /**< Maximum slices in a texture array                                                  */
+    CU_DEVICE_ATTRIBUTE_SURFACE_ALIGNMENT                 = 30, /**< Alignment requirement for surfaces                                                 */
+    CU_DEVICE_ATTRIBUTE_CONCURRENT_KERNELS                = 31, /**< Device can possibly execute multiple kernels concurrently                          */
+    CU_DEVICE_ATTRIBUTE_ECC_ENABLED                       = 32, /**< Device has ECC support enabled                                                     */
+    CU_DEVICE_ATTRIBUTE_PCI_BUS_ID                        = 33, /**< PCI bus ID of the device                                                           */
+    CU_DEVICE_ATTRIBUTE_PCI_DEVICE_ID                     = 34, /**< PCI device ID of the device                                                        */
+    CU_DEVICE_ATTRIBUTE_TCC_DRIVER                        = 35  /**< Device is using TCC driver model                                                   */
+
 #if __CUDA_API_VERSION >= 4000
-  , CU_DEVICE_ATTRIBUTE_MEMORY_CLOCK_RATE = 36,                 /**< Peak memory clock frequency in kilohertz */
-    CU_DEVICE_ATTRIBUTE_GLOBAL_MEMORY_BUS_WIDTH = 37,           /**< Global memory bus width in bits */
-    CU_DEVICE_ATTRIBUTE_L2_CACHE_SIZE = 38,                     /**< Size of L2 cache in bytes */
-    CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_MULTIPROCESSOR = 39,    /**< Maximum resident threads per multiprocessor */
-    CU_DEVICE_ATTRIBUTE_ASYNC_ENGINE_COUNT = 40,                /**< Number of asynchronous engines */
-    CU_DEVICE_ATTRIBUTE_UNIFIED_ADDRESSING = 41,                /**< Device uses shares a unified address space with the host */
-    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE1D_LAYERED_WIDTH = 42,   /**< Maximum 1D layered texture width */
-    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE1D_LAYERED_LAYERS = 43   /**< Maximum layers in a 1D layered texture */
+
+  , CU_DEVICE_ATTRIBUTE_MEMORY_CLOCK_RATE                 = 36, /**< Peak memory clock frequency in kilohertz                                           */
+    CU_DEVICE_ATTRIBUTE_GLOBAL_MEMORY_BUS_WIDTH           = 37, /**< Global memory bus width in bits                                                    */
+    CU_DEVICE_ATTRIBUTE_L2_CACHE_SIZE                     = 38, /**< Size of L2 cache in bytes                                                          */
+    CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_MULTIPROCESSOR    = 39, /**< Maximum resident threads per multiprocessor                                        */
+    CU_DEVICE_ATTRIBUTE_ASYNC_ENGINE_COUNT                = 40, /**< Number of asynchronous engines                                                     */
+    CU_DEVICE_ATTRIBUTE_UNIFIED_ADDRESSING                = 41, /**< Device uses shares a unified address space with the host                           */
+    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE1D_LAYERED_WIDTH   = 42, /**< Maximum 1D layered texture width                                                   */
+    CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE1D_LAYERED_LAYERS  = 43  /**< Maximum layers in a 1D layered texture                                             */
+
 #endif
 } CUdevice_attribute;
-
 
 /**
  * Error codes
