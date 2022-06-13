a7c3a30521d8c3c816ff16fec4be2ab3e6b56f20
Gilles Caulier <caulier.gilles@gmail.com>
2022-06-13 14:16:47 +0200

    fix MSVC warning (why GCC or Clang do not report this?)

--- core/libs/threadimageio/video/videothumbdecoder_p.cpp
+++ core/libs/threadimageio/video/videothumbdecoder_p.cpp
@@ -257,7 +257,7 @@ bool VideoThumbDecoder::Private::initFilterGraph(enum AVPixelFormat pixfmt,
 
     if (inputs || outputs)
     {
-        return (-1);
+        return false;
     }
 
     ret = avfilter_graph_config(filterGraph, nullptr);
