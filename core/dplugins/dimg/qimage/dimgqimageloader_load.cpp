4dff05ebee96ba4d44de549cfac1195bfd9d162c
Maik Qualmann <metzpinguin@gmail.com>
2022-06-24 12:46:58 +0200

    sync with qt5-maintenance

--- core/dplugins/dimg/qimage/dimgqimageloader_load.cpp
+++ core/dplugins/dimg/qimage/dimgqimageloader_load.cpp
@@ -47,6 +47,7 @@ bool DImgQImageLoader::load(const QString& filePath, DImgLoaderObserver* const o
     // progress info are only pseudo values
 
     QImageReader reader(filePath);
+    reader.setAutoTransform(false);
     reader.setDecideFormatFromContent(true);
 
     QByteArray readFormat = reader.format();
