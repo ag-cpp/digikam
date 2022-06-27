40625c6958d373c92aea2b31700778bbe10cc118
Gilles Caulier <caulier.gilles@gmail.com>
2022-06-27 11:09:58 +0200

    restore

--- project/bundles/appimage/01-build-host.sh
+++ project/bundles/appimage/01-build-host.sh
@@ -261,7 +261,7 @@ cmake $ORIG_WD/../3rdparty \
 
 # Install new cmake recent version to /opt
 
-#cmake --build . --config RelWithDebInfo --target ext_cmake        -- -j$CPU_CORES
+cmake --build . --config RelWithDebInfo --target ext_cmake        -- -j$CPU_CORES
 
 #################################################################################################
 
@@ -282,8 +282,8 @@ rm -rf $BUILDING_DIR/* || true
 # Low level libraries and Qt5 dependencies
 # NOTE: The order to compile each component here is very important.
 
-#/opt/cmake/bin/cmake --build . --config RelWithDebInfo --target ext_libicu        -- -j$CPU_CORES
-#/opt/cmake/bin/cmake --build . --config RelWithDebInfo --target ext_openssl       -- -j$CPU_CORES
+/opt/cmake/bin/cmake --build . --config RelWithDebInfo --target ext_libicu        -- -j$CPU_CORES
+/opt/cmake/bin/cmake --build . --config RelWithDebInfo --target ext_openssl       -- -j$CPU_CORES
 
 /opt/cmake/bin/cmake --build . --config RelWithDebInfo --target ext_qt            -- -j$CPU_CORES    # depend of tiff, png, jpeg
 
