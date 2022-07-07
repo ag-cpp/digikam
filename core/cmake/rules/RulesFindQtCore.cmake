09ab7ba9725eae391212469fed692429fc780385
Gilles Caulier <caulier.gilles@gmail.com>
2022-07-07 15:52:18 +0200

    force Qt6 detection with BUILD_WITH_QT6 cmake flag used at configuration time if Qt6 is not found, report and error and stop the configuration. Do not mix Qt5 and Qt6 detection. CCBUGS: 456289

--- core/cmake/rules/RulesFindQtCore.cmake
+++ core/cmake/rules/RulesFindQtCore.cmake
@@ -4,20 +4,20 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-find_package(Qt6 ${QT6_MIN_VERSION} QUIET COMPONENTS Core)
+if(BUILD_WITH_QT6)
 
-if(NOT Qt6_FOUND)
+    find_package(Qt6 ${QT6_MIN_VERSION} REQUIRED COMPONENTS Core)
+
+    set(QT_MIN_VERSION   ${QT6_MIN_VERSION})
+    set(QT_VERSION_MAJOR 6)
+
+else()
 
     find_package(Qt5 ${QT5_MIN_VERSION} REQUIRED COMPONENTS Core)
 
     set(QT_MIN_VERSION   ${QT5_MIN_VERSION})
     set(QT_VERSION_MAJOR 5)
 
-else()
-
-    set(QT_MIN_VERSION   ${QT6_MIN_VERSION})
-    set(QT_VERSION_MAJOR 6)
-
 endif()
 
-message(STATUS "Suitable Qt${QT_VERSION_MAJOR} >= ${QT_MIN_VERSION} detected...")
+message(STATUS "Suitable Qt${QT_VERSION_MAJOR} >= ${QT_MIN_VERSION} detected ($QT_VERSION)...")
