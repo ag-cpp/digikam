a4a2d4ccef4228b8197b793a965117a77a54a8f9
Gilles Caulier <caulier.gilles@gmail.com>
2022-07-07 10:57:36 +0200

    enable ccache

--- project/bundles/appimage/03-build-digikam.sh
+++ project/bundles/appimage/03-build-digikam.sh
@@ -124,6 +124,7 @@ echo "---------- Configure digiKam $DK_VERSION"
       -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DCMAKE_INSTALL_PREFIX=/usr \
       -DBUILD_TESTING=OFF \
+      -DBUILD_WITH_CCACHE=ON \
       -DDIGIKAMSC_CHECKOUT_PO=ON \
       -DDIGIKAMSC_CHECKOUT_DOC=OFF \
       -DDIGIKAMSC_COMPILE_PO=ON \
