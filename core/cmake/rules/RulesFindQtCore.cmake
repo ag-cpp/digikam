ef3574ff62023609ea15aea5d0947697c01dfd3f
Gilles Caulier <caulier.gilles@gmail.com>
2022-07-08 10:06:07 +0200

    print right Qt version detected by camke at configuration stage. CCBUGS: 456289

--- core/cmake/rules/RulesFindQtCore.cmake
+++ core/cmake/rules/RulesFindQtCore.cmake
@@ -8,6 +8,7 @@ if(BUILD_WITH_QT6)
 
     find_package(Qt6 ${QT6_MIN_VERSION} REQUIRED COMPONENTS Core)
 
+    set(QT_VERSION       ${Qt6Core_VERSION})
     set(QT_MIN_VERSION   ${QT6_MIN_VERSION})
     set(QT_VERSION_MAJOR 6)
 
@@ -15,9 +16,10 @@ else()
 
     find_package(Qt5 ${QT5_MIN_VERSION} REQUIRED COMPONENTS Core)
 
+    set(QT_VERSION       ${Qt5Core_VERSION})
     set(QT_MIN_VERSION   ${QT5_MIN_VERSION})
     set(QT_VERSION_MAJOR 5)
 
 endif()
 
-message(STATUS "Suitable Qt${QT_VERSION_MAJOR} >= ${QT_MIN_VERSION} detected ($QT_VERSION)...")
+message(STATUS "Suitable Qt${QT_VERSION_MAJOR} >= ${QT_MIN_VERSION} detected: '${QT_VERSION}'.")
