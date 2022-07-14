52e5584db389a26030638a8587f93cb7ac3084cd
Gilles Caulier <caulier.gilles@gmail.com>
2022-07-14 10:36:43 +0200

    this one do not compile under MXE due to missing libical

--- project/bundles/mxe/02-build-extralibs.sh
+++ project/bundles/mxe/02-build-extralibs.sh
@@ -110,7 +110,7 @@ ${MXE_BUILD_TARGETS}-cmake --build . --config RelWithDebInfo --target ext_kimage
 ${MXE_BUILD_TARGETS}-cmake --build . --config RelWithDebInfo --target ext_marble              -- -j$CPU_CORES
 
 # Calendar support
-${MXE_BUILD_TARGETS}-cmake --build . --config RelWithDebInfo --target ext_kcalendarcore       -- -j$CPU_CORES
+#${MXE_BUILD_TARGETS}-cmake --build . --config RelWithDebInfo --target ext_kcalendarcore       -- -j$CPU_CORES
 
 # Marble install shared lib at wrong place.
 mv $MXE_INSTALL_PREFIX/libmarble* $MXE_INSTALL_PREFIX/bin || true
