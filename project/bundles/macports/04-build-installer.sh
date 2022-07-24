c9bc0188f843d90e189c7ce481b0fcfe6c2ae943
Gilles Caulier <caulier.gilles@gmail.com>
2022-07-24 09:11:06 +0200

    host ExifTool binary in the MacOS bundle

--- project/bundles/macports/04-build-installer.sh
+++ project/bundles/macports/04-build-installer.sh
@@ -595,6 +595,21 @@ ln -sv "../../../digikam.app/Contents/lib/mariadb-10.5"                         
 ln -sv "../../../digikam.app/Contents/etc/mariadb-10.5"                          "$TEMPROOT/digikam.app/Contents/etc/mariadb"
 
 #################################################################################################
+# Install ExifTool binary.
+
+cd $DOWNLOAD_DIR
+
+#if [ ! -f $DOWNLOAD_DIR/Image-ExifTool.tar.gz ] ; then
+    wget https://files.kde.org/digikam/exiftool/Image-ExifTool.tar.gz -O Image-ExifTool.tar.gz
+#fi
+
+tar -xvf $DOWNLOAD_DIR/Image-ExifTool.tar.gz -C $TEMPROOT/digikam.app/Contents/bin
+mv $TEMPROOT/digikam.app/Contents/bin/Image-ExifTool* $TEMPROOT/digikam.app/Contents/bin/Image-ExifTool
+
+cd $TEMPROOT/digikam.app/Contents/bin
+ln -s ./Image-ExifTool/exiftool exiftool
+
+#################################################################################################
 # Build PKG file
 
 echo "---------- Create MacOS package for digiKam $DKRELEASEID"
