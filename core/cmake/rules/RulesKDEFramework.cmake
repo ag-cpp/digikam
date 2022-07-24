407ba31d8f376d2b1cf6cdc42a6bd0ebb74c4d94
TRAN Quoc Hung <quochungtran1999@gmail.com>
2022-07-24 23:08:00 +0200

    Cmake configuration for ocr text converter plugin

--- core/cmake/rules/RulesKDEFramework.cmake
+++ core/cmake/rules/RulesKDEFramework.cmake
@@ -22,6 +22,7 @@ find_package(KF5 ${KF5_MIN_VERSION} QUIET
                                     ThreadWeaver                # For Panorama tool.
                                     NotifyConfig                # Plasma desktop application notify configuration.
                                     Notifications               # Plasma desktop notifications integration.
+                                    TextWidgets
 )
 
 if(ENABLE_KFILEMETADATASUPPORT)
