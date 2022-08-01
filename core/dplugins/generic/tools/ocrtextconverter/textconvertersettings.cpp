f17dce695b14c0c4387ed6e1d49166b86518bf52
TRAN Quoc Hung <quochungtran1999@gmail.com>
2022-08-01 20:06:05 +0200

    implement ocr settings widget

---
+++ core/dplugins/generic/tools/ocrtextconverter/textconvertersettings.cpp
@@ -0,0 +1,209 @@
+
+
+// Qt includes
+
+#include <QCloseEvent>
+#include <QFile>
+#include <QFileInfo>
+#include <QGridLayout>
+#include <QPixmap>
+#include <QTimer>
+#include <QTreeWidgetItemIterator>
+#include <QApplication>
+#include <QMessageBox>
+#include <QMenu>
+#include <QPushButton>
+#include <QCursor>
+#include <QWidget>
+#include <QProcess>
+
+// KDE includes
+
+#include <kconfiggroup.h>
+#include <ksharedconfig.h>
+#include <klocalizedstring.h>
+#include <kprocess.h>
+
+// Local includes
+
+#include "filesaveconflictbox.h"
+#include "dcombobox.h"
+#include "dprogresswdg.h"
+#include "dmetadata.h"
+#include "digikam_debug.h"
+#include "imagedialog.h"
+#include "dexpanderbox.h"
+#include "dfileoperations.h"
+#include "exiftoolerrorview.h"
+#include "exiftoolparser.h"
+
+#include "textconverterlist.h"
+#include "textconvertersettings.h"
+#include "ocroptions.h"
+
+using namespace Digikam;
+
+namespace DigikamGenericTextConverterPlugin
+{
+
+class Q_DECL_HIDDEN TextConverterSettings::Private
+{
+public:
+
+    explicit Private()
+      : ocrTesseractLanguageMode   (nullptr),
+        ocrTesseractPSMMode        (nullptr),
+        ocrTesseractOEMMode        (nullptr),
+        errorView                  (nullptr)
+    {
+    }
+
+    // Tesseract options 
+    
+    DComboBox*          ocrTesseractLanguageMode;
+    
+    DComboBox*          ocrTesseractPSMMode;
+
+    DComboBox*          ocrTesseractOEMMode;
+
+    ExifToolErrorView*  errorView;
+};
+
+
+TextConverterSettings::TextConverterSettings(QWidget* const parent)
+    : QWidget(parent),
+      d      (new Private)
+{
+     // ------------
+
+    QLabel* ocrTesseractLanguageLabel   = new QLabel(i18nc("@label", "Languages:"));
+    d->ocrTesseractLanguageMode         = new DComboBox(this);
+    
+    QMap<OcrOptions::Languages, QString>             langMap  = OcrOptions::languagesNames();
+    QMap<OcrOptions::Languages, QString>::const_iterator it   = langMap.constBegin();
+
+    while (it != langMap.constEnd())
+    {
+        d->ocrTesseractLanguageMode->addItem(it.value(), (int)it.key());
+        ++it;
+    }
+    
+    d->ocrTesseractLanguageMode->setDefaultIndex(OcrOptions::Languages::ENG);
+    
+    // ------------
+
+    QLabel* ocrTesseractPSMLabel  = new QLabel(i18nc("@label", "Segmentation mode:"));
+    d->ocrTesseractPSMMode        = new DComboBox(this);
+
+    QMap<OcrOptions::PageSegmentationModes, QString>                psmMap = OcrOptions::psmNames();
+    QMap<OcrOptions::PageSegmentationModes, QString>::const_iterator it1   = psmMap.constBegin();
+
+    while (it1 != psmMap.constEnd())
+    {
+        d->ocrTesseractPSMMode->addItem(it1.value(), (int)it1.key());
+        ++it1;
+    }
+
+    d->ocrTesseractPSMMode->setDefaultIndex(OcrOptions::PageSegmentationModes::FULLY_AUTO_PAGE);
+
+    // ------------
+   
+    QLabel* ocrTesseractOEMLabel  = new QLabel(i18nc("@label", "Engine mode:"));
+    d->ocrTesseractOEMMode        = new DComboBox(this);
+    
+    QMap<OcrOptions::EngineModes, QString>                oemMap  = OcrOptions::oemNames();
+    QMap<OcrOptions::EngineModes, QString>::const_iterator it2    = oemMap.constBegin();
+
+    while (it2 !=  oemMap.constEnd())
+    {
+        d->ocrTesseractOEMMode->addItem(it2.value(), (int)it2.key());
+        ++it2;
+    }
+
+    d->ocrTesseractOEMMode->setDefaultIndex(OcrOptions::EngineModes::DEFAULT);
+
+
+    // ------------
+
+    QGridLayout* const settingsBoxLayout = new QGridLayout(this);
+    settingsBoxLayout->addWidget(ocrTesseractLanguageLabel,        0, 0, 1, 1);
+    settingsBoxLayout->addWidget(d->ocrTesseractLanguageMode,      0, 1, 1, 1);
+    settingsBoxLayout->addWidget(ocrTesseractPSMLabel,             1, 0, 1, 1);
+    settingsBoxLayout->addWidget(d->ocrTesseractPSMMode,           1, 1, 1, 1);
+    settingsBoxLayout->addWidget(ocrTesseractOEMLabel,             2, 0, 1, 1);
+    settingsBoxLayout->addWidget(d->ocrTesseractOEMMode,           2, 1, 1, 1);
+    settingsBoxLayout->setRowStretch(9, 10);
+    settingsBoxLayout->setContentsMargins(QMargins());
+
+    // ------------------------------------------------------------------------
+    
+    connect(d->ocrTesseractLanguageMode, SIGNAL((DComboBox::activated(int))),
+            this, SIGNAL(signalSettingsChanged()));
+
+    connect(d->ocrTesseractPSMMode, SIGNAL((DComboBox::activated(int))),
+            this, SIGNAL(signalSettingsChanged()));
+
+    connect(d->ocrTesseractOEMMode, SIGNAL((DComboBox::activated(int))),
+            this, SIGNAL(signalSettingsChanged()));
+
+//    connect(d->errorView, SIGNAL(signalSetupExifTool()),
+//            this, SIGNAL(signalSetupExifTool()));
+
+    // ------------------------------------------------------------------------
+
+//    slotSetupChanged();
+
+}   
+
+TextConverterSettings::~TextConverterSettings()
+{
+    delete d;
+}
+
+void TextConverterSettings::setDefaultSettings()
+{
+    d->ocrTesseractLanguageMode->slotReset();
+    d->ocrTesseractOEMMode->slotReset();
+    d->ocrTesseractPSMMode->slotReset(); 
+}
+
+void TextConverterSettings::setLanguagesMode(int mode)
+{
+    d->ocrTesseractLanguageMode->setCurrentIndex(mode);
+}
+
+int TextConverterSettings::LanguagesMode() const
+{
+    return d->ocrTesseractLanguageMode->currentIndex();
+}
+
+
+void TextConverterSettings::setPSMMode(int mode)
+{
+    d->ocrTesseractPSMMode->setCurrentIndex(mode);
+}
+
+int TextConverterSettings::PSMMode() const
+{
+    return d->ocrTesseractPSMMode->currentIndex();
+}
+
+void TextConverterSettings::setOEMMode(int mode)
+{
+    d->ocrTesseractOEMMode->setCurrentIndex(mode);
+}
+
+int TextConverterSettings::OEMMode() const
+{
+    return d->ocrTesseractOEMMode->currentIndex();
+}
+
+
+void TextConverterSettings::slotSetupChanged()
+{
+    QScopedPointer<ExifToolParser> const parser(new ExifToolParser(this));
+
+    d->errorView->setVisible(!parser->exifToolAvailable());
+}
+
+} // namespace DigikamGenericTextConverterPlugin
---
+++ core/dplugins/generic/tools/ocrtextconverter/textconvertersettings.h
@@ -0,0 +1,57 @@
+#ifndef DIGIKAM_TEXT_CONVERTER_SETTINGS_H
+#define DIGIKAM_TEXT_CONVERTER_SETTINGS_H
+
+// Qt includes
+
+#include <QWidget>
+#include <QString>
+
+// Local includes
+
+#include "digikam_export.h"
+#include "kconfiggroup.h"
+
+using namespace Digikam;
+
+namespace DigikamGenericTextConverterPlugin
+{
+
+class TextConverterSettings  : public QWidget
+{
+    Q_OBJECT
+
+public:
+
+    explicit TextConverterSettings(QWidget* const parent = nullptr);
+    ~TextConverterSettings() override;
+
+    void setLanguagesMode(int mode);
+    int  LanguagesMode()        const;
+
+    void setPSMMode(int mode);
+    int  PSMMode()              const;
+
+    void setOEMMode(int mode);
+    int  OEMMode()              const;
+
+    void setDefaultSettings();
+
+public Q_SLOTS:
+
+    /// To handle changes from host application Setup dialog.
+    void slotSetupChanged();
+
+Q_SIGNALS:
+
+    void signalSettingsChanged();
+    void signalSetupExifTool();
+
+private:
+
+    class Private;
+    Private* const d;
+};
+
+} // namespace DigikamGenericTextConverterPlugin
+
+#endif // DIGIKAM_TEXT_CONVERTER_SETTINGS_H
