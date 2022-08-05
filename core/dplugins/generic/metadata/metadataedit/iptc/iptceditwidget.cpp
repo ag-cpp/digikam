a12924eb048e2774fab080b5cf9a88ba95d18bab
Gilles Caulier <caulier.gilles@gmail.com>
2022-08-05 22:07:49 +0200

    fix header

--- core/dplugins/generic/metadata/metadataedit/iptc/iptceditwidget.cpp
+++ core/dplugins/generic/metadata/metadataedit/iptc/iptceditwidget.cpp
@@ -218,24 +218,24 @@ void IPTCEditWidget::saveSettings()
 
 void IPTCEditWidget::slotItemChanged()
 {
-    d->page_content->setHeader(d->dlg->currentItemTitleHeader(i18nc("@info", "Content Information\n"
-                     "Use this panel to describe the visual content of the image")));
-    d->page_origin->setHeader(d->dlg->currentItemTitleHeader(i18nc("@info", "Origin Information\n"
-                     "Use this panel for formal descriptive information about the image")));
-    d->page_credits->setHeader(d->dlg->currentItemTitleHeader(i18nc("@info", "Credit Information\n"
-                     "Use this panel to record copyright information about the image")));
-    d->page_subjects->setHeader(d->dlg->currentItemTitleHeader(i18nc("@info", "Subject Information\n"
-                     "Use this panel to record subject information about the image")));
-    d->page_keywords->setHeader(d->dlg->currentItemTitleHeader(i18nc("@info", "Keyword Information\n"
-                     "Use this panel to record keywords relevant to the image")));
-    d->page_categories->setHeader(d->dlg->currentItemTitleHeader(i18nc("@info", "Category Information\n"
-                     "Use this panel to record categories relevant to the image")));
-    d->page_status->setHeader(d->dlg->currentItemTitleHeader(i18nc("@info", "Status Information\n"
-                     "Use this panel to record workflow information")));
-    d->page_properties->setHeader(d->dlg->currentItemTitleHeader(i18nc("@info", "Status Properties\n"
-                     "Use this panel to record workflow properties")));
-    d->page_envelope->setHeader(d->dlg->currentItemTitleHeader(i18nc("@info", "Envelope Information\n"
-                     "Use this panel to record editorial details")));
+    d->page_content->setHeader(d->dlg->currentItemTitleHeader(i18nc("@info", "<qt>Content Information<br/>"
+                     "<i>Use this panel to describe the visual content of the image</i></qt>")));
+    d->page_origin->setHeader(d->dlg->currentItemTitleHeader(i18nc("@info", "<qt>Origin Information<br/>"
+                     "<i>Use this panel for formal descriptive information about the image</i></qt>")));
+    d->page_credits->setHeader(d->dlg->currentItemTitleHeader(i18nc("@info", "<qt>Credit Information<br/>"
+                     "<i>Use this panel to record copyright information about the image</i></qt>")));
+    d->page_subjects->setHeader(d->dlg->currentItemTitleHeader(i18nc("@info", "<qt>Subject Information<br/>"
+                     "<i>Use this panel to record subject information about the image</i></qt>")));
+    d->page_keywords->setHeader(d->dlg->currentItemTitleHeader(i18nc("@info", "<qt>Keyword Information<br/>"
+                     "<i>Use this panel to record keywords relevant to the image</i></qt>")));
+    d->page_categories->setHeader(d->dlg->currentItemTitleHeader(i18nc("@info", "<qt>Category Information<br/>"
+                     "<i>Use this panel to record categories relevant to the image</i></qt>")));
+    d->page_status->setHeader(d->dlg->currentItemTitleHeader(i18nc("@info", "<qt>Status Information<br/>"
+                     "<i>Use this panel to record workflow information</i></qt>")));
+    d->page_properties->setHeader(d->dlg->currentItemTitleHeader(i18nc("@info", "<qt>Status Properties<br/>"
+                     "<i>Use this panel to record workflow properties</i></qt>")));
+    d->page_envelope->setHeader(d->dlg->currentItemTitleHeader(i18nc("@info", "<qt>Envelope Information<br/>"
+                     "<i>Use this panel to record editorial details</i></qt>")));
 
     QScopedPointer<DMetadata> meta(new DMetadata);
     meta->load((*d->dlg->currentItem()).toLocalFile());
