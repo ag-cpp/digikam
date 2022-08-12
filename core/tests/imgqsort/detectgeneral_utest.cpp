97e5570390fed5ca6155a0cd30744210cc6316ff
Gilles Caulier <caulier.gilles@gmail.com>
2022-08-12 17:10:23 +0200

    first try to fix Linux gitlab CI/CD due to exceeds warnings limit

--- core/tests/imgqsort/detectgeneral_utest.cpp
+++ core/tests/imgqsort/detectgeneral_utest.cpp
@@ -7,7 +7,7 @@
  * Description : an unit-test to detect image quality level
  *
  * Copyright (C) 2021-2022 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright (C) 2021 by Phuoc Khanh Le <phuockhanhnk94 at gmail dot com>
+ * Copyright (C) 2021      by Phuoc Khanh Le <phuockhanhnk94 at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -40,7 +40,37 @@
 
 using namespace Digikam;
 
-QTEST_MAIN(ImgQSortTestGeneral)
+int    s_newArgc = 0;                                                                                                           \
+char** s_newArgv = nullptr;                                                                                                     \
+
+int main(int argc, char* argv[])
+{
+    // This test generate more than 2000 warnings on the console.
+    // This code is a hack to disable warnings to prevent breaking workflow with CI/CD on gitlab.
+
+    int i     = 0;                                                                                                      \
+    s_newArgc = argc + 2;                                                                                               \
+    s_newArgv = new char*[s_newArgc];                                                                                   \
+                                                                                                                        \
+    for (i = 0 ; i < argc ; ++i)                                                                                        \
+    {                                                                                                                   \
+        s_newArgv[i] = new char[strlen(argv[i]) + 1];                                                                   \
+        strcpy(s_newArgv[i], argv[i]);                                                                                  \
+    }                                                                                                                   \
+                                                                                                                        \
+    const char* const headLessOpt1 = "-maxwarnings";                                                                       \
+    s_newArgv[i]                   = new char[strlen(headLessOpt1) + 1];                                                \
+    strcpy(s_newArgv[i], headLessOpt1);                                                                                 \
+                                                                                                                        \
+    const char* const headLessOpt2 = "0";                                                                       \
+    s_newArgv[i + 1]               = new char[strlen(headLessOpt2) + 1];                                                \
+    strcpy(s_newArgv[i + 1], headLessOpt2);                                                                             \
+                                                                                                                        \
+    argc = s_newArgc;
+    argv = s_newArgv;                                                                      \
+
+    QTEST_MAIN_IMPL(ImgQSortTestGeneral)
+}
 
 ImgQSortTestGeneral::ImgQSortTestGeneral(QObject* const)
 {
