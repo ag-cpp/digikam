d2f56774ba1cff9f140743d8994edca68d5da613
Gilles Caulier <caulier.gilles@gmail.com>
2022-08-24 07:51:34 +0200

    backup and restore Setup ICC tab between sessions

--- core/utilities/setup/setup.cpp
+++ core/utilities/setup/setup.cpp
@@ -270,6 +270,7 @@ Setup::~Setup()
     KSharedConfig::Ptr config = KSharedConfig::openConfig();
     KConfigGroup group        = config->group(QLatin1String("Setup Dialog"));
     group.writeEntry(QLatin1String("Setup Page"), (int)activePageIndex());
+    group.writeEntry(QLatin1String("ICC Tab"),    (int)d->iccPage->activeTab());
     group.writeEntry(QLatin1String("Camera Tab"), (int)d->cameraPage->activeTab());
     group.writeEntry(QLatin1String("Plugin Tab"), (int)d->pluginsPage->activeTab());
     group.writeEntry(QLatin1String("Misc Tab"),   (int)d->miscPage->activeTab());
@@ -537,6 +538,7 @@ void Setup::showPage(Setup::Page page)
         KConfigGroup group        = config->group(QLatin1String("Setup Dialog"));
 
         item = d->pageItem((Page)group.readEntry(QLatin1String("Setup Page"), (int)CollectionsPage));
+        d->iccPage->setActiveTab((SetupICC::ICCTab)group.readEntry(QLatin1String("ICC Tab"), (int)SetupICC::Behavior));
         d->cameraPage->setActiveTab((SetupCamera::CameraTab)group.readEntry(QLatin1String("Camera Tab"), (int)SetupCamera::Devices));
         d->pluginsPage->setActiveTab((SetupPlugins::PluginTab)group.readEntry(QLatin1String("Plugin Tab"), (int)SetupPlugins::Generic));
         d->miscPage->setActiveTab((SetupMisc::MiscTab)group.readEntry(QLatin1String("Misc Tab"), (int)SetupMisc::Behaviour));
