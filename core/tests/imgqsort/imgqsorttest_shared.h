34a912a95319bd4eadfc16d4b360cd941bda0779
Phuoc-Khanh LE <phuockhanhnk94@gmail.com>
2022-09-03 18:42:18 +0200

    fix bug general test

--- core/tests/imgqsort/imgqsorttest_shared.h
+++ core/tests/imgqsort/imgqsorttest_shared.h
@@ -58,6 +58,7 @@ struct CustomDetection
     bool detectNoise;
     bool detectExposure;
     bool detectCompression;
+    bool detectAesthetic;
 };
 
 QHash<QString, int> ImgQSortTest_ParseTestImagesDefautDetection(DetectionType type, const QFileInfoList& list);
