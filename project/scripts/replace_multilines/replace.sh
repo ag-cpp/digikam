aa86b8c569914d40dfec84611f309bf6cf213825
Gilles Caulier <caulier.gilles@gmail.com>
2022-09-06 06:36:46 +0200

    add script to search and remplace mulitines text in source codes

---
+++ project/scripts/replace_multilines/replace.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2010-2020, Gilles Caulier, <caulier dot gilles at gmail dot com>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+find . -type f -name "*.cpp" \
+    -exec ./replace.py \
+      -F ./input.txt \
+      -T ./output.txt \
+      -i {} \;
+
+find . -type f -name "*.h" \
+    -exec ./replace.py \
+      -F ./input.txt \
+      -T ./output.txt \
+      -i {} \;
