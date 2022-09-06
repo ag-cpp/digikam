1220558c4fac6d4671218f4960ca23cde9c1378c
Gilles Caulier <caulier.gilles@gmail.com>
2022-09-06 11:02:31 +0200

    SPDX++

--- core/libs/progressmanager/progressmanager.h
+++ core/libs/progressmanager/progressmanager.h
@@ -10,16 +10,7 @@
  * Copyright (C) 2012      by Marcel Wiesweg <marcel dot wiesweg at gmx dot de>
  * Copyright (C) 2004      by Till Adam <adam at kde dot org>
  *
- * This program is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General
- * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * SPDX-License-Identifier: GPL-2.0-or-later
  *
  * ============================================================ */
 
