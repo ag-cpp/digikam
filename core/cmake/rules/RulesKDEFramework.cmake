d1d18dafe1442cd8e8399a0d435c5ef97d041b29
Gilles Caulier <caulier.gilles@gmail.com>
2022-09-09 15:28:35 +0200

    restore Sonnet detection

--- core/cmake/rules/RulesKDEFramework.cmake
+++ core/cmake/rules/RulesKDEFramework.cmake
@@ -22,6 +22,7 @@ find_package(KF5 ${KF5_MIN_VERSION} QUIET
                                     ThreadWeaver                # For Panorama tool.
                                     NotifyConfig                # Plasma desktop application notify configuration.
                                     Notifications               # Plasma desktop notifications integration.
+                                    Sonnet                      # For spell-checking.
 )
 
 if(ENABLE_KFILEMETADATASUPPORT)
