d7fca7c84b07de1da02f42556456750aea717602
Gilles Caulier <caulier.gilles@gmail.com>
2022-09-18 13:54:17 +0200

    SPDX++

--- core/libs/settings/applicationsettings_p.h
+++ core/libs/settings/applicationsettings_p.h
@@ -7,7 +7,7 @@
  * Description : application settings interface
  *
  * SPDX-FileCopyrightText: 2003-2022 by Gilles Caulier <caulier dot gilles at gmail dot com>
- * Copyright (C  2015      by Mohamed_Anwer <m_dot_anwer at gmx dot com>
+ * SPDX-FileCopyrightText: 2015      by Mohamed_Anwer <m_dot_anwer at gmx dot com>
  * SPDX-FileCopyrightText: 2017      by Simon Frei <freisim93 at gmail dot com>
  *
  * SPDX-License-Identifier: GPL-2.0-or-later
