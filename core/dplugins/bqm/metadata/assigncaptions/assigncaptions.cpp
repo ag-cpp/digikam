ec26b3c9c4190ef5cb07a8e68425646063e8942c
Yuri Chornoivan <yurchor@ukr.net>
2022-09-26 08:37:15 +0300

    Fix minor typos

--- core/dplugins/bqm/metadata/assigncaptions/assigncaptions.cpp
+++ core/dplugins/bqm/metadata/assigncaptions/assigncaptions.cpp
@@ -106,8 +106,8 @@ void AssignCaptions::registerSettingsWidget()
     d->captionsWidget->setTitleWidget(d->setCaptions);
     d->captionsWidget->slotEnabledInternalWidgets(false);
 
-    d->cleanupCB          = new QCheckBox(i18n("Cleanup up and overwrite"), vbox);
-    d->cleanupCB->setToolTip(i18nc("@info", "If you turn on this options, titles and captions"
+    d->cleanupCB          = new QCheckBox(i18n("Clean up and overwrite"), vbox);
+    d->cleanupCB->setToolTip(i18nc("@info", "If you turn on this options, titles and captions "
                                    "will be cleaned and replaced by the new values,\nelse "
                                    "old values will be merged with new values."));
 
