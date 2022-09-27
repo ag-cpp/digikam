4506dbf330803ec60efff3b7c9596c5abe45effe
Gilles Caulier <caulier.gilles@gmail.com>
2022-09-27 19:34:46 +0200

    fix directories removal

--- project/bundles/macports/03-build-digikam.sh
+++ project/bundles/macports/03-build-digikam.sh
@@ -86,7 +86,7 @@ FILES=$(find . | grep -E '(digikam|showfoto)')
 for FILE in $FILES ; do
     if [[ -f ${FILE} || -d ${FILE} ]] ; then
         echo -e "   ==> $INSTALL_PREFIX/${FILE} will be removed from previous install"
-        rm -f $INSTALL_PREFIX/${FILE}
+        rm -fr $INSTALL_PREFIX/${FILE}
     fi
 done
 
