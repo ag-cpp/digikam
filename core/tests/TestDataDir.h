7bc84a8d45529ebc418c379c7cab68b8181deca9
Gilles Caulier <caulier.gilles@gmail.com>
2022-10-13 16:50:22 +0200

    fix coding style

--- core/tests/TestDataDir.h
+++ core/tests/TestDataDir.h
@@ -12,8 +12,8 @@
  *
  * ============================================================ */
 
-#ifndef TEST_DATA_DIR_H
-#define TEST_DATA_DIR_H
+#ifndef DIGIKAM_TEST_DATA_DIR_H
+#define DIGIKAM_TEST_DATA_DIR_H
 
 #include <QDir>
 
@@ -28,78 +28,120 @@
 class TestDataDir
 {
 public:
-    /** True if the instance is correctly instantiated.
-     * Valid means that the desired root directory was located. */
-    bool isValid() { return _isValid; }
 
-    /** Root directory of test data hierarchy. */
-    QDir root() { return _testDataDir; }
-    
-    /** Path to any test file or directory, specified using relative path from root. */
-    QString path(QString name) { return root().filePath(name); }
+    /**
+     * True if the instance is correctly instantiated.
+     * Valid means that the desired root directory was located.
+     */
+    bool isValid()
+    {
+        return _isValid;
+    }
 
-    /** Any test directory, specified using relative path from root. */
-    QDir dir(QString relPath) { return QDir(path(relPath)); }
+    /**
+     * Root directory of test data hierarchy.
+     */
+    QDir root()
+    {
+        return _testDataDir;
+    }
 
-    /** Any test file, specified using relative path from root. */
-    QFile file(QString name) { return QFile(path(name)); }
-    
-    /** Returns TestDataDir for the Digikam Test Data root directory.
-     *  This provides access to all files in Digikam Test Data.
+    /**
+     * Path to any test file or directory, specified using relative path from root.
+     */
+    QString path(const QString& name)
+    {
+        return root().filePath(name);
+    }
+
+    /**
+     * Any test directory, specified using relative path from root.
+     */
+    QDir dir(const QString& relPath)
+    {
+        return QDir(path(relPath));
+    }
+
+    /**
+     * Any test file, specified using relative path from root.
+     */
+    QFile file(const QString& name)
+    {
+        return QFile(path(name));
+    }
+
+    /**
+     * Returns TestDataDir for the Digikam Test Data root directory.
+     * This provides access to all files in Digikam Test Data.
      */
     static TestDataDir TestDataRoot()
     {
         return TestDataDir();
     }
 
-    /** Returns TestDataDir for a sub-tree of the Digikam Test Data.
-     *  This provides access to files in the subtree.
+    /**
+     * Returns TestDataDir for a sub-tree of the Digikam Test Data.
+     * This provides access to files in the subtree.
      * 
      * \param subdirPath path of subdir, relative to the Digikam Test Data root
      */
-    static TestDataDir TestData(QString subdirPath)
+    static TestDataDir TestData(const QString& subdirPath)
     {
         return TestDataDir(subdirPath);
     }
-    
-private:
-    QDir _testDataDir;
-    bool _isValid;
 
-    static bool FindDirectoryUpwards(QDir dir, QString target, QDir& result)
+private:
+
+    QDir _testDataDir;
+    bool _isValid = false;
+
+private:
+
+    static bool FindDirectoryUpwards(const QDir& dir, const QString& target, QDir& result)
     {
         while (dir.exists(target) == false)
         {
-            if (!dir.cdUp()) return false;
+            if (!dir.cdUp())
+                return false;
         }
 
-        if (!dir.cd(target)) return false;
-        
+        if (!dir.cd(target))
+            return false;
+
         result = dir;
+
         return true;
     }
-    
+
     void Initialize()
     {
         _isValid = FindDirectoryUpwards(QDir(), QString::fromUtf8("test-data"), _testDataDir);
     }
-    
+
 protected:
-    /** Create */
+
+    /**
+     * Create
+     */
     TestDataDir()
     {
         Initialize();
     }
-        
+
     TestDataDir(QString subdirPath)
     {
         Initialize();
-        if (!_isValid) return;
-        
+
+        if (!_isValid)
+            return;
+
         _isValid = _testDataDir.exists(subdirPath);
-        if (!_isValid) return;
+
+        if (!_isValid)
+            return;
+
         _testDataDir.cd(subdirPath);
     }
 };
 
-#endif
+#endif // DIGIKAM_TEST_DATA_DIR
