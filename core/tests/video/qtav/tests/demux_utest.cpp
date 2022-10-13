92d40118af08572444e18abcadaa83e6b622b79e
Steve Robbins <steve@sumost.ca>
2022-10-12 20:16:09 -0500

    Test demux with existing 4 file formats.

--- core/tests/video/qtav/tests/demux_utest.cpp
+++ core/tests/video/qtav/tests/demux_utest.cpp
@@ -33,6 +33,77 @@ private:
     QtAvTestDataDir _testDataDir;
     QString testFile1() { return _testDataDir.basemediav1_mp4(); }
 
+    void checkPackets(QString filename, int audioFrames, int videoFrames)
+    {
+        AVDemuxer demux;
+        QVERIFY(demux.setMedia(filename));
+        QVERIFY(demux.load());
+
+        QCOMPARE(demux.audioStreams().size(), 1);
+        QCOMPARE(demux.videoStreams().size(), 1);
+        QCOMPARE(demux.subtitleStreams().size(), 0);
+        
+        int audioStreamNum = demux.audioStream();
+        int videoStreamNum = demux.videoStream();
+
+        QCOMPARE(demux.frames(demux.audioStream()), audioFrames);
+        QCOMPARE(demux.frames(demux.videoStream()), videoFrames);
+
+        QCOMPARE(demux.startTime(), demux.startTimeUs() / 1000);
+        QCOMPARE(demux.duration(), demux.durationUs() / 1000);
+
+        qreal audio_dts = -1;
+        qreal video_dts = -1;
+        
+        int audioFrameCount = 0;
+        int videoFrameCount = 0;
+        while(demux.readFrame())
+        {
+            std::stringstream ss;
+
+            if (demux.stream() == audioStreamNum)
+            {
+                ++audioFrameCount;
+                ss << "failed at audio frame " << audioFrameCount;
+            }
+            else if (demux.stream() == videoStreamNum)
+            {
+                ++videoFrameCount;
+                ss << "failed at video frame " << videoFrameCount;
+            }
+            else
+            {
+                QFAIL("read unknown stream");
+            }
+
+            auto temp = ss.str();
+            auto errMsg = temp.c_str();
+
+            QVERIFY2(!demux.atEnd(), errMsg);
+
+            auto p = demux.packet();
+            QVERIFY2(p.isValid(), errMsg);
+            QVERIFY2(!p.isCorrupt, errMsg);
+            
+            // decoding time stamps (DTS) should be sequential
+            if (demux.stream() == audioStreamNum)
+            {
+                QVERIFY2(p.dts >= audio_dts, errMsg);
+                audio_dts = p.dts;
+            }
+            else
+            {
+                QVERIFY2(p.dts >= video_dts, errMsg);
+                video_dts = p.dts;
+            }
+        }
+
+        QCOMPARE(audioFrameCount, audioFrames); 
+        QCOMPARE(videoFrameCount, videoFrames);
+        QVERIFY(demux.atEnd());
+        // still loaded? QCOMPARE(demux.mediaStatus(), MediaStatus::EndOfMedia);
+    }
+
     
 private Q_SLOTS:
 
@@ -80,81 +151,11 @@ private Q_SLOTS:
         QCOMPARE(demux.fileName(), testFile1());
         QCOMPARE(demux.isLoaded(), false);
     }
-
-    void readPackets()
-    {
-        AVDemuxer demux;
-        QVERIFY(demux.setMedia(testFile1()));
-        QVERIFY(demux.load());
-
-        QCOMPARE(demux.audioStreams().size(), 1);
-        QCOMPARE(demux.videoStreams().size(), 1);
-        QCOMPARE(demux.subtitleStreams().size(), 0);
-        
-        int audioStreamNum = demux.audioStream();
-        int videoStreamNum = demux.videoStream();
-
-        QCOMPARE(demux.frames(demux.audioStream()), 236);
-        QCOMPARE(demux.frames(demux.videoStream()), 150);
-
-        QCOMPARE(demux.startTime(), demux.startTimeUs() / 1000);
-        QCOMPARE(demux.duration(), demux.durationUs() / 1000);
-
-        qreal audio_dts = -1;
-        qreal video_dts = -1;
-        
-        int audioFrameCount = 0;
-        int videoFrameCount = 0;
-        while(demux.readFrame())
-        {
-            std::stringstream ss;
-
-            if (demux.stream() == audioStreamNum)
-            {
-                ++audioFrameCount;
-                ss << "failed at audio frame " << audioFrameCount;
-            }
-            else if (demux.stream() == videoStreamNum)
-            {
-                ++videoFrameCount;
-                ss << "failed at video frame " << videoFrameCount;
-            }
-            else
-            {
-                QFAIL("read unknown strem");
-            }
-
-            auto temp = ss.str();
-            auto errMsg = temp.c_str();
-
-            QVERIFY2(!demux.atEnd(), errMsg);
-
-            auto p = demux.packet();
-            QVERIFY2(p.isValid(), errMsg);
-            QVERIFY2(!p.isCorrupt, errMsg);
-            
-            // decoding time stamps (DTS) should be sequential
-            if (demux.stream() == audioStreamNum)
-            {
-                QVERIFY2(p.dts >= audio_dts, errMsg);
-                audio_dts = p.dts;
-            }
-            else
-            {
-                QVERIFY2(p.dts >= video_dts, errMsg);
-                video_dts = p.dts;
-            }
-        }
-
-        QVERIFY(demux.atEnd());
-
-        QCOMPARE(demux.frames(demux.audioStream()), 236);
-        QCOMPARE(demux.frames(demux.videoStream()), 150);
-
-        QCOMPARE(audioFrameCount, 236); 
-        QCOMPARE(videoFrameCount, 150);
-        // still loaded? QCOMPARE(demux.mediaStatus(), MediaStatus::EndOfMedia);
-    }
+    
+    void checkBasemedia() { checkPackets(_testDataDir.basemediav1_mp4(), 236, 150); }
+    void checkRiffMpeg() { checkPackets(_testDataDir.riffMpeg_avi(), 210, 151); }
+    void checkMpeg2() { checkPackets(_testDataDir.mpeg2_mp4(), 236, 150); }
+    void checkMpeg4() { checkPackets(_testDataDir.mpeg4_mp4(), 236, 150); }
 };
 
 QTEST_MAIN(DemuxTest)
