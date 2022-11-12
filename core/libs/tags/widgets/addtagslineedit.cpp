ac5e05caa849831adacbd07d88a40f8e6747a6f5
Maik Qualmann <metzpinguin@gmail.com>
2022-11-12 07:43:46 +0100

    fix connections to the QCompleter Krazy should be happy with this solution too. BUGS: 461710 FIXED-IN: 8.0.0

--- core/libs/tags/widgets/addtagslineedit.cpp
+++ core/libs/tags/widgets/addtagslineedit.cpp
@@ -72,14 +72,14 @@ AddTagsLineEdit::AddTagsLineEdit(QWidget* const parent)
             this, SLOT(slotTextEdited(QString)));
 
     connect(d->completer, QOverload<const TaggingAction&>::of(&TagCompleter::signalActivated),
-            this, [=](const TaggingAction& action)
+            d->completer, [=](const TaggingAction& action)
             {
                 completerActivated(action);
             }
     );
 
     connect(d->completer, QOverload<const TaggingAction&>::of(&TagCompleter::signalHighlighted),
-            this, [=](const TaggingAction& action)
+            d->completer, [=](const TaggingAction& action)
             {
                 completerHighlighted(action);
             }
