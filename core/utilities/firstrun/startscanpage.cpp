8f7499f1e5ef3e957189c5560776d0e053548a74
Gilles Caulier <caulier.gilles@gmail.com>
2023-02-01 08:04:04 +0100

    link to online doc

--- core/utilities/firstrun/startscanpage.cpp
+++ core/utilities/firstrun/startscanpage.cpp
@@ -43,7 +43,7 @@ StartScanPage::StartScanPage(QWizard* const dlg)
                        "To learn more about the digiKam world, we recommend reading the <b>digiKam handbook</b> "
                        "using the <b>Help/Handbook</b> menu entry (you need to install the separate digiKam documentation package.) "
                        "You can also read the manual online from the "
-                       "<a href='https://www.digikam.org/docs'>digikam.org website</a>.</p>"
+                       "<a href='https://docs.digikam.org/en/index.html'>dedicated website</a>.</p>"
                        "<p>Press <b>Finish</b> to close this assistant. digiKam will scan your "
                        "collection to register all albums in the database. Items including in "
                        "albums are collected after startup stage in background.</p>"
