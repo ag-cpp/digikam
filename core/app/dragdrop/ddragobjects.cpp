3646129dfff77178b22e1ac21068340bd0789211
Maik Qualmann <metzpinguin@gmail.com>
2023-03-08 07:55:19 +0100

    add file paths in text/plain format for clipboard actions BUGS: 467013 BUGS: 443682 CCBUGS: 467024 FIXED-IN: 7.10.0

--- core/app/dragdrop/ddragobjects.cpp
+++ core/app/dragdrop/ddragobjects.cpp
@@ -28,6 +28,10 @@
 #include <QByteArray>
 #include <QDataStream>
 
+// Local includes
+
+#include "digikam_config.h"
+
 namespace Digikam
 {
 
@@ -54,6 +58,25 @@ DItemDrag::DItemDrag(const QList<QUrl>& urls,
     QDataStream ds4(&ba4, QIODevice::WriteOnly);
     ds4 << imageIDs;
     setData(QLatin1String("digikam/item-ids"), ba4);
+
+    QString txt;
+
+    foreach (const QUrl& url, urls)
+    {
+
+#ifdef Q_OS_WIN
+
+    txt.append(QString::fromUtf8("%1\r\n").arg(url.toLocalFile()));
+
+#else
+
+    txt.append(QString::fromUtf8("%1\n").arg(url.toLocalFile()));
+
+#endif
+
+    }
+
+    setText(txt);
 
     // commonly accessible mime data, for dragging to outside digikam
 
