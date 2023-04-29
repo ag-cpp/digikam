9d2a939f5d3c99da4d9e20097bfe0ea2ad887358
Maik Qualmann <metzpinguin@gmail.com>
2023-04-29 21:43:44 +0200

    polish

--- core/utilities/advancedrename/parser/options/sequencenumberoption.cpp
+++ core/utilities/advancedrename/parser/options/sequencenumberoption.cpp
@@ -140,7 +140,7 @@ void SequenceNumberOption::slotTokenTriggered(const QString& token)
 
             if (!randomAware && (step > 1))
             {
-                result.append(QString::fromUtf8(",%1").arg(QString::number(step)));
+                result.append(QString::fromUtf8(",%1").arg(step));
             }
 
             result.append(QLatin1Char(']'));
