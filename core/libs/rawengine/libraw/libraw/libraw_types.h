b317600202f766628af1180ebc77cef5dc0456d7
Gilles Caulier <caulier.gilles@gmail.com>
2023-07-30 21:37:43 +0200

    update internal libraw to snapshot 20/07/2023

--- core/libs/rawengine/libraw/libraw/libraw_types.h
+++ core/libs/rawengine/libraw/libraw/libraw_types.h
@@ -102,8 +102,8 @@ typedef unsigned long long UINT64;
 
   typedef unsigned char uchar;
   typedef unsigned short ushort;
-/*
-#ifdef LIBRAW_WIN32_DLLDEFS
+
+/*#ifdef LIBRAW_WIN32_DLLDEFS
 #ifdef LIBRAW_NODLL
 #define DllDef
 #else
