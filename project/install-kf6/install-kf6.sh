a22b6d795c6d8e18c0f7afdfd19cca70f1ce5267
Gilles Caulier <caulier.gilles@gmail.com>
2023-09-03 08:58:03 +0200

    install KF6 must be based on source code from git master, not the offcial download tarballs

---
+++ project/install-kf6/install-kf6.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Script to build extra libraries using Linux host.
+# This script must be run as sudo
+#
+# SPDX-FileCopyrightText: 2015-2023 by Gilles Caulier  <caulier dot gilles at gmail dot com>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+# Halt and catch errors
+set -eE
+trap 'PREVIOUS_COMMAND=$THIS_COMMAND; THIS_COMMAND=$BASH_COMMAND' DEBUG
+trap 'echo "FAILED COMMAND: $PREVIOUS_COMMAND"' ERR
+
+#################################################################################################
+# Manage script traces to log file
+
+mkdir -p ./logs
+exec > >(tee ./logs/build-extralibs.full.log) 2>&1
+
+#################################################################################################
+# Pre-processing checks
+
+. ./common.sh
+. ./config.sh
+ChecksRunAsRoot
+StartScript
+ChecksCPUCores
+HostAdjustments
+RegisterRemoteServers
+
+ORIG_WD="`pwd`"
+
+# Create the build dir for the 3rdparty deps
+if [ ! -d $BUILDING_DIR ] ; then
+    mkdir $BUILDING_DIR
+fi
+
+if [ ! -d $DOWNLOAD_DIR ] ; then
+    mkdir $DOWNLOAD_DIR
+fi
+
+#################################################################################################
+
+cd $BUILDING_DIR
+
+rm -rf $BUILDING_DIR/* || true
+
+cmake $ORIG_WD \
+      -DCMAKE_INSTALL_PREFIX:PATH=/usr \
+      -DINSTALL_ROOT=/usr \
+      -DEXTERNALS_DOWNLOAD_DIR=$DOWNLOAD_DIR \
+      -DEXTERNALS_BUILD_DIR=$BUILDING_DIR \
+      -DKA_VERSION=$DK_KA_VERSION \
+      -DKP_VERSION=$DK_KP_VERSION \
+      -DKDE_VERSION=$DK_KDE_VERSION \
+      -DENABLE_QTVERSION=$DK_QTVERSION \
+      -Wno-dev
+
+cmake --build . --config RelWithDebInfo --target ext_kf6_frameworks      -- -j$CPU_CORES
+
+#################################################################################################
+
+TerminateScript
