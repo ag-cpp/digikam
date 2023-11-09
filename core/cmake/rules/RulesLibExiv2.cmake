ba6e8a02c3fe93bc42ea7492d7aa8bf360af543c
Maik Qualmann <metzpinguin@gmail.com>
2023-11-09 21:35:09 +0100

    set correct exiv2 package name and less duplicate CMake code

--- core/cmake/rules/RulesLibExiv2.cmake
+++ core/cmake/rules/RulesLibExiv2.cmake
@@ -9,19 +9,21 @@ find_package(LibExiv2 OPTIONAL_COMPONENTS)
 if(LibExiv2_FOUND AND ("${LibExiv2_VERSION}" VERSION_GREATER "0.27.99"))
 
     find_package(LibExiv2 REQUIRED)
+    set(EXIV2_PACKAGE_NAME "LibExiv2")
 
 else()
 
-    set (LibExiv2_FOUND FALSE)
+    set(LibExiv2_FOUND FALSE)
     find_package(exiv2 REQUIRED)
+    set(EXIV2_PACKAGE_NAME "exiv2")
 
 endif()
 
-set_package_properties("exiv2"     PROPERTIES
-                       DESCRIPTION "Required to build digiKam"
-                       URL         "https://www.exiv2.org"
-                       TYPE        RECOMMENDED
-                       PURPOSE     "Library to manage image metadata"
+set_package_properties(${EXIV2_PACKAGE_NAME} PROPERTIES
+                       DESCRIPTION           "Required to build digiKam"
+                       URL                   "https://www.exiv2.org"
+                       TYPE                  RECOMMENDED
+                       PURPOSE               "Library to manage image metadata"
 )
 
 if(LibExiv2_FOUND)
@@ -29,16 +31,6 @@ if(LibExiv2_FOUND)
     if("${LibExiv2_VERSION}" VERSION_LESS ${EXIV2_MIN_VERSION})
 
         message(FATAL_ERROR "Exiv2 version is too old (${LibExiv2_VERSION})! Minimal version required:${EXIV2_MIN_VERSION}.")
-
-    endif()
-
-    if("${LibExiv2_VERSION}" VERSION_LESS "0.27.99")
-
-        set(EXIV2_CXX_STANDARD 11)
-
-    else()
-
-        set(EXIV2_CXX_STANDARD 17)
 
     endif()
 
@@ -50,14 +42,14 @@ else()
 
     endif()
 
-    if("${exiv2_VERSION}" VERSION_LESS "0.27.99")
+endif()
 
-        set(EXIV2_CXX_STANDARD 11)
+if(("${exiv2_VERSION}" VERSION_GREATER "0.27.99") OR ("${LibExiv2_VERSION}" VERSION_GREATER "0.27.99"))
 
-    else()
+    set(EXIV2_CXX_STANDARD 17)
 
-        set(EXIV2_CXX_STANDARD 17)
+else()
 
-    endif()
+    set(EXIV2_CXX_STANDARD 11)
 
 endif()
