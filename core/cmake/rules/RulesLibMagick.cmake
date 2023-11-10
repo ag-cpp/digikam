35146f7f42e63a617e4c46532de948ca1a4c21c6
Gilles Caulier <caulier.gilles@gmail.com>
2023-11-10 08:05:32 +0100

    start to handle GM instead IM. Not yet completed

--- core/cmake/rules/RulesLibMagick.cmake
+++ core/cmake/rules/RulesLibMagick.cmake
@@ -1,5 +1,5 @@
 #
-# SPDX-FileCopyrightText: 2010-2022 by Gilles Caulier, <caulier dot gilles at gmail dot com>
+# SPDX-FileCopyrightText: 2010-2023 by Gilles Caulier, <caulier dot gilles at gmail dot com>
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -17,5 +17,33 @@ endif()
 if(ImageMagick_FOUND)
 
     include_directories(${ImageMagick_INCLUDE_DIRS})
+
+else()
+
+    # Fail back to GraphicsMagick if available
+
+    pkg_check_modules(MAGICK_PLUS GraphicsMagick++)
+    pkg_check_modules(MAGICK_CORE GraphicsMagick)
+    pkg_check_modules(MAGICK_WAND GraphicsMagickWand)
+
+    if(MAGICK_PLUS_FOUND AND MAGICK_CORE_FOUND AND MAGICK_WAND_FOUND)
+
+        message(STATUS "GraphicsMagick API used instead ImageMagick")
+        set(ImageMagick_FOUND TRUE)
+
+        message(STATUS "GraphicsMagick definitions: ${MAGICK_PLUS_DEFINITIONS}")
+        add_definitions(${MAGICK_PLUS_DEFINITIONS})
+
+        message(STATUS "GraphicsMagick includes: ${MAGICK_CORE_DEFINITIONS}")
+        include_directories(${MAGICK_CORE_INCLUDE_DIRS})
+
+    endif()
+
+#   ImageMagick_FOUND                  - TRUE if all components are found.
+#   ImageMagick_EXECUTABLE_DIR         - Full path to executables directory.
+#   ImageMagick_<component>_FOUND      - TRUE if <component> is found.
+#   ImageMagick_<component>_EXECUTABLE - Full path to <component> executable.
+#   ImageMagick_VERSION_STRING         - the version of ImageMagick found
+
 
 endif()
