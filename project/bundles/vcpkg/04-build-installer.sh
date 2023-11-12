d4e04aa2b4b0c03c6188a3b3249ca6985dd4c7c4
Gilles Caulier <caulier.gilles@gmail.com>
2023-11-12 10:54:38 +0100

    no need debug mode with scp

--- project/bundles/vcpkg/04-build-installer.sh
+++ project/bundles/vcpkg/04-build-installer.sh
@@ -334,12 +334,12 @@ if [[ $DK_UPLOAD = 1 ]] ; then
 
     echo -e "---------- Upload new Windows bundle files to files.kde.org repository \n"
 
-    scp -v $ORIG_WD/bundle/$TARGET_INSTALLER $DK_UPLOADURL:$DK_UPLOADDIR
-    scp -v $ORIG_WD/bundle/$PORTABLE_FILE $DK_UPLOADURL:$DK_UPLOADDIR
+    scp $ORIG_WD/bundle/$TARGET_INSTALLER $DK_UPLOADURL:$DK_UPLOADDIR
+    scp $ORIG_WD/bundle/$PORTABLE_FILE $DK_UPLOADURL:$DK_UPLOADDIR
 
     if [[ $DK_SIGN = 1 ]] ; then
-        scp -v $ORIG_WD/bundle/$TARGET_INSTALLER.sig $DK_UPLOADURL:$DK_UPLOADDIR
-        scp -v $ORIG_WD/bundle/$PORTABLE_FILE.sig $DK_UPLOADURL:$DK_UPLOADDIR
+        scp $ORIG_WD/bundle/$TARGET_INSTALLER.sig $DK_UPLOADURL:$DK_UPLOADDIR
+        scp $ORIG_WD/bundle/$PORTABLE_FILE.sig $DK_UPLOADURL:$DK_UPLOADDIR
     fi
 
     # update remote files list
@@ -350,7 +350,7 @@ if [[ $DK_UPLOAD = 1 ]] ; then
     rm $ORIG_WD/bundle/ls.tmp
     rm $ORIG_WD/bundle/ls.txt
     sftp -q $DK_UPLOADURL:$DK_UPLOADDIR <<< "rm FILES"
-    scp -v $BUILDDIR/bundle/FILES $DK_UPLOADURL:$DK_UPLOADDIR
+    scp $BUILDDIR/bundle/FILES $DK_UPLOADURL:$DK_UPLOADDIR
 
 else
 
