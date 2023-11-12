5fc53379a7dd036abcc306643f67a123144272f2
Gilles Caulier <caulier.gilles@gmail.com>
2023-11-12 17:22:55 +0100

    another missing qt component for KF6

--- project/bundles/vcpkg/01-build-vcpkg.sh
+++ project/bundles/vcpkg/01-build-vcpkg.sh
@@ -118,6 +118,7 @@ $VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install qtimageformats
 $VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install qttools
 $VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install qtsvg
 $VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install qtscxml
+$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install qtspeech
 $VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install qtwebengine
 $VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install qt5compat
 
@@ -136,7 +137,6 @@ $VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install gperf
 $VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install libsnoretoast
 $VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install hunspell
 $VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install lensfun
-$VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install graphicsmagick
 $VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install openal-soft
 $VCPKG_DIR/vcpkg ${VCPKG_COMMON_OPTIONS[@]} install curl
 
@@ -147,52 +147,6 @@ cp -f ../../scripts/create_manifest.sh $VCPKG_DIR
 cd $VCPKG_DIR
 $VCPKG_DIR/create_manifest.sh $VCPKG_DIR vcpkg
 cp $VCPKG_DIR/vcpkg_manifest.txt $ORIG_WD/data/
-
-if [ ] ; then
-
-#################################################################################################
-
-echo -e "\n"
-echo "---------- Building digiKam 3rd-party dependencies with VCPKG"
-
-# Create the build dir for the 3rdparty deps
-
-if [ ! -d $BUILDING_DIR/dk_cmake ] ; then
-    mkdir -p $BUILDING_DIR/dk_cmake
-fi
-
-if [ ! -d $DOWNLOAD_DIR ] ; then
-    mkdir -p $DOWNLOAD_DIR
-fi
-
-cd $BUILDING_DIR/dk_cmake
-rm -rf $BUILDING_DIR/dk_cmake/* || true
-
-cmake $ORIG_WD/../3rdparty \
-                           -DCMAKE_TOOLCHAIN_FILE=$VCKPG_DIR/scripts/buildsystems/vcpkg.cmake \
-                           -DVCPKG_TARGET_TRIPLET=$VCPKG_TRIPLET \
-                           -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-                           -DCMAKE_COLOR_MAKEFILE=ON \
-                           -DCMAKE_INSTALL_PREFIX=$VCPKG_INSTALL_PREFIX \
-                           -DINSTALL_ROOT=$VCPKG_INSTALL_PREFIX \
-                           -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
-                           -DEXTERNALS_DOWNLOAD_DIR=$DOWNLOAD_DIR \
-                           -DKA_VERSION=$DK_KA_VERSION \
-                           -DKP_VERSION=$DK_KP_VERSION \
-                           -DKDE_VERSION=$DK_KDE_VERSION
-
-#                           -DCMAKE_FIND_PREFIX_PATH=${CMAKE_PREFIX_PATH} \
-#                           -DCMAKE_SYSTEM_INCLUDE_PATH=${CMAKE_PREFIX_PATH}/include \
-#                           -DCMAKE_INCLUDE_PATH=${CMAKE_PREFIX_PATH}/include \
-#                           -DCMAKE_LIBRARY_PATH=${CMAKE_PREFIX_PATH}/lib \
-
-# Low level libraries
-# NOTE: The order to compile each component here is very important.
-
-cmake --build . --parallel --config RelWithDebInfo --target ext_imagemagick
-
-#################################################################################################
-fi
 
 export PATH=$ORIG_PATH
 
