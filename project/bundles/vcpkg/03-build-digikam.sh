c7cf3e04306a78f9dfdcf795e11e387d55a8349e
Gilles Caulier <caulier.gilles@gmail.com>
2023-11-20 09:22:26 +0100

    compile Exiv2 stable just after KF6

--- project/bundles/vcpkg/03-build-digikam.sh
+++ project/bundles/vcpkg/03-build-digikam.sh
@@ -84,8 +84,6 @@ cmake $ORIG_WD/../3rdparty \
       -DENABLE_QTWEBENGINE=$DK_QTWEBENGINE \
       -Wno-dev
 
-cmake --build . --config RelWithDebInfo --target ext_exiv2 --parallel
-cp $DOWNLOAD_DIR/exiv2_manifest.txt $ORIG_WD/data/
 cmake --build . --config RelWithDebInfo --target ext_lensfun --parallel
 cp $DOWNLOAD_DIR/lensfun_manifest.txt $ORIG_WD/data/
 
