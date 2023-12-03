92bcd7b6e16139c91cbf24ff87feb073c35dc483
Gilles Caulier <caulier.gilles@gmail.com>
2023-12-03 10:39:37 +0100

    set VS DLL version

--- project/bundles/vcpkg/04-build-installer.sh
+++ project/bundles/vcpkg/04-build-installer.sh
@@ -304,15 +304,17 @@ done
 
 echo -e "\n---------- Copy redistributable VSCommunity compatibility dlls"
 
+VS_DLL_VERSION=140
+
 VS_DLL_FILES="\
-$VCPKG_INSTALL_PREFIX/bin/msvcp140.dll              \
-$VCPKG_INSTALL_PREFIX/bin/msvcp140_1.dll            \
-$VCPKG_INSTALL_PREFIX/bin/msvcp140_2.dll            \
-$VCPKG_INSTALL_PREFIX/bin/msvcp140_atomic_wait.dll  \
-$VCPKG_INSTALL_PREFIX/bin/msvcp140_codecvt_ids.dll  \
-$VCPKG_INSTALL_PREFIX/bin/vcruntime140_1.dll        \
-$VCPKG_INSTALL_PREFIX/bin/vcruntime140.dll          \
-$VCPKG_INSTALL_PREFIX/bin/concrt140.dll             \
+$VCPKG_INSTALL_PREFIX/bin/msvcp$VS_DLL_VERSION.dll              \
+$VCPKG_INSTALL_PREFIX/bin/msvcp$VS_DLL_VERSION_1.dll            \
+$VCPKG_INSTALL_PREFIX/bin/msvcp$VS_DLL_VERSION_2.dll            \
+$VCPKG_INSTALL_PREFIX/bin/msvcp$VS_DLL_VERSION_atomic_wait.dll  \
+$VCPKG_INSTALL_PREFIX/bin/msvcp$VS_DLL_VERSION_codecvt_ids.dll  \
+$VCPKG_INSTALL_PREFIX/bin/vcruntime$VS_DLL_VERSION_1.dll        \
+$VCPKG_INSTALL_PREFIX/bin/vcruntime$VS_DLL_VERSION.dll          \
+$VCPKG_INSTALL_PREFIX/bin/concrt$VS_DLL_VERSION.dll             \
 "
 
 for vsdll in $VS_DLL_FILES ; do
@@ -324,9 +326,10 @@ done
 
 # Copy also the vcomp dlls for Windows 11 support
 
+# To prevent broken array parsing of paths with spaces.
 IFS=$'\n'
 
-VS_DLL_COMP="`find "/c/Program Files/Microsoft Visual Studio/" -name "vcomp140*.dll" -type f | grep 'x64/' | grep 'OpenMP' | grep -v 'onecore'`"
+VS_DLL_COMP="`find "/c/Program Files/Microsoft Visual Studio/" -name "vcomp$VS_DLL_VERSION*.dll" -type f | grep 'x64/' | grep 'OpenMP' | grep -v 'onecore'`"
 
 for vscompdll in $VS_DLL_COMP ; do
 
