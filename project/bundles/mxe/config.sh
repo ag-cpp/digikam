20cc278be028576d6a92ca22252619e249c8529e
Gilles Caulier <caulier.gilles@gmail.com>
2023-12-03 16:03:08 +0100

    switch to master

--- project/bundles/mxe/config.sh
+++ project/bundles/mxe/config.sh
@@ -85,8 +85,8 @@ DK_QTVERSION="5"
 # digiKam tag version from git. Official tarball do not include extra shared libraries.
 # The list of tags can be listed with this url: https://invent.kde.org/graphics/digikam/-/tags
 # If you want to package current implementation from git, use "master" as tag.
-DK_VERSION=v8.2.0
-#DK_VERSION=master
+#DK_VERSION=v8.2.0
+DK_VERSION=master
 #DK_VERSION=qt5-maintenance
 
 # Installer sub version to differentiates newer updates of the installer itself, even if the underlying application hasn’t changed.
