b98b9082ed2c19b36832984089e5a9112589eab3
Gilles Caulier <caulier.gilles@gmail.com>
2023-12-23 13:20:29 +0100

    reuse++

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,8 @@
-#! /bin/sh
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2008-2023 by Gilles Caulier, <caulier dot gilles at gmail dot com>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
 $EXTRACTRC `find ./core -name \*.rc -o -name \*.ui | grep -v '/tests/'` >> rc.cpp || exit 11
 $XGETTEXT rc.cpp `find ./core -name \*.h -o -name \*.cpp | grep -v '/tests/'` `find ./core/app -name \*.h.cmake.in` -o $podir/digikam.pot
