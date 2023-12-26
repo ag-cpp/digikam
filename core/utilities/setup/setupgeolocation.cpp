546852b999e0dfd8c5908c187cdbd1bb42c0f73b
Gilles Caulier <caulier.gilles@gmail.com>
2023-12-26 11:49:21 +0100

    Prepare the Setup Geolocation to host the GoogleMaps API key Not yet suitable

--- core/utilities/setup/setupgeolocation.cpp
+++ core/utilities/setup/setupgeolocation.cpp
@@ -17,7 +17,12 @@
 // Qt includes
 
 #include <QApplication>
-#include <QLayout>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QFileInfo>
+#include <QFile>
+#include <QStandardPaths>
+#include <QLineEdit>
 #include <QPushButton>
 #include <QTabWidget>
 #include <QList>
@@ -43,14 +48,28 @@ public:
 
     Private() = default;
 
-    MarbleConfigView* tab = nullptr;
+    MarbleConfigView* tab  = nullptr;
+    QWidget* gMapView      = nullptr;
+    QLineEdit* gMapApi     = nullptr;
+
+public:
+/*
+    QString htmlFilePath() const
+    {
+        QString htmlPath = QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation);
+        htmlPath        += QLatin1String("/digikam/geoiface/");
+        QString htmlFile = htmlPath + d->htmlFileName;
+
+        return htmlFile;
+    }
+*/
 };
 
 SetupGeolocation::SetupGeolocation(QWidget* const parent)
     : QScrollArea(parent),
       d          (new Private)
 {
-    QWidget* const panel   = new QWidget(viewport());
+    QWidget* const panel    = new QWidget(viewport());
     setWidget(panel);
     setWidgetResizable(true);
 
@@ -58,6 +77,21 @@ SetupGeolocation::SetupGeolocation(QWidget* const parent)
     panel->setLayout(vbl);
 
     d->tab                 = new MarbleConfigView(MarbleSettings::instance()->mainMarbleWidget(), this);
+
+    // ---
+
+    d->gMapView             = new QWidget(this);
+    QLabel* const title     = new QLabel(i18n("API Key:"), d->gMapView);
+    d->gMapApi              = new QLineEdit(d->gMapView);
+    d->gMapApi->setEchoMode(QLineEdit::Normal);
+
+    QVBoxLayout* const vlay = new QVBoxLayout(d->gMapView);
+    vlay->addWidget(title);
+    vlay->addWidget(d->gMapApi);
+    vlay->addStretch();
+    d->tab->insertTab(GoogleMaps, d->gMapView, i18n("Google Maps"));
+
+    // ---
 
     vbl->addWidget(d->tab);
 
@@ -87,11 +121,86 @@ SetupGeolocation::GeolocationTab SetupGeolocation::activeTab() const
 void SetupGeolocation::applySettings()
 {
     d->tab->applySettings();
+/*
+    QString key      = d->gMapApi->text().trimmed();
+    QString htmlFile = htmlFilePath();
+
+    if (key.isEmpty())
+    {
+        if (QFileInfo::exists(htmlFile))
+        {
+            QFile::remove(htmlFile);
+        }
+
+        const QUrl htmlUrl = GeoIfaceGlobalObject::instance()->locateDataFile(d->htmlFileName);
+        d->htmlWidget->load(htmlUrl);
+
+        return;
+    }
+
+    QString jsFile = QStandardPaths::locate(QStandardPaths::GenericDataLocation,
+                                            QLatin1String("digikam/geoiface/backend-googlemaps-js.js"));
+
+    if (!QFileInfo::exists(jsFile))
+    {
+        return;
+    }
+
+    QString htmlText = d->htmlTemplate.arg(key).arg(QUrl::fromLocalFile(jsFile).toString());
+
+    if (!QFileInfo::exists(htmlPath))
+    {
+        QDir().mkpath(htmlPath);
+    }
+
+    QFile writeFile(htmlFile);
+
+    if (writeFile.open(QIODevice::WriteOnly))
+    {
+        writeFile.write(htmlText.toLatin1());
+        d->keyChanged = true;
+        writeFile.close();
+    }
+*/
 }
 
 void SetupGeolocation::readSettings()
 {
     d->tab->readSettings();
+
+    // Read Google API key
+/*
+    QString htmlFile = htmlFilePath();
+    QString oldKey;
+
+    if (QFileInfo::exists(htmlFile))
+    {
+        QFile readFile(htmlFile);
+
+        if (readFile.open(QIODevice::ReadOnly))
+        {
+            QString oldHtml = QString::fromLatin1(readFile.readAll());
+            readFile.close();
+
+            if (!oldHtml.isEmpty())
+            {
+                int firstIdx = oldHtml.indexOf(QLatin1String("key="));
+
+                if (firstIdx != -1)
+                {
+                    int lastIdx = oldHtml.indexOf(QLatin1String("\"></script>"), firstIdx);
+
+                    if (lastIdx > (firstIdx + 4))
+                    {
+                        oldKey = oldHtml.mid(firstIdx + 4, lastIdx - firstIdx - 4);
+                    }
+                }
+            }
+        }
+    }
+
+    d->gMapApi->setText(oldKey);
+*/
 }
 
 } // namespace Digikam
