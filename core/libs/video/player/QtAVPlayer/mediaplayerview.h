0de9f4000bbd11d548c4e466ccf6bcfbee0447db
Gilles Caulier <caulier.gilles@gmail.com>
2024-01-11 09:29:38 +0100

    dir missing

---
+++ core/libs/video/player/QtAVPlayer/mediaplayerview.h
@@ -0,0 +1,97 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * https://www.digikam.org
+ *
+ * Date        : 2006-20-12
+ * Description : a view to embed QtAv media player.
+ *
+ * SPDX-FileCopyrightText: 2006-2024 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * ============================================================ */
+
+#ifndef DIGIKAM_MEDIA_PLAYER_VIEW_H
+#define DIGIKAM_MEDIA_PLAYER_VIEW_H
+
+// Qt includes
+
+#include <QStackedWidget>
+#include <QEvent>
+#include <QUrl>
+
+// QtAVPlayer includes
+
+#include "qavplayer.h"
+
+// Local includes
+
+#include "digikam_export.h"
+#include "dinfointerface.h"
+
+namespace Digikam
+{
+
+class DIGIKAM_EXPORT MediaPlayerView : public QStackedWidget
+{
+    Q_OBJECT
+
+public:
+
+    explicit MediaPlayerView(QWidget* const parent);
+    ~MediaPlayerView() override;
+
+    void setCurrentItem(const QUrl& url   = QUrl(),
+                        bool  hasPrevious = false,
+                        bool  hasNext     = false);
+
+    void setInfoInterface(DInfoInterface* const iface);
+    void escapePreview();
+    void reload();
+
+Q_SIGNALS:
+
+    void signalNextItem();
+    void signalPrevItem();
+    void signalEscapePreview();
+
+public Q_SLOTS:
+
+    void slotEscapePressed();
+    void slotRotateVideo();
+
+private Q_SLOTS:
+
+    void slotPlayerStateChanged(QAVPlayer::State newState);
+    void slotMediaStatusChanged(QAVPlayer::MediaStatus newStatus);
+    void slotHandlePlayerError(QAVPlayer::Error err);
+    void slotThemeChanged();
+
+    /// Slidebar slots
+    void slotPositionChanged(qint64 position);
+    void slotDurationChanged(qint64 duration);
+    void slotVolumeChanged(int volume);
+    void slotLoopToggled(bool loop);
+    void slotPosition(int position);
+    void slotPausePlay();
+    void slotCapture();
+
+    void slotAudioFrame(const QAVAudioFrame& frame);
+
+    void slotImageCaptured(const QImage& image);
+
+private:
+
+    int  previewMode();
+    void setPreviewMode(int mode);
+
+private:
+
+    class Private;
+    Private* const d;
+};
+
+} // namespace Digikam
+
+#endif // DIGIKAM_MEDIA_PLAYER_VIEW_H
