939866803b9ff67fbe4edf3455ecea02112504d4
Gilles Caulier <caulier.gilles@gmail.com>
2024-01-17 07:54:40 +0100

    port++

--- project/bundles/homebrew/03-build-digikam.sh
+++ project/bundles/homebrew/03-build-digikam.sh
@@ -210,7 +210,7 @@ cmake --build . --config RelWithDebInfo --target ext_gmic_qt    -- -j$CPU_CORES
 cmake --build . --config RelWithDebInfo --target ext_mosaicwall -- -j$CPU_CORES
 cmake --build . --config RelWithDebInfo --target ext_flowview   -- -j$CPU_CORES
 
-mv -f $INSTALL_PREFIX/libexec/qt5/plugins/digikam/editor/*.so* $INSTALL_PREFIX/lib/plugins/digikam/editor/
+mv -f $INSTALL_PREFIX/libexec/qt6/plugins/digikam/editor/*.so* $INSTALL_PREFIX/share/qt/plugins/digikam/editor/
 
 #################################################################################################
 
