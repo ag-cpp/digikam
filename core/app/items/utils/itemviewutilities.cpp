190054cf1eaea11ab61f734a360f4c1293339e25
Maik Qualmann <metzpinguin@gmail.com>
2024-02-15 21:00:30 +0100

    fix group by file name through changes in 8.2.0 BUGS: 481366 FIXED-IN: 8.3.0

--- core/app/items/utils/itemviewutilities.cpp
+++ core/app/items/utils/itemviewutilities.cpp
@@ -457,7 +457,7 @@ void ItemViewUtilities::createGroupByFilenameFromInfoList(const ItemInfoList& it
     for (it = groupingList.begin() ; it != groupingList.end() ; )
     {
         QList<ItemInfo> group;
-        QString fname = it->name().left(it->name().indexOf(QLatin1Char('.')));
+        QString fname = it->name().left(it->name().lastIndexOf(QLatin1Char('.')));
 
         // don't know the leader yet so put first element also in group
 
@@ -465,7 +465,7 @@ void ItemViewUtilities::createGroupByFilenameFromInfoList(const ItemInfoList& it
 
         for (it2 = it + 1 ; it2 != groupingList.end() ; ++it2)
         {
-            QString fname2 = it2->name().left(it2->name().indexOf(QLatin1Char('.')));
+            QString fname2 = it2->name().left(it2->name().lastIndexOf(QLatin1Char('.')));
 
             if (fname == fname2)
             {
