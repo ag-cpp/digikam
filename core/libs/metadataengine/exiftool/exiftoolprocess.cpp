958108d14143a90257f9218d8074ddae6d2bd701
Maik Qualmann <metzpinguin@gmail.com>
2024-05-24 20:59:44 +0200

    polish Qt version check

--- core/libs/metadataengine/exiftool/exiftoolprocess.cpp
+++ core/libs/metadataengine/exiftool/exiftoolprocess.cpp
@@ -400,7 +400,7 @@ void ExifToolProcess::initExifTool()
     connect(this, &QProcess::started,
             this, &ExifToolProcess::slotStarted);
 
-#if QT_VERSION >= 0x060000
+#if (QT_VERSION >= QT_VERSION_CHECK(6, 0, 0))
 
     connect(this, &QProcess::finished,
             this, &ExifToolProcess::slotFinished);
