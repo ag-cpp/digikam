0722cbf80550c64148109f0f80f4cf376983a70f
Gilles Caulier <caulier.gilles@gmail.com>
2024-07-19 14:06:33 +0200

    krazy++

--- core/utilities/geolocation/engine/layers/MergedLayerDecorator.cpp
+++ core/utilities/geolocation/engine/layers/MergedLayerDecorator.cpp
@@ -5,8 +5,9 @@
  *
  * Date        : 2023-05-15
  * Description : geolocation engine based on Marble.
+ *               (c) 2007-2022 Marble Team
+ *               https://invent.kde.org/education/marble/-/raw/master/data/credits_authors.html
  *
- * SPDX-FileCopyrightText: 2007-2022 Marble Team
  * SPDX-FileCopyrightText: 2023-2024 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * SPDX-License-Identifier: LGPL-2.1-or-later
