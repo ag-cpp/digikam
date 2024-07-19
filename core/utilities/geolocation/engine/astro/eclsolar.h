a266053eb7be8cd0732bc013f3e4a6ab0881f1d9
Gilles Caulier <caulier.gilles@gmail.com>
2024-07-19 03:19:19 +0000

    fix CR 1610413

--- core/utilities/geolocation/engine/astro/eclsolar.h
+++ core/utilities/geolocation/engine/astro/eclsolar.h
@@ -23,12 +23,12 @@
 namespace Marble
 {
 
-const int GBL_ECLBUF = 12;  // maximum number of eclipses allowed
+const int GBL_ECLBUF = 12;          // maximum number of eclipses allowed
 const int MAXLUN     = 15;
 
-typedef double PMJD[MAXLUN];  // array of MJD's of the respective phase
+typedef double PMJD[MAXLUN];        // array of MJD's of the respective phase
 
-class DIGIKAM_EXPORT EclSolar     // Calculate Solar Eclipses
+class DIGIKAM_EXPORT EclSolar       // Calculate Solar Eclipses
 {
 public:
 
@@ -75,95 +75,101 @@ public:
 
 private:
 
-    void esinit();  // initialize EclSolar
-    static double atan23(double y, double x);   // atan without singularity for x,y=0
-    void DefTime();   // Get System Time and Date
-    void calcMaxPos(double& lat, double& lng); // get position of maximum eclipse
+    void esinit();                                                                  // initialize EclSolar
+    static double atan23(double y, double x);                                       // atan without singularity for x,y=0
+    void DefTime();                                                                 // Get System Time and Date
+    void calcMaxPos(double& lat, double& lng);                                      // get position of maximum eclipse
     static void GetMonth(int mm, char* mchr);
     static double phmjd(double yearf, double phase, double tdut,
                         int& eph, double& ejd, double& emag);
     void ckphase(double minmjd, double maxmjd, double yr,
                  double deltdut, int& mp, PMJD p, double phase);
     static void dtmstr(double jdmoon, char* dts);
-    void moonph();  // calculate phases of the Moon
-    void eclStart();   // initialize detailed calcs for selected eclipse
+    void moonph();                                                                  // calculate phases of the Moon
+    void eclStart();                                                                // initialize detailed calcs for selected eclipse
     static double getlocmag(double jd, double ep2, double phi, double lamda,
                             double height, const Vec3& rs, const Vec3& rm, int& totflg);
     static int iscrs(double vrc0, double vrc1, double dpn,
                      double& vrx0, double& vrx1, double& vrx20, double& vrx21);
-    void InitBound(); // initialize boundary calcs
-    void InRSBound();  // initialize Sunrise/Sunset boundaries
+    void InitBound();                                                               // initialize boundary calcs
+    void InRSBound();                                                               // initialize Sunrise/Sunset boundaries
     static double DegFDms(double h);
     int localStart(int j, double* spt, double* ept, int* spp,
                    int p, char* otxt);
-    static double navCourse(double lat1, double lng1, double lat2, double lng2);  // navigation course from p1 to p2
+    static double navCourse(double lat1, double lng1, double lat2, double lng2);    // navigation course from p1 to p2
     static void navNewPos(double d, double an, double lat1, double lng1, double& lat2, double& lng2);
-    static double sunObscure(double l1, double l2, double m);  // get the Obscuration of the Sun
+    static double sunObscure(double l1, double l2, double m);                       // get the Obscuration of the Sun
 
 private:
 
     // data fields
 
-    bool eb_start_called;  // true if eclStart() has already been called for eclipse
-    bool eb_moonph_called; // true if moonph has already been called for year
-    bool eb_lunecl;        // true if lunar eclipses are to be included
-    bool eb_lunactive;     // true if current eclipse is a lunar eclipse
-    bool eb_local_called;  // true if local circumstances calculated
+    bool eb_start_called    = false;    // true if eclStart() has already been called for eclipse
+    bool eb_moonph_called   = false;    // true if moonph has already been called for year
+    bool eb_lunecl          = false;    // true if lunar eclipses are to be included
+    bool eb_lunactive       = false;    // true if current eclipse is a lunar eclipse
+    bool eb_local_called    = false;    // true if local circumstances calculated
 
-    int eb_day;           // date
-    int eb_month;
-    int eb_year;
-    int eb_hour;
-    int eb_minute;
-    int eb_second;
-    double eb_time;
-    double eb_tzone;      // timezone in hours
-    double eb_del_tdut;   // TDT - UT in sec
-    int eb_del_auto;      // 1 = automatic del_tdut,  0 = manual
-    double eb_geolat;     // geographic coordinates (angles in degrees)
-    double eb_geolong;
-    double eb_geoheight;  // in meters
-    int eb_lstcall;  // 1 = last call moonph; 2 = caldisp; 0 = no call
-    int eb_locecl;   // 1 = local eclipse call; 0 = normal (global) call
+    int eb_day              = 0;        // date
+    int eb_month            = 0;
+    int eb_year             = 0;
+    int eb_hour             = 0;
+    int eb_minute           = 0;
+    int eb_second           = 0;
+    double eb_time          = 0.0;
+    double eb_tzone         = 0.0;      // timezone in hours
+    double eb_del_tdut      = 0.0;      // TDT - UT in sec
+    int eb_del_auto         = 0;        // 1 = automatic del_tdut,  0 = manual
+    double eb_geolat        = 0.0;      // geographic coordinates (angles in degrees)
+    double eb_geolong       = 0.0;
+    double eb_geoheight     = 0.0;      // in meters
+    int eb_lstcall          = 0;        // 1 = last call moonph; 2 = caldisp; 0 = no call
+    int eb_locecl           = 0;        // 1 = local eclipse call; 0 = normal (global) call
 
-    bool eb_finished;  // true if the loop run on consecutive calls is finished
-    bool eb_finished2;
-    int eb_numecl;   // number of eclipses of the year;
-    int eb_eclselect; // eclipse selected for detailed calculation
-    int eb_lastyear;   // last year for which moon phases were calculated
-    double eb_lasttz;  // last used timezone
-    double eb_lastdlt; // last delta-t used
-    double eb_cstep;   // step width in minutes used for central eclipse output
-    double eb_eclmjd [GBL_ECLBUF];  // the MJD's of the middle of the eclipses
-    double eb_magnitude [GBL_ECLBUF];   // magnitude of respective eclipse
-    int eb_phase [GBL_ECLBUF];    // phase of the eclipse. 0 if no eclipse,
+    bool eb_finished        = false;    // true if the loop run on consecutive calls is finished
+    bool eb_finished2       = false;
+    int eb_numecl           = 0;        // number of eclipses of the year;
+    int eb_eclselect        = 0;        // eclipse selected for detailed calculation
+    int eb_lastyear         = 0;        // last year for which moon phases were calculated
+    double eb_lasttz        = 0.0;      // last used timezone
+    double eb_lastdlt       = 0.0;      // last delta-t used
+    double eb_cstep         = 0.0;      // step width in minutes used for central eclipse output
+
+    double eb_eclmjd [GBL_ECLBUF]       = { 0.0 };   // the MJD's of the middle of the eclipses
+    double eb_magnitude [GBL_ECLBUF]    = { 0.0 };   // magnitude of respective eclipse
+    int eb_phase [GBL_ECLBUF]           = { 0 };     // phase of the eclipse. 0 if no eclipse,
+
     // 1 partial Sun, 2 non-central annular, 3 non-central total,
     // 4 annular, 5 total, 6 annual/total  Sun
     // -1 partial penumbral Moon, -2 penumbral Moon,
     // -3 partial Moon, -4 total Moon.
-    int eb_nphase;   // number of phases for eclipse details
-    int eb_spp[4];     // kind of eclipse phase i
-    double eb_spt[4];  // start time in MJD for phase i
-    double eb_ept[4];  // end time in MJD for phase i
-    int eb_lccnt; // = 0 not visible, 1,2 = first rise/set; 3,4 second r/s
-    double eb_lcb1, eb_lcb2, eb_lce1, eb_lce2;  // rise and set times (up to 2 possible)
-    double eb_jdmaxps; // MJD of maximum phase
-    double eb_maxps;  // value of maximum phase
-    double eb_maxelv; // elevation at maximum phase
-    double eb_ltotb;  // begin of totality/annularity at local position
-    double eb_ltote;  // end of totality/annularity at local position
-    int eb_cphs;   // phase of the central eclipse for center line plotting
-    double eb_clat; // current latitude of center line (in decimal degrees)
-    double eb_clng; // current longitude of center line (in decimal degrees)
-    double eb_cmxlat; // latitude of maximum eclipse (in decimal degrees)
-    double eb_cmxlng; // longitude of maximum eclipse (in decimal degrees)
-    double eb_jdstart; // (global) start jd of eclipse
-    double eb_jdstop;  // (global) end jd of eclipse
-    double eb_lastjd;  // last used MJD for eclipse calcs
-    double eb_dpb;   // Base value for diameter of penumbra
-    double eb_dpd;   // delta value for diameter of penumbra
-    double eb_penangle; // factor for multiplying the penumbra angle (1.0 for normal borders)
-    int eb_penamode;  // mode for using eb_penangle
+    int eb_nphase           = 0;        // number of phases for eclipse details
+
+    int eb_spp[4]           = { 0 };    // kind of eclipse phase i
+    double eb_spt[4]        = { 0.0 };  // start time in MJD for phase i
+    double eb_ept[4]        = { 0.0 };  // end time in MJD for phase i
+    int eb_lccnt            = 0;        // = 0 not visible, 1,2 = first rise/set; 3,4 second r/s
+
+    double eb_lcb1 = 0.0, eb_lcb2 = 0.0, eb_lce1 = 0.0, eb_lce2 = 0.0;  // rise and set times (up to 2 possible)
+
+    double eb_jdmaxps       = 0.0;      // MJD of maximum phase
+    double eb_maxps         = 0.0;      // value of maximum phase
+    double eb_maxelv        = 0.0;      // elevation at maximum phase
+    double eb_ltotb         = 0.0;      // begin of totality/annularity at local position
+    double eb_ltote         = 0.0;      // end of totality/annularity at local position
+    int eb_cphs             = 0;        // phase of the central eclipse for center line plotting
+    double eb_clat          = 0.0;      // current latitude of center line (in decimal degrees)
+    double eb_clng          = 0.0;      // current longitude of center line (in decimal degrees)
+    double eb_cmxlat        = 0.0;      // latitude of maximum eclipse (in decimal degrees)
+    double eb_cmxlng        = 0.0;      // longitude of maximum eclipse (in decimal degrees)
+    double eb_jdstart       = 0.0;      // (global) start jd of eclipse
+    double eb_jdstop        = 0.0;      // (global) end jd of eclipse
+    double eb_lastjd        = 0.0;      // last used MJD for eclipse calcs
+    double eb_dpb           = 0.0;      // Base value for diameter of penumbra
+    double eb_dpd           = 0.0;      // delta value for diameter of penumbra
+    double eb_penangle      = 0.0;      // factor for multiplying the penumbra angle (1.0 for normal borders)
+    int eb_penamode         = 0;        // mode for using eb_penangle
+
     Vec3 eb_ubm;  // Penumbra (or Umbra or Moon) base vector
     Vec3 eb_ube;  // Shadow base vector for upper boundary
     Vec3 eb_udm;  // Penumbra base delta vector
