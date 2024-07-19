64bdfc0840795894094af7155e2acf421f04a4b6
Gilles Caulier <caulier.gilles@gmail.com>
2024-07-19 19:20:03 +0000

    krazy++

--- core/utilities/geolocation/engine/osm/MarbleZipWriter.h
+++ core/utilities/geolocation/engine/osm/MarbleZipWriter.h
@@ -5,11 +5,11 @@
  *
  * Date        : 2023-05-15
  * Description : geolocation engine based on Marble.
+ *               (c) 2015 The Qt Company Ltd.
  *               This file is based on qzipwriter_p.h from Qt with the original license
  *               below, taken from
  *               https://code.qt.io/cgit/qt/qt.git/plain/src/gui/text/qzipwriter_p.h
  *
- * SPDX-FileCopyrightText: 2015 The Qt Company Ltd. <https://www.qt.io/licensing/>
  * SPDX-FileCopyrightText: 2023-2024 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * SPDX-License-Identifier: LGPL-2.1-only WITH Qt-LGPL-exception-1.1 OR LGPL-3.0-only WITH Qt-LGPL-exception-1.1 OR GPL-3.0-only OR LicenseRef-Qt-Commercial
