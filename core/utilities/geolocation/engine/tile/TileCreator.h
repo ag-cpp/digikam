86aa37da5fdaff0db43600a541c44a5d55e7485a
Gilles Caulier <caulier.gilles@gmail.com>
2024-07-19 08:39:34 +0200

    krazy++

--- core/utilities/geolocation/engine/tile/TileCreator.h
+++ core/utilities/geolocation/engine/tile/TileCreator.h
@@ -6,7 +6,7 @@
  * Date        : 2023-05-15
  * Description : geolocation engine based on Marble.
  *
- * SPDX-FileCopyrightText: 2007-2022 Marble Team
+ * SPDX-FileCopyrightText: 2007-2022 Marble Team <marble dot kde dot org> <marble dot kde dot org>
  * SPDX-FileCopyrightText: 2023-2024 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * SPDX-License-Identifier: LGPL-2.1-or-later
