c65b90fa4a22dbbf313a11776e66a68712f5d0aa
Gilles Caulier <caulier.gilles@gmail.com>
2024-09-11 03:41:58 +0000

    Macports : factorize rules to compile with Jasper headers. Fix broken compilation with __STDC_VERSION__ CCBUGS: 491857

--- core/dplugins/dimg/jpeg2000/dimgjpeg2000loader.cpp
+++ core/dplugins/dimg/jpeg2000/dimgjpeg2000loader.cpp
@@ -12,44 +12,7 @@
  *
  * ============================================================ */
 
-#include "dimgjpeg2000loader.h"
-
-// Local includes
-
-#include "digikam_debug.h"
-
-// Jasper includes
-
-#ifndef Q_CC_MSVC
-extern "C"
-{
-#endif
-
-#if !defined(Q_OS_DARWIN) && defined(Q_CC_GNU)
-#   pragma GCC diagnostic push
-#   pragma GCC diagnostic ignored "-Wcpp"
-#endif
-
-#if defined(Q_OS_DARWIN) && defined(Q_CC_CLANG)
-#   pragma clang diagnostic push
-#   pragma clang diagnostic ignored "-Wshift-negative-value"
-#endif
-
-#include <jasper/jasper.h>
-#include <jasper/jas_version.h>
-
-// Restore warnings
-#if !defined(Q_OS_DARWIN) && defined(Q_CC_GNU)
-#   pragma GCC diagnostic pop
-#endif
-
-#if defined(Q_OS_DARWIN) && defined(Q_CC_CLANG)
-#   pragma clang diagnostic pop
-#endif
-
-#ifndef Q_CC_MSVC
-}
-#endif
+#include "dimgjpeg2000loader_p.h"
 
 namespace DigikamJPEG2000DImgPlugin
 {
--- core/dplugins/dimg/jpeg2000/dimgjpeg2000plugin.cpp
+++ core/dplugins/dimg/jpeg2000/dimgjpeg2000plugin.cpp
@@ -24,44 +24,8 @@
 
 // Local includes
 
-#include "digikam_debug.h"
-#include "digikam_config.h"
-#include "digikam_globals.h"
-#include "dimgjpeg2000loader.h"
+#include "dimgjpeg2000loader_p.h"
 #include "dimgjpeg2000exportsettings.h"
-
-// Jasper includes
-
-#ifndef Q_CC_MSVC
-extern "C"
-{
-#endif
-
-#if !defined(Q_OS_DARWIN) && defined(Q_CC_GNU)
-#   pragma GCC diagnostic push
-#   pragma GCC diagnostic ignored "-Wcpp"
-#endif
-
-#if defined(Q_OS_DARWIN) && defined(Q_CC_CLANG)
-#   pragma clang diagnostic push
-#   pragma clang diagnostic ignored "-Wshift-negative-value"
-#endif
-
-#include <jasper/jasper.h>
-#include <jasper/jas_version.h>
-
-// Restore warnings
-#if !defined(Q_OS_DARWIN) && defined(Q_CC_GNU)
-#   pragma GCC diagnostic pop
-#endif
-
-#if defined(Q_OS_DARWIN) && defined(Q_CC_CLANG)
-#   pragma clang diagnostic pop
-#endif
-
-#ifndef Q_CC_MSVC
-}
-#endif
 
 namespace DigikamJPEG2000DImgPlugin
 {
