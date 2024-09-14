3a4d9a1a2bbd8233c60233140c5392da17c090dc
Gilles Caulier <caulier.gilles@gmail.com>
2024-09-14 03:43:25 +0000

    polish

--- project/bundles/macports/config.sh
+++ project/bundles/macports/config.sh
@@ -91,6 +91,7 @@ DK_URL="http://download.kde.org/stable/digikam"
 DK_BUILDTEMP=~/dktemp
 
 # Qt version to use in bundle and provided by Macports.
+#DK_QTVERSION="5"
 DK_QTVERSION="6"
 
 # Mariadb version to install for Qt SQL plugin.
